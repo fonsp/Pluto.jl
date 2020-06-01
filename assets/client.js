41459560d7c0f84834176b3d15209d2ff0e5f640
Michiel Dral <m.c.dral@gmail.com>
2020-06-01 18:29:11 +0200

    Update client.js

--- assets/client.js
+++ assets/client.js
@@ -1,3 +1,24 @@
+// Polyfill for Blob::text when there is none (safari)
+// This is not "officialy" supported
+if (Blob.prototype.text == null) {
+    Blob.prototype.text = function() {
+        const reader = new FileReader()
+        const promise = new Promise((resolve, reject) => {
+            // on read success
+            reader.onload = () => {
+                resolve(reader.result)
+            }
+            // on failure
+            reader.onerror = (e) => {
+                reader.abort()
+                reject(e)
+            }
+        })
+        reader.readAsText(this)
+        return promise;
+    }
+}
+
 export class PlutoConnection {
     ping(onSucces, onFailure) {
         fetch("ping", {
