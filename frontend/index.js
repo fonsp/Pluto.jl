a1d03bd5d79a64d644eccce16dd67cba6b2fc089
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2020-08-01 09:25:18 +1000

    Include polyfill

--- frontend/index.js
+++ frontend/index.js
@@ -2,4 +2,6 @@ import { html, render } from "./common/Preact.js"
 
 import { Welcome } from "./components/Welcome.js"
 
+import "./common/Polyfill.js"
+
 render(html`<${Welcome} />`, document.querySelector("main"))
