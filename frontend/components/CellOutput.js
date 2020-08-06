cf38322152ea5e1b04305bafb238624df6410d7d
János Veres <janos.veres@gmail.com>
2020-08-06 18:05:09 +0200

    Update CellOutput.js

--- frontend/components/CellOutput.js
+++ frontend/components/CellOutput.js
@@ -60,8 +60,8 @@ const OutputBody = ({ mime, body, cell_id, all_completed_promise, requests }) =>
         case "image/gif":
         case "image/bmp":
         case "image/svg+xml":
-            console.log(body)
-            return html`<div><img src=${unescape(body)} /></div>`
+            const _body = String.raw `${body}` 
+            return html`<div><img src=${_body} /></div>`
             break
         case "text/html":
         case "application/vnd.pluto.tree+xml":
