f921b2d508924676d52ea27ef0c539f1dd9626b4
Roger-luo <rogerluo.rl18@gmail.com>
2020-09-14 18:26:15 -0400

    add tests for notebooks

--- test/WorkspaceManager.jl
+++ test/WorkspaceManager.jl
@@ -31,5 +31,20 @@ import Pluto: update_save_run!, WorkspaceManager, ClientSession, ServerSession, 
         @test notebookB.cells[1].errored == true
     end
 
+    @testset "notebook environment" begin
+        session_options = CompilerOptions()
+        notebook = Notebook([Cell("x")])
+        notebook.compiler_options = CompilerOptions(;project="test")
+        @test _merge_notebook_compiler_options(notebook, session_options).project ==
+            joinpath(dirname(notebook.path), "test")
 
+        notebook.compiler_options = CompilerOptions(;project=project_relative_path("test"))
+        @test _merge_notebook_compiler_options(notebook, session_options).project ==
+            project_relative_path("test")
+        
+        session_options = CompilerOptions(;project=project_relative_path("test"))
+        notebook.compiler_options = CompilerOptions(;project=project_relative_path("Project.toml"))
+        @test _merge_notebook_compiler_options(notebook, session_options).project ==
+            project_relative_path("Project.toml")
+    end
 end
