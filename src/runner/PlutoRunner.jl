94742fa32ade9ce0156bf84935c625aae5eb6618
fonsp <fonsvdplas@gmail.com>
2020-09-26 14:42:53 +0000

    try catch

--- src/runner/PlutoRunner.jl
+++ src/runner/PlutoRunner.jl
@@ -708,17 +708,22 @@ Logging.shouldlog(::PlutoLogger, args...) = true
 Logging.min_enabled_level(::PlutoLogger) = Logging.Debug
 Logging.catch_exceptions(::PlutoLogger) = false
 function Logging.handle_message(::PlutoLogger, level, msg, _module, group, id, file, line; kwargs...)
-    put!(log_channel, (
-        level=string(level),
-        msg=(msg isa String) ? msg : repr(msg),
-        group=group,
-        # id=id,
-        file=file,
-        line=line,
-        kwargs=Dict((k=>repr(v) for (k,v) in kwargs)...),
-    ))
-    # also print to console
-    Logging.handle_message(old_logger[], level, msg, _module, group, id, file, line; kwargs...)
+    try
+        put!(log_channel, (
+            level=string(level),
+            msg=(msg isa String) ? msg : repr(msg),
+            group=group,
+            # id=id,
+            file=file,
+            line=line,
+            kwargs=Dict((k=>repr(v) for (k,v) in kwargs)...),
+        ))
+        # also print to console
+        Logging.handle_message(old_logger[], level, msg, _module, group, id, file, line; kwargs...)
+    catch e
+        println(stderr, "Failed to relay log from PlutoRunner")
+        showerror(stderr, e, stacktrace(catch_backtrace()))
+    end
 end
 
 # we put this in __init__ to fix a world age problem
