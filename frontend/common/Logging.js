c2c254abca63889db566ac0bcdc570a3277ec118
fonsp <fonsvdplas@gmail.com>
2020-09-27 08:49:37 +0000

    Fixed logging while code is hidden by removing the line highlight and just doing cell highlight instead

--- frontend/common/Logging.js
+++ frontend/common/Logging.js
@@ -11,21 +11,11 @@ export const handle_log = ({ level, msg, file, line, kwargs }, filename) => {
     if (Object.keys(kwargs).length !== 0) {
         args.push(kwargs)
     }
-
     if (file.startsWith(filename)) {
         const cell_id = file.substring(file.indexOf("#==#") + 4)
         const cell_node = document.getElementById(cell_id)
 
-        // hacky hackkkkkk
-        const cm_internal_node = cell_node.querySelector(".CodeMirror-code")
-        const origin_line = cm_internal_node.children[line - 1]
-
-        args.push(`\n\nfrom`)
-        if (origin_line == null) {
-            args.push(cell_node)
-        } else {
-            args.push(origin_line)
-        }
+        args.push(`\n\nfrom`, cell_node)
     }
     f(...args)
 }
