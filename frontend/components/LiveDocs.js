1e54ccac72775b4a1e3b9452998c53897fd5679e
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2020-10-05 17:55:46 +0700

    Rewrite LiveDocs in functional component

--- frontend/components/LiveDocs.js
+++ frontend/components/LiveDocs.js
@@ -1,96 +1,77 @@
-import { html, Component } from "../common/Preact.js"
+import { html, Component, useState, useEffect, useRef } from "../common/Preact.js"
 import observablehq from "../common/SetupCellEnvironment.js"
 import { cl } from "../common/ClassTable.js"
 
 import { RawHTMLContainer } from "./CellOutput.js"
 
-export class LiveDocs extends Component {
-    constructor() {
-        super()
-        this.state = {
-            shown_query: null,
-            searched_query: null,
-            body: "Start typing in a cell to learn more!",
-            hidden: true,
-            loading: false,
-        }
-        this.updateDocTimer = undefined
-    }
+export const LiveDocs = ({ desired_doc_query, on_update_doc_query, client, notebook }) => {
+    const [shown_query, set_shown_query] = useState(null)
+    const [searched_query, set_searched_query] = useState(null)
+    const [body, set_body] = useState("Start typing in a cell to learn more!")
+    const [hidden, set_hidden] = useState(true)
+    const [loading, set_loading] = useState(false)
 
-    componentDidMount() {
-        window.addEventListener("open_live_docs", () => {
-            // https://github.com/fonsp/Pluto.jl/issues/321
-            this.setState({
-                hidden: false,
-            })
-            if (window.getComputedStyle(this.base).display === "none") {
-                alert("This browser window is too small to show docs.\n\nMake the window bigger, or try zooming out.")
-            }
-        })
-    }
+    const liveDocRef = useRef()
 
-    componentDidUpdate() {
-        if (this.state.hidden || this.state.loading) {
-            return
-        }
-        if (!/[^\s]/.test(this.props.desired_doc_query)) {
-            // only whitespace
-            return
-        }
+    const fetch_docs = () => {
+        const new_query = desired_doc_query
+        set_loading(true)
+        set_searched_query(new_query)
 
-        if (this.state.searched_query === this.props.desired_doc_query) {
-            return
-        }
-
-        this.fetch_docs()
-    }
-
-    fetch_docs() {
-        const new_query = this.props.desired_doc_query
-        this.setState({
-            loading: true,
-            searched_query: new_query,
-        })
         Promise.race([
             observablehq.Promises.delay(2000, false),
-            this.props.client.send("docs", { query: new_query }, { notebook_id: this.props.notebook.notebook_id }).then((u) => {
+            client.send("docs", { query: new_query }, { notebook_id: notebook.notebook_id }).then((u) => {
                 if (u.message.status === "⌛") {
                     return false
                 }
                 if (u.message.status === "👍") {
-                    this.setState({
-                        shown_query: new_query,
-                        body: u.message.doc,
-                    })
+                    set_shown_query(new_query)
+                    set_body(u.message.doc)
                     return true
                 }
-            }),
-        ]).then(() => {
-            this.setState({
-                loading: false,
             })
+        ]).then(() => {
+            set_loading(false)
         })
     }
 
-    render() {
-        return html`
-            <aside id="helpbox-wrapper">
-                <pluto-helpbox class=${cl({ hidden: this.state.hidden, loading: this.state.loading })}>
-                    <header onClick=${() => this.setState({ hidden: !this.state.hidden })}>
-                        ${this.state.hidden || this.state.searched_query == null ? "Live docs" : this.state.searched_query}
-                    </header>
-                    <section>
-                        <h1><code>${this.state.shown_query}</code></h1>
-                        <${RawHTMLContainer}
-                            body=${this.state.body}
-                            pure=${true}
-                            on_render=${(n) => resolve_doc_reference_links(n, this.props.on_update_doc_query)}
-                        />
-                    </section>
-                </pluto-helpbox>
-            </aside>
-        `
-    }
+    useEffect(() => {
+        window.addEventListener("open_live_docs", () => {
+            // https://github.com/fonsp/Pluto.jl/issues/321
+            set_hidden(false)
+            if (window.getComputedStyle(liveDocRef.current).display === "none") {
+                alert("This browser window is too small to show docs.\n\nMake the window bigger, or try zooming out.")
+            }
+        })
+    }, [])
+
+    useEffect(() => {
+        if (hidden || loading) {
+            return
+        }
+        if (!/[^\s]/.test(desired_doc_query)) {
+            // only whitespace
+            return
+        }
+
+        if (searched_query === desired_doc_query) {
+            return
+        }
+
+        fetch_docs()
+    })
+
+    return html`
+        <aside id="helpbox-wrapper">
+            <pluto-helpbox class=${cl({ hidden, loading })}>
+                <header onClick=${() => set_hidden(!hidden)}>${hidden || searched_query == null ? "Live docs" : searched_query}</header>
+                <section>
+                    <h1><code>${shown_query}</code></h1>
+                    <${RawHTMLContainer} body=${body} pure=${true} on_render=${(n) => resolve_doc_reference_links(n, on_update_doc_query)} />
+                </section>
+            </pluto-helpbox>
+        </aside>
+    `
 }
 
 const resolve_doc_reference_links = (node, on_update_doc_query) => {
