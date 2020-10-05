472eba3ce26ad55bd8757dd0fe4649bf98feb2dc
Michiel Dral <m.c.dral@gmail.com>
2020-10-05 16:25:47 +0200

    Enhance javascript

--- src/notebook/Cell.jl
+++ src/notebook/Cell.jl
@@ -1,30 +1,3 @@
-# import UUIDs: UUID, uuid1
-# import .ExpressionExplorer: SymbolsState
-
-# "The building block of a `Notebook`. Contains code, output, reactivity data, mitochondria and ribosomes."
-# mutable struct Cell
-#     "Because Cells can be reordered, they get a UUID. The JavaScript frontend indexes cells using the UUID."
-#     cell_id::UUID
-#     code::String
-    
-#     output_repr::Union{Vector{UInt8},String,Nothing}
-#     repr_mime::MIME
-#     errored::Bool
-#     runtime::Union{Missing,UInt64}
-#     code_folded::Bool
-#     queued::Bool
-#     running::Bool
-#     last_run_timestamp::Float64
-    
-#     parsedcode::Union{Nothing,Expr}
-#     module_usings::Set{Expr}
-#     rootassignee::Union{Nothing,Symbol}
-# end
-
-# Cell(cell_id, code) = Cell(cell_id, code, nothing, MIME("text/plain"), false, missing, false, false, false, 0, nothing, Set{Expr}(), nothing)
-# Cell(code) = Cell(uuid1(), code)
-# Cell() = Cell("")
-
 import UUIDs: UUID, uuid1
 import .ExpressionExplorer: SymbolsState
 
