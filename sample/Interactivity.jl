7cc235ff2e5a786b83981f4634551d96cd9118bf
Michiel Dral <m.c.dral@gmail.com>
2020-10-07 15:12:51 +0200

    Javascript """""enhancements"""" (#539)
    Co-authored-by: fonsp <fonsvdplas@gmail.com>

--- sample/Interactivity.jl
+++ sample/Interactivity.jl
@@ -93,8 +93,8 @@ Try drawing a rectangle in the canvas below 👇 and notice that the `area` vari
 <canvas width="200" height="200" style="position: relative"></canvas>
 
 <script>
-// 🐸 `this` is the cell output wrapper - we use it to select elements 🐸 //
-const canvas = this.querySelector("canvas")
+// 🐸 `currentScript` is the current script tag - we use it to select elements 🐸 //
+const canvas = currentScript.closest('pluto-output').querySelector("canvas")
 const ctx = canvas.getContext("2d")
 
 var startX = 80
