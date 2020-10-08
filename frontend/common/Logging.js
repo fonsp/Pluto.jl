b113783ac2673822598a6ee8c4e0abd02797521c
Michiel Dral <m.c.dral@gmail.com>
2020-10-08 02:58:30 +0200

    Possibly "fixed" the startsWith error

--- frontend/common/Logging.js
+++ frontend/common/Logging.js
@@ -6,16 +6,18 @@ const log_functions = {
 }
 
 export const handle_log = ({ level, msg, file, line, kwargs }, filename) => {
-    const f = log_functions[level] || console.log
-    const args = [`%c┌ ${level}:\n`, `font-weight: bold`, msg]
-    if (Object.keys(kwargs).length !== 0) {
-        args.push(kwargs)
-    }
-    if (file.startsWith(filename)) {
-        const cell_id = file.substring(file.indexOf("#==#") + 4)
-        const cell_node = document.getElementById(cell_id)
+    try {
+        const f = log_functions[level] || console.log
+        const args = [`%c┌ ${level}:\n`, `font-weight: bold`, msg]
+        if (Object.keys(kwargs).length !== 0) {
+            args.push(kwargs)
+        }
+        if (file.startsWith(filename)) {
+            const cell_id = file.substring(file.indexOf("#==#") + 4)
+            const cell_node = document.getElementById(cell_id)
 
-        args.push(`\n\nfrom`, cell_node)
-    }
-    f(...args)
+            args.push(`\n\nfrom`, cell_node)
+        }
+        f(...args)
+    } catch (err) {} // TODO
 }
