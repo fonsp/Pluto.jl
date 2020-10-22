6239bbe6019363c804ca4a0ef3d42187c20a45ab
Patrick Bouffard <bouffard@eecs.berkeley.edu>
2020-10-22 12:26:51 -0700

    auto-indent

--- frontend/components/CellInput.js
+++ frontend/components/CellInput.js
@@ -153,6 +153,9 @@ export const CellInput = ({
                     cm.setSelections(new_selections)
                 }
             }
+            keys["Shift-Alt-F"]  = () => {
+                cm.execCommand("indentAuto");
+            }
             const swap = (a, i, j) => {
                 ;[a[i], a[j]] = [a[j], a[i]]
             }
