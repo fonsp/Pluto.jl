e2d06a2ae59a17841852f3332085fed0d7e0ef36
Michiel Dral <m.c.dral@gmail.com>
2020-10-28 01:33:45 +0100

    Lets get it updatin

--- test/frontend/__tests__/autocomplete_test.js
+++ test/frontend/__tests__/autocomplete_test.js
@@ -1,27 +1,26 @@
-import {
-    lastElement,
-    dismissBeforeUnloadDialogs, saveScreenshot, getTestScreenshotPath
-} from '../helpers/common'
-import { getCellIds, importNotebook, waitForCellOutput, getPlutoUrl, prewarmPluto } from '../helpers/pluto'
+import { lastElement, dismissBeforeUnloadDialogs, saveScreenshot, getTestScreenshotPath } from "../helpers/common"
+import { getCellIds, importNotebook, waitForCellOutput, getPlutoUrl, prewarmPluto } from "../helpers/pluto"
 
-describe('PlutoAutocomplete', () => {
+jest.setTimeout(100000)
+
+describe("PlutoAutocomplete", () => {
     beforeAll(async () => {
         dismissBeforeUnloadDialogs(page)
         await prewarmPluto(page)
     })
 
     beforeEach(async () => {
-        await page.goto(getPlutoUrl(), { waitUntil: 'networkidle0' })
+        await page.goto(getPlutoUrl(), { waitUntil: "networkidle0" })
     })
 
     afterEach(async () => {
         await saveScreenshot(page, getTestScreenshotPath())
     })
 
-    it('should get the correct autocomplete suggestions', async () => {
-        await importNotebook('autocomplete_notebook.jl')
+    it("should get the correct autocomplete suggestions", async () => {
+        await importNotebook("autocomplete_notebook.jl")
         const importedCellIds = await getCellIds(page)
-        await Promise.all(importedCellIds.map(cellId => waitForCellOutput(page, cellId)))
+        await Promise.all(importedCellIds.map((cellId) => waitForCellOutput(page, cellId)))
 
         // Add a new cell
         let lastPlutoCellId = lastElement(importedCellIds)
@@ -30,21 +29,21 @@ describe('PlutoAutocomplete', () => {
 
         // Type the partial input
         lastPlutoCellId = lastElement(await getCellIds(page))
-        await page.type(`pluto-cell[id="${lastPlutoCellId}"] pluto-input textarea`, 'my_su')
+        await page.type(`pluto-cell[id="${lastPlutoCellId}"] pluto-input textarea`, "my_su")
 
         // Trigger autocomplete suggestions
-        await page.keyboard.press('Tab')
-        await page.waitForSelector('.CodeMirror-hints')
+        await page.keyboard.press("Tab")
+        await page.waitForSelector(".CodeMirror-hints")
         // Get suggestions
-        const suggestions = await page.evaluate(() => Array.from(document.querySelectorAll('.CodeMirror-hints li')).map(suggestion => suggestion.textContent))
+        const suggestions = await page.evaluate(() => Array.from(document.querySelectorAll(".CodeMirror-hints li")).map((suggestion) => suggestion.textContent))
         suggestions.sort()
-        expect(suggestions).toEqual(['my_subtract', 'my_sum1', 'my_sum2'])
+        expect(suggestions).toEqual(["my_subtract", "my_sum1", "my_sum2"])
     })
 
-    it('should automatically autocomplete if there is only one possible suggestion', async () => {
-        await importNotebook('autocomplete_notebook.jl')
+    it("should automatically autocomplete if there is only one possible suggestion", async () => {
+        await importNotebook("autocomplete_notebook.jl")
         const importedCellIds = await getCellIds(page)
-        await Promise.all(importedCellIds.map(cellId => waitForCellOutput(page, cellId)))
+        await Promise.all(importedCellIds.map((cellId) => waitForCellOutput(page, cellId)))
 
         // Add a new cell
         let lastPlutoCellId = lastElement(importedCellIds)
@@ -53,14 +52,17 @@ describe('PlutoAutocomplete', () => {
 
         // Type the partial input
         lastPlutoCellId = lastElement(await getCellIds(page))
-        await page.type(`pluto-cell[id="${lastPlutoCellId}"] pluto-input textarea`, 'my_sub')
+        await page.type(`pluto-cell[id="${lastPlutoCellId}"] pluto-input textarea`, "my_sub")
 
         // Trigger autocomplete
-        await page.keyboard.press('Tab')
-        await page.waitFor(1000)
+        await page.keyboard.press("Tab")
+        await page.waitFor(5000)
 
         // Get suggestions
-        const autocompletedInput = await page.evaluate((selector) => document.querySelector(selector).textContent.trim(), `pluto-cell[id="${lastPlutoCellId}"] pluto-input .CodeMirror-line`)
-        expect(autocompletedInput).toEqual('my_subtract')
+        const autocompletedInput = await page.evaluate(
+            (selector) => document.querySelector(selector).textContent.trim(),
+            `pluto-cell[id="${lastPlutoCellId}"] pluto-input .CodeMirror-line`
+        )
+        expect(autocompletedInput).toEqual("my_subtract")
     })
 })
