acf989502fdca93692d440e8d7769daaee89f50d
fonsp <fonsvdplas@gmail.com>
2020-11-11 13:43:34 +0000

    🦓 Fix #662

--- src/evaluation/WorkspaceManager.jl
+++ src/evaluation/WorkspaceManager.jl
@@ -1,6 +1,6 @@
 module WorkspaceManager
 import UUIDs: UUID
-import ..Pluto: Configuration, Notebook, Cell, ServerSession, ExpressionExplorer, pluto_filename, trycatch_expr, Token, withtoken, tamepath, project_relative_path, putnotebookupdates!, UpdateMessage
+import ..Pluto: Configuration, Notebook, Cell, ServerSession, ExpressionExplorer, pluto_filename, trycatch_expr, Token, withtoken, Promise, tamepath, project_relative_path, putnotebookupdates!, UpdateMessage
 import ..Configuration: CompilerOptions
 import ..Pluto.ExpressionExplorer: FunctionName
 import ..PlutoRunner
@@ -29,7 +29,7 @@ const process_preamble = [
 ]
 
 const moduleworkspace_count = Ref(0)
-const workspaces = Dict{UUID,Workspace}()
+const workspaces = Dict{UUID,Promise{Workspace}}()
 
 
 """Create a workspace for the notebook, optionally in a separate process.
@@ -184,11 +184,10 @@ end
 "Return the `Workspace` of `notebook`; will be created if none exists yet."
 function get_workspace(session_notebook::Tuple{ServerSession, Notebook})::Workspace
     session, notebook = session_notebook
-    if haskey(workspaces, notebook.notebook_id)
-        workspaces[notebook.notebook_id]
-    else
-        workspaces[notebook.notebook_id] = make_workspace(session_notebook)
+    promise = get!(workspaces, notebook.notebook_id) do
+        Promise{Workspace}(() -> make_workspace(session_notebook))
     end
+    wait(promise)
 end
 get_workspace(workspace::Workspace)::Workspace = workspace
 
