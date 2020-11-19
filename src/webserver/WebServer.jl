b205e1febc1786e324608b8c5a8ed0671c714c69
fonsp <fonsvdplas@gmail.com>
2020-11-19 18:25:31 +0000

    🎪 HTTP blub

--- src/webserver/WebServer.jl
+++ src/webserver/WebServer.jl
@@ -166,6 +166,8 @@ function run(session::ServerSession)
                     catch ex
                         if ex isa InterruptException
                             shutdown_server[]()
+                        elseif ex isa HTTP.WebSockets.WebSocketError || ex isa EOFError
+                            # that's fine!
                         else
                             bt = stacktrace(catch_backtrace())
                             @warn "Reading WebSocket client stream failed for unknown reason:" exception = (ex, bt)
