a901a7ac8fca272930231ce2c5d11599abfc028d
Fons van der Plas <fonsvdplas@gmail.com>
2020-12-11 18:48:42 +0100

    merge master

--- frontend/components/ExportBanner.js
+++ frontend/components/ExportBanner.js
@@ -46,15 +46,20 @@ export const ExportBanner = ({ notebook, pluto_version, onClose, open }) => {
                         }).then((html) => {
                             if (html != null) {
                                 const fake_anchor = document.createElement("a")
-                                fake_anchor.download = `${notebook.shortpath}.html`
-                                fake_anchor.href = URL.createObjectURL(
-                                    new Blob([html], {
-                                        type: "text/html",
-                                    })
+                                fake_anchor.setAttribute("download", `${notebook.shortpath}.html`)
+                                fake_anchor.setAttribute(
+                                    "href",
+                                    URL.createObjectURL(
+                                        new Blob([html], {
+                                            type: "text/html",
+                                        })
+                                    )
                                 )
                                 document.body.appendChild(fake_anchor)
-                                fake_anchor.click()
-                                document.body.removeChild(fake_anchor)
+                                setTimeout(() => {
+                                    fake_anchor.click()
+                                    document.body.removeChild(fake_anchor)
+                                }, 100)
                             }
                         })
                     }}
