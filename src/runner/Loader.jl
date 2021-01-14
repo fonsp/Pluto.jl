cea9e822b168f4f2fe9080b5fc73e5e66c215a54
Fons van der Plas <fonsvdplas@gmail.com>
2021-01-14 01:15:57 +0100

    Update Loader.jl

--- src/runner/Loader.jl
+++ src/runner/Loader.jl
@@ -16,16 +16,19 @@ write(new_ptoml_path, ptoml_contents[findfirst("[deps]", ptoml_contents)[1]:end]
 local pkg_ctx = Pkg.Types.Context(env=Pkg.Types.EnvCache(new_ptoml_path))
 
 try
-    Pkg.resolve(pkg_ctx; io=devnull)
-    Pkg.instantiate(pkg_ctx; io=devnull)
+    Pkg.resolve(pkg_ctx; io=devnull) # supress IO
 catch
     # if it failed, do it again without suppressing io
     try
         Pkg.resolve(pkg_ctx)
-        Pkg.instantiate(pkg_ctx)
     catch e
-        @error "Failed to resolve+instantiate notebook boot environment" exception=(e, catch_backtrace())
+        @error "Failed to resolve notebook boot environment" exception=(e, catch_backtrace())
     end
+end
+try
+    Pkg.instantiate(pkg_ctx) # we don't suppress IO for this one because it can take very long, and that would be a frustrating experience without IO
+catch e
+    @error "Failed to instantiate notebook boot environment" exception=(e, catch_backtrace())
 end
 
 
