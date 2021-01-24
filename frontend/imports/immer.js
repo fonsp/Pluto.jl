93efd83759f2630314fa1fe4137a59b3a55bc10c
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@intelligencia.ai>
2021-01-25 01:31:24 +0200

    Disable immer autofreeze

--- frontend/imports/immer.js
+++ frontend/imports/immer.js
@@ -1,7 +1,8 @@
 // @ts-nocheck
-import immer, { produceWithPatches, applyPatches, enablePatches } from "https://cdn.jsdelivr.net/npm/immer@8.0.0/dist/immer.esm.js"
+import immer, { produceWithPatches, applyPatches, enablePatches, setAutoFreeze } from "https://cdn.jsdelivr.net/npm/immer@8.0.0/dist/immer.esm.js"
 
 export { applyPatches, produceWithPatches }
 export default immer
 
 enablePatches()
+setAutoFreeze(false)
