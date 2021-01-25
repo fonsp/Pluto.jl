834ce1a4bf942285f32d8ec148ebf78e4da0949a
Fons van der Plas <fonsvdplas@gmail.com>
2021-01-25 15:45:37 +0100

    update immer comment

--- frontend/imports/immer.js
+++ frontend/imports/immer.js
@@ -5,5 +5,9 @@ export { applyPatches, produceWithPatches }
 export default immer
 
 enablePatches()
-// Probably https://github.com/immerjs/immer/issues/576, in conjuction with not-only-using-immer for this.setState
+
+// we have some Editor.setState functions that use immer, so Editor.this.state becomes an "immer immutable frozen object". But we also have some Editor.setState functions that don't use immer, and they try to _mutate_ Editor.this.state. This gives errors like https://github.com/immerjs/immer/issues/576
+
+// The solution is to tell immer not to create immutable objects
+
 setAutoFreeze(false)
