da1971829aa10b980c11d50c25373d612e3faf1c
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@intelligencia.ai>
2021-01-26 21:22:35 +0200

    Immer set autofreeze to false

--- frontend/imports/immer.js
+++ frontend/imports/immer.js
@@ -1,3 +1,9 @@
-import immer from "https://cdn.jsdelivr.net/npm/immer@7.0.9/dist/immer.esm.js"
+import immer, { setAutoFreeze } from "https://cdn.jsdelivr.net/npm/immer@7.0.9/dist/immer.esm.js"
 
 export default immer
+
+// we have some Editor.setState functions that use immer, so Editor.this.state becomes an "immer immutable frozen object". But we also have some Editor.setState functions that don't use immer, and they try to _mutate_ Editor.this.state. This gives errors like https://github.com/immerjs/immer/issues/576
+
+// The solution is to tell immer not to create immutable objects
+
+setAutoFreeze(false)
