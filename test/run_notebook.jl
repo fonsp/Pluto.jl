8e6f60a67f30c1781b2ab49822a0ca367372cb4f
Fons van der Plas <fonsvdplas@gmail.com>
2021-01-28 15:05:01 +0100

    Update run_notebook.jl

--- test/run_notebook.jl
+++ test/run_notebook.jl
@@ -3,7 +3,17 @@ Pkg.activate(mktempdir())
 Pkg.develop(Pkg.PackageSpec(path=ARGS[1]))
 import Pluto
 s = Pluto.ServerSession()
-nb = Pluto.SessionActions.open_url(s, "https://raw.githubusercontent.com/fonsp/Pluto.jl/v0.12.16/sample/Basic.jl"; run_async=false)
-@show [c.output_repr for c in nb.cells]
-success = !any(c.errored for c in nb.cells)
+urls = [
+    "https://raw.githubusercontent.com/fonsp/Pluto.jl/v0.12.16/sample/Basic.jl",
+    "https://raw.githubusercontent.com/fonsp/Pluto.jl/v0.12.16/sample/Basic.jl",
+    "https://gist.githubusercontent.com/fonsp/4e164a262a60fc4bdd638e124e629d64/raw/8ffe93c680e539056068456a62dea7bf6b8eb622/basic_pkg_notebook.jl",
+]
+success = all(urls) do url
+    @show url
+    nb = Pluto.SessionActions.open_url(s, url; run_async=false)
+    @show [c.output_repr for c in nb.cells]
+    nb_success = !any(c.errored for c in nb.cells)
+    @info "Done" url nb_success
+    nb_success
+end
 exit(success ? 0 : 1)
