844b3b9fbe0ec95d660ec3467c5511a69d0a3846
Benjamin Lungwitz <benj.lung@googlemail.com>
2021-02-08 19:39:23 +0100

    handle missing case

--- src/webserver/Firebasey.jl
+++ src/webserver/Firebasey.jl
@@ -224,7 +224,9 @@ end
 
 # ╔═╡ 5e360fcd-9943-4a17-9672-f1fded2f7e3a
 function diff(old::T, new::T) where T
-	if old == new
+	if old === missing || new === missing
+		JSONPatch[ReplacePatch([], new)]
+	elseif old == new
 		NoChanges
 	else
 		JSONPatch[ReplacePatch([], new)]
