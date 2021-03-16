83560f64f9947c95d0a1c790c1b8b1ceed5a6cd6
Fons van der Plas <fonsvdplas@gmail.com>
2021-03-16 10:40:43 +0100

    add function name so that the stack trace is leesbaar

--- src/webserver/Dynamic.jl
+++ src/webserver/Dynamic.jl
@@ -490,7 +490,7 @@ responses[:write_file] = function (🙋::ClientRequest)
 end
 
 # Third party messages, passing on to handlers inside the PlutoRunner process
-responses[:integrations] = function (🙋::ClientRequest)
+responses[:integrations] = function response_integrations(🙋::ClientRequest)
     @assert (haskey(🙋.body, "module_name")) "Integrations message needs a `module_name` property"
     @assert (haskey(🙋.body, "body")) "Integrations message needs a `body` property"
     
