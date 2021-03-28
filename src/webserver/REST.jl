f69513f8e1b3885c7519dfd8857f5c5cd8420e78
Connor Burns <ctrekker4@gmail.com>
2021-03-28 10:18:14 -0600

    Assume serialization error means function

--- src/webserver/REST.jl
+++ src/webserver/REST.jl
@@ -199,9 +199,9 @@ function Base.getproperty(with_args::PlutoNotebookWithArgs, symbol::Symbol)
     try
         return REST.evaluate(symbol, Base.getfield(Base.getfield(with_args, :notebook), :filename), Base.getfield(Base.getfield(with_args, :notebook), :host); Base.getfield(with_args, :kwargs)...)
     catch e
-        if contains(e.msg, "function") # See if the function error was thrown, and return a PlutoCallable struct
+        # if contains(e.msg, "function") # See if the function error was thrown, and return a PlutoCallable struct
             return PlutoCallable(Base.getfield(with_args, :notebook), symbol)
-        end
+        # end
         throw(e)
     end
 end
