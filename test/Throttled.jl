35816397755be1b1254a5e267210d180d5420553
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@intelligencia.ai>
2021-04-01 21:01:25 +0300

    'Fix' tests

--- test/Throttled.jl
+++ test/Throttled.jl
@@ -10,40 +10,40 @@ import Pluto:throttled
     @test x[] == 1
     
     dt = 1 / 100
-    ft, stop = throttled(f, dt)
+    ft, stop = throttled(f, dt; leading=false, trailing=true)
     
     for x in 1:10
         ft()
     end
-    @test x[] == 2
+    @test x[] == 1 # Update scheduled
     sleep(2dt)
-    @test x[] == 3
+    @test x[] == 2 # Update done
     
     for x in 1:10
         ft()
     end
-    @test x[] == 4
+    @test x[] == 2 # Update scheduled
     sleep(2dt)
-    @test x[] == 5
+    @test x[] == 3 # Update done
     
     
     for x in 1:5
-        ft()
-        sleep(1.5dt)
+        ft() 
+        sleep(1.5dt) # wait to go through
     end
-    @test x[] == 10
-    sleep(2dt)
-    @test x[] == 10
+    @test x[] == 8 # all updates happened
+    sleep(2dt) 
+    @test x[] == 8 # no new updates
     
     ###
     
-    ft()
-    ft()
-    @test x[] == 11
-    stop()
-    @test x[] == 12
-    sleep(2dt)
-    @test x[] == 12
+    ft() # schedule
+    ft() # schedule
+    @test x[] == 8
+    stop() # cancel schedule and run
+    @test x[] == 9
+    sleep(2dt) # cancel works
+    @test x[] == 9
 
     @test false
 end
