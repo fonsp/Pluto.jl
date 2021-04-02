74c93d5a40b8e07cc1fdb07261827062c749f82e
Fons van der Plas <fonsvdplas@gmail.com>
2021-04-02 18:11:21 +0200

    css grid

--- frontend/components/Logs.js
+++ frontend/components/Logs.js
@@ -2,11 +2,14 @@ import { html, useState, useEffect, useLayoutEffect, useRef, useContext } from "
 
 export const Logs = ({ logs }) => {
     return html`
-        <pluto-logs>
-            ${logs.map((log, i) => {
-                return html`<${Dot} level=${log.level} body=${log.msg} x=${i} y=${log.line - 1} /> `
-            })}
-        </pluto-logs>
+        <pluto-logs-container>
+            <pluto-logs>
+                <div style="grid-row: 1 / 20 "></div>
+                ${logs.map((log, i) => {
+                    return html`<${Dot} level=${log.level} body=${log.msg} x=${i} y=${log.line - 1} /> `
+                })}
+            </pluto-logs>
+        </pluto-logs-container>
     `
 }
 
@@ -18,8 +21,10 @@ const Dot = ({ body, x, y, level }) => {
     }, [body])
 
     useLayoutEffect(() => {
-        node_ref.current.style.top = `${17 * y}px`
-        node_ref.current.style.left = `${17 * x}px`
+        // node_ref.current.style.top = `${17 * y}px`
+        // node_ref.current.style.left = `${17 * x}px`
+        node_ref.current.style.gridColumn = `${x + 1}`
+        node_ref.current.style.gridRow = `${y + 1}`
     }, [x, y])
     return html` <pluto-log-dot class=${level} ref=${node_ref}><div ref=${label_ref}></div></pluto-log-dot>`
 }
