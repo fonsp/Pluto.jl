08cdb45486212ad1e14c1948add28026bb706f8c
Fons van der Plas <fonsvdplas@gmail.com>
2021-04-02 13:16:10 +0200

    whitespace

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,20 +1,19 @@
 // mostly based on https://developers.google.com/web/fundamentals/primers/service-workers
 
-const DEBUG = false
-const noop = () => undefined
+const DEBUG = true
+const noop = () => {}
 const logger = {
-    log: DEBUG ? window.console.log : noop,
-    table: DEBUG ? window.console.table : noop,
-    warn: DEBUG ? window.console.warn : noop,
-    info: DEBUG ? window.console.info : noop,
+    log: DEBUG ? console.log : noop,
+    warn: DEBUG ? console.warn : noop,
+    info: DEBUG ? console.info : noop,
 }
 var CACHE_NAME = "pluto-cache-v2"
 
-self.addEventListener("install", function (event) {
-    console.log("Hello from service worker 👋")
+self.addEventListener("install", (event) => {
+    logger.log("Hello from service worker 👋")
     // Perform install steps
     event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
+        caches.open(CACHE_NAME).then((cache) => {
             logger.log("Opened cache")
         })
     )
@@ -27,7 +26,7 @@ function shouldCache(request) {
     return request.method === "GET" && allowList.includes(url.host) && !url.search.includes("skip_sw")
 }
 
-self.addEventListener("fetch", function (event) {
+self.addEventListener("fetch", (event) => {
     // if (navigator.userAgent.includes("Firefox")) {
     //     return
     // }
@@ -36,37 +35,33 @@ self.addEventListener("fetch", function (event) {
         return
     }
     event.respondWith(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.match(event.request).then(function (response) {
-                // Cache hit - return response
-                if (response) {
-                    logger.warn("CACHE HIT", event.request)
+        caches.open(CACHE_NAME).then((cache) =>
+            cache.match(event.request).then((response) => {
+                if (response != null) {
+                    logger.info("CACHE HIT", event.request)
                     return response
+                } else {
+                    logger.warn("Cache miss", event.request.url)
+
+                    return fetch(event.request).then(function (response) {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200) {
+                            return response
+                        } else {
+                            logger.warn("FETCHED")
+
+                            // Clone the response. A response is a stream
+                            // and because we want the browser to consume the response
+                            // as well as the cache consuming the response, we need
+                            // to clone it so we have two streams.
+                            var responseToCache = response.clone()
+                            cache.put(event.request, responseToCache)
+
+                            return response
+                        }
+                    })
                 }
-
-                logger.warn("Cache miss", event.request.url)
-
-                return fetch(event.request).then(function (response) {
-                    // Check if we received a valid response
-                    if (!response || response.status !== 200) {
-                        return response
-                    }
-
-                    // console.warn("FETCHED")
-
-                    // Clone the response. A response is a stream
-                    // and because we want the browser to consume the response
-                    // as well as the cache consuming the response, we need
-                    // to clone it so we have two streams.
-                    var responseToCache = response.clone()
-
-                    // cache.add()
-
-                    cache.put(event.request, responseToCache)
-
-                    return response
-                })
             })
-        })
+        )
     )
 })
