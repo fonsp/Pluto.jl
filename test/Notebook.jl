3f71b9993283be457f563de1acd94af878e18da1
Fons van der Plas <fonsvdplas@gmail.com>
2021-04-16 11:05:18 +0200

    more file extension flexibility

--- test/Notebook.jl
+++ test/Notebook.jl
@@ -211,6 +211,8 @@ end
             write(new_path, join(to_write, '\n'))
             @test_logs (:warn, r"Backup saved to") load_notebook(new_path)
             @test num_backups_in(new_dir) == 2
+
+            @test readdir(new_dir) == ["nb backup 1.jl", "nb backup 2.jl", "nb.jl"]
         end
     end
 
