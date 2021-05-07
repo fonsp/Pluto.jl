d9244689844d38a25fe8f240899366c9d6b16964
Benjamin Lungwitz <benj.lung@googlemail.com>
2021-05-07 19:42:34 +0200

    added some tests for cell disabling backend

---
+++ test/cell_disabling.jl
@@ -0,0 +1,63 @@
+using Test
+using Pluto
+using Pluto: update_run!, ServerSession, ClientSession, Cell, Notebook
+
+@testset "Cell Disabling" begin
+    🍭 = ServerSession()
+    🍭.options.evaluation.workspace_use_distributed = false
+
+    fakeclient = ClientSession(:fake, nothing)
+    🍭.connected_clients[fakeclient.id] = fakeclient
+
+    notebook = Notebook([
+                Cell("""y = begin
+                    2x
+                end"""),
+                Cell("x = 14"),
+                Cell("z = sqrt(y)"),
+                Cell("a = 3x"),
+                Cell("z^3"),
+            ])
+    fakeclient.connected_notebook = notebook
+    update_run!(🍭, notebook, notebook.cells)
+
+    # helper functions
+    id(i) = notebook.cells[i].cell_id
+    get_disabled_cells(notebook) = [i for (i, c) in pairs(notebook.cells) if c.is_indirectly_disabled]
+
+    @test !any(c.is_disabled for c in notebook.cells)
+    @test !any(c.is_indirectly_disabled for c in notebook.cells)
+
+    # disable first cell
+    notebook.cells[1].is_disabled = true
+    update_run!(🍭, notebook, notebook.cells)
+    should_be_disabled = [1, 3, 5]
+    @test get_disabled_cells(notebook) == should_be_disabled
+
+    # disable root cell
+    notebook.cells[2].is_disabled = true
+    update_run!(🍭, notebook, notebook.cells)
+    @test get_disabled_cells(notebook) == collect(1:length(notebook.cells))
+
+    # reactivate first cell - still all cells should be is_disabled
+    notebook.cells[1].is_disabled = false
+    update_run!(🍭, notebook, notebook.cells)
+    @test get_disabled_cells(notebook) == collect(1:length(notebook.cells))
+
+    # reactivate root cell
+    notebook.cells[2].is_disabled = false
+    update_run!(🍭, notebook, notebook.cells)
+    @test get_disabled_cells(notebook) == []
+
+    # disable first cell again
+    notebook.cells[1].is_disabled = true
+    update_run!(🍭, notebook, notebook.cells)
+    should_be_disabled = [1, 3, 5]
+    @test get_disabled_cells(notebook) == should_be_disabled
+
+    # and reactivate it
+    notebook.cells[1].is_disabled = false
+    update_run!(🍭, notebook, notebook.cells)
+    @test get_disabled_cells(notebook) == []
+
+end
