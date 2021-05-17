e23589e93fa6dc33299bf2f0e681b88536574cb4
Fons van der Plas <fons@plutojl.org>
2021-05-17 17:06:00 +0200

    iframe allow all

--- frontend/components/CellOutput.js
+++ frontend/components/CellOutput.js
@@ -179,7 +179,14 @@ let IframeContainer = ({ body }) => {
         return () => URL.revokeObjectURL(url)
     }, [body])
 
-    return html`<iframe style=${{ width: "100%", border: "none" }} src="" ref=${iframeref}></div>`
+    return html`<iframe
+        style=${{ width: "100%", border: "none" }}
+        src=""
+        ref=${iframeref}
+        frameborder="0"
+        allow="accelerometer; ambient-light-sensor; autoplay; battery; camera; display-capture; document-domain; encrypted-media; execution-while-not-rendered; execution-while-out-of-viewport; fullscreen; geolocation; gyroscope; layout-animations; legacy-image-formats; magnetometer; microphone; midi; navigation-override; oversized-images; payment; picture-in-picture; publickey-credentials-get; sync-xhr; usb; wake-lock; screen-wake-lock; vr; web-share; xr-spatial-tracking"
+        allowfullscreen
+    ></iframe>`
 }
 
 /**
