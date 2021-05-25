875c8f0d27d2363ed28dd08b47024aa5146e077c
Fons van der Plas <fons@plutojl.org>
2021-05-25 14:16:47 +0200

    Add tests

--- test/cell_disabling.jl
+++ test/cell_disabling.jl
@@ -11,12 +11,12 @@ using Pluto: update_run!, ServerSession, ClientSession, Cell, Notebook
 
     notebook = Notebook([
                 Cell("""y = begin
-                    2x
+                    1 + x
                 end"""),
-                Cell("x = 14"),
+                Cell("x = 2"),
                 Cell("z = sqrt(y)"),
-                Cell("a = 3x"),
-                Cell("z^3"),
+                Cell("a = 4x"),
+                Cell("w = z^5"),
             ])
     fakeclient.connected_notebook = notebook
     update_run!(🍭, notebook, notebook.cells)
@@ -34,6 +34,25 @@ using Pluto: update_run!, ServerSession, ClientSession, Cell, Notebook
     should_be_disabled = [1, 3, 5]
     @test get_disabled_cells(notebook) == should_be_disabled
 
+    # change x, this change should not propagate through y
+    original_y_output = notebook.cells[1].output.body
+    original_z_output = notebook.cells[3].output.body
+    original_a_output = notebook.cells[4].output.body
+    original_w_output = notebook.cells[5].output.body
+    setcode(notebook.cells[2], "x = 123123")
+    update_run!(🍭, notebook, notebook.cells[2])
+    @test notebook.cells[1].output.body == original_y_output
+    @test notebook.cells[3].output.body == original_z_output
+    @test notebook.cells[4].output.body != original_a_output
+    @test notebook.cells[5].output.body == original_w_output
+
+    setcode(notebook.cells[2], "x = 2")
+    update_run!(🍭, notebook, notebook.cells[2])
+    @test notebook.cells[1].output.body == original_y_output
+    @test notebook.cells[3].output.body == original_z_output
+    @test notebook.cells[4].output.body == original_a_output
+    @test notebook.cells[5].output.body == original_w_output
+
     # disable root cell
     notebook.cells[2].running_disabled = true
     update_run!(🍭, notebook, notebook.cells)
@@ -44,10 +63,24 @@ using Pluto: update_run!, ServerSession, ClientSession, Cell, Notebook
     update_run!(🍭, notebook, notebook.cells)
     @test get_disabled_cells(notebook) == collect(1:length(notebook.cells))
 
+    # the x cell is disabled, so changing it should have no effect
+    setcode(notebook.cells[2], "x = 123123")
+    update_run!(🍭, notebook, notebook.cells[2])
+    @test notebook.cells[1].output.body == original_y_output
+    @test notebook.cells[3].output.body == original_z_output
+    @test notebook.cells[4].output.body == original_a_output
+    @test notebook.cells[5].output.body == original_w_output
+
     # reactivate root cell
     notebook.cells[2].running_disabled = false
     update_run!(🍭, notebook, notebook.cells)
     @test get_disabled_cells(notebook) == []
+
+
+    @test notebook.cells[1].output.body != original_y_output
+    @test notebook.cells[3].output.body != original_z_output
+    @test notebook.cells[4].output.body != original_a_output
+    @test notebook.cells[5].output.body != original_w_output
 
     # disable first cell again
     notebook.cells[1].running_disabled = true
