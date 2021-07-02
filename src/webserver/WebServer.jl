421df2ff6765eca17180d7e822f431dacec57265
Jack Shannon <j.b.shannon@lse.ac.uk>
2021-07-03 00:35:20 +0100

    removed open_in_default_browser

--- src/webserver/WebServer.jl
+++ src/webserver/WebServer.jl
@@ -19,25 +19,6 @@ function detectwsl()
     occursin(r"Microsoft|WSL"i, read("/proc/sys/kernel/osrelease", String))
 end
 
-function open_in_default_browser(url::AbstractString)::Bool
-    try
-        if Sys.isapple()
-            Base.run(`open $url`)
-            true
-        elseif Sys.iswindows() || detectwsl()
-            Base.run(`powershell.exe Start "'$url'"`)
-            true
-        elseif Sys.islinux()
-            Base.run(`xdg-open $url`)
-            true
-        else
-            false
-        end
-    catch ex
-        false
-    end
-end
-
 isurl(s::String) = startswith(s, "http://") || startswith(s, "https://")
 
 swallow_exception(f, exception_type::Type{T}) where T =
