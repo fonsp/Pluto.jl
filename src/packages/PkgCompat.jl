163b7ce199c97cea95df7776fe5740300c57b669
Fons van der Plas <fonsvdplas@gmail.com>
2021-07-06 21:13:29 +0200

    Some Pkg tweaks (#1292)

--- src/packages/PkgCompat.jl
+++ src/packages/PkgCompat.jl
@@ -323,15 +323,17 @@ function dependencies(ctx)
 				Pkg.dependencies(ctx.env)
 			end
 		catch e
-			@error """
-			Pkg error: you might need to use
+			if !occursin(r"expected.*exist.*manifest", sprint(showerror, e))
+				@error """
+				Pkg error: you might need to use
 
-			Pluto.reset_notebook_environment(notebook_path)
+				Pluto.reset_notebook_environment(notebook_path)
 
-			to reset this notebook's environment.
+				to reset this notebook's environment.
 
-			Before doing so, consider sending your notebook file to https://github.com/fonsp/Pluto.jl/issues together with the following info:
-			""" Pluto.PLUTO_VERSION VERSION exception=(e,catch_backtrace())
+				Before doing so, consider sending your notebook file to https://github.com/fonsp/Pluto.jl/issues together with the following info:
+				""" Pluto.PLUTO_VERSION VERSION exception=(e,catch_backtrace())
+			end
 
 			Dict()
 		end
@@ -368,6 +370,11 @@ end
 # WRITING COMPAT ENTRIES
 ###
 
+
+_project_key_order = ["name", "uuid", "keywords", "license", "desc", "deps", "compat"]
+project_key_order(key::String) =
+    something(findfirst(x -> x == key, _project_key_order), length(_project_key_order) + 1)
+
 # ✅ Public API
 function _modify_compat(f!::Function, ctx::PkgContext)::PkgContext
 	toml = Pkg.TOML.parsefile(project_file(ctx))
@@ -378,7 +385,7 @@ function _modify_compat(f!::Function, ctx::PkgContext)::PkgContext
 	isempty(compat) && delete!(toml, "compat")
 
 	write(project_file(ctx), sprint() do io
-		Pkg.TOML.print(io, toml; sorted=true)
+		Pkg.TOML.print(io, toml; sorted=true, by=(key -> (project_key_order(key), key)))
 	end)
 	
 	return load_ctx(ctx)
