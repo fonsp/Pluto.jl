762ef90b353bd817d834f1ff54d76f4b8198e7b5
Alexander <alexander@plav.in>
2021-07-15 10:03:56 +0300

    fix https://github.com/fonsp/Pluto.jl/issues/1322

--- src/packages/PkgCompat.jl
+++ src/packages/PkgCompat.jl
@@ -404,7 +404,7 @@ function write_auto_compat_entries(ctx::PkgContext)::PkgContext
 			if !haskey(compat, p)
 				m_version = get_manifest_version(ctx, p)
 				if m_version !== nothing && !is_stdlib(p)
-					compat[p] = "~" * string(m_version)
+					compat[p] = "~" * string(Base.thispatch(m_version))  # drop build number
 				end
 			end
 		end
