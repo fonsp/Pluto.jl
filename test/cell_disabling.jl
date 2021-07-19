891b110e17bd83c275e4454b0a5f4247f81e197b
Benjamin Lungwitz <benj.lung@googlemail.com>
2021-07-19 20:30:21 +0200

    add tests that notebook is executable

--- test/cell_disabling.jl
+++ test/cell_disabling.jl
@@ -1,6 +1,6 @@
 using Test
 using Pluto
-using Pluto: update_run!, ServerSession, ClientSession, Cell, Notebook
+using Pluto: update_run!, ServerSession, ClientSession, Cell, Notebook, save_notebook
 
 @testset "Cell Disabling" begin
     🍭 = ServerSession()
@@ -22,6 +22,8 @@ using Pluto: update_run!, ServerSession, ClientSession, Cell, Notebook
     fakeclient.connected_notebook = notebook
     update_run!(🍭, notebook, notebook.cells)
 
+    notebook_file = tempname()
+
     # helper functions
     id(i) = notebook.cells[i].cell_id
     get_disabled_cells(notebook) = [i for (i, c) in pairs(notebook.cells) if c.cell_dependencies.depends_on_disabled_cells[]]
@@ -29,11 +31,16 @@ using Pluto: update_run!, ServerSession, ClientSession, Cell, Notebook
     @test !any(c.running_disabled for c in notebook.cells)
     @test !any(c.cell_dependencies.depends_on_disabled_cells[] for c in notebook.cells)
 
+    save_notebook(notebook, notebook_file)
+    @test jl_is_runnable(notebook_file)
+
     # disable first cell
     notebook.cells[1].running_disabled = true
     update_run!(🍭, notebook, notebook.cells)
     should_be_disabled = [1, 3, 5]
     @test get_disabled_cells(notebook) == should_be_disabled
+    save_notebook(notebook, notebook_file)
+    @test jl_is_runnable(notebook_file)
 
     # change x, this change should not propagate through y
     original_y_output = notebook.cells[1].output.body
@@ -46,6 +53,8 @@ using Pluto: update_run!, ServerSession, ClientSession, Cell, Notebook
     @test notebook.cells[3].output.body == original_z_output
     @test notebook.cells[4].output.body != original_a_output
     @test notebook.cells[5].output.body == original_w_output
+    save_notebook(notebook, notebook_file)
+    @test jl_is_runnable(notebook_file)
 
     setcode(notebook.cells[2], "x = 2")
     update_run!(🍭, notebook, notebook.cells[2])
@@ -53,12 +62,15 @@ using Pluto: update_run!, ServerSession, ClientSession, Cell, Notebook
     @test notebook.cells[3].output.body == original_z_output
     @test notebook.cells[4].output.body == original_a_output
     @test notebook.cells[5].output.body == original_w_output
+    save_notebook(notebook, notebook_file)
+    @test jl_is_runnable(notebook_file)
 
     # disable root cell
     notebook.cells[2].running_disabled = true
     update_run!(🍭, notebook, notebook.cells)
     @test get_disabled_cells(notebook) == collect(1:5)
-
+    save_notebook(notebook, notebook_file)
+    @test jl_is_runnable(notebook_file)
 
     original_6_output = notebook.cells[6].output.body
     setcode(notebook.cells[6], "x + 6")
@@ -66,11 +78,15 @@ using Pluto: update_run!, ServerSession, ClientSession, Cell, Notebook
     @test notebook.cells[6].cell_dependencies.depends_on_disabled_cells[]
     @test notebook.cells[6].errored === false
     @test notebook.cells[6].output.body == original_6_output
+    save_notebook(notebook, notebook_file)
+    @test jl_is_runnable(notebook_file)
 
     # reactivate first cell - still all cells should be running_disabled
     notebook.cells[1].running_disabled = false
     update_run!(🍭, notebook, notebook.cells)
     @test get_disabled_cells(notebook) == collect(1:6)
+    save_notebook(notebook, notebook_file)
+    @test jl_is_runnable(notebook_file)
 
     # the x cell is disabled, so changing it should have no effect
     setcode(notebook.cells[2], "x = 123123")
@@ -79,27 +95,34 @@ using Pluto: update_run!, ServerSession, ClientSession, Cell, Notebook
     @test notebook.cells[3].output.body == original_z_output
     @test notebook.cells[4].output.body == original_a_output
     @test notebook.cells[5].output.body == original_w_output
+    save_notebook(notebook, notebook_file)
+    @test jl_is_runnable(notebook_file)
 
     # reactivate root cell
     notebook.cells[2].running_disabled = false
     update_run!(🍭, notebook, notebook.cells)
     @test get_disabled_cells(notebook) == []
 
-
     @test notebook.cells[1].output.body != original_y_output
     @test notebook.cells[3].output.body != original_z_output
     @test notebook.cells[4].output.body != original_a_output
     @test notebook.cells[5].output.body != original_w_output
+    save_notebook(notebook, notebook_file)
+    @test jl_is_runnable(notebook_file)
 
     # disable first cell again
     notebook.cells[1].running_disabled = true
     update_run!(🍭, notebook, notebook.cells)
     should_be_disabled = [1, 3, 5]
     @test get_disabled_cells(notebook) == should_be_disabled
+    save_notebook(notebook, notebook_file)
+    @test jl_is_runnable(notebook_file)
 
     # and reactivate it
     notebook.cells[1].running_disabled = false
     update_run!(🍭, notebook, notebook.cells)
     @test get_disabled_cells(notebook) == []
+    save_notebook(notebook, notebook_file)
+    @test jl_is_runnable(notebook_file)
 
 end
