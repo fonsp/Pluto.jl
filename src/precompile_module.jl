8f624150fbf1e3d7aa6e9969ee0b767138d174b0
Benjamin Lungwitz <benj.lung@googlemail.com>
2021-08-22 20:18:54 +0200

    more precompilations

--- src/precompile_module.jl
+++ src/precompile_module.jl
@@ -1,7 +1,8 @@
 
 let
-    session = ServerSession()
-    session.options.evaluation.workspace_use_distributed = false
+    config = Configuration.from_flat_kwargs(; workspace_use_distributed=false)
+    session = ServerSession(;options=config)
+    http_router_for(session)
 
     fakeclient = ClientSession(:fake, nothing)
     session.connected_clients[fakeclient.id] = fakeclient
@@ -24,7 +25,10 @@ let
     old = notebook.topology
     new = notebook.topology = updated_topology(old, notebook, cells) # macros are not yet resolved
 	update_dependency_cache!(notebook)
+    save_notebook(notebook, tempname())
 
     notebook_to_js(notebook) # export to JS
 
+    client_request = Pluto.ClientRequest(session=session, notebook=notebook)
+    send_notebook_changes!(client_request)
 end
