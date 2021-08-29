43077f42faa4373b2d1cf2ea9bf97540633432d9
uPvtkBlpiucvDUf <>
2021-08-29 20:28:17 +0200

    Simpler version of checkScriptIntegrity using fetch()

--- frontend/imports/check-integrity.js
+++ frontend/imports/check-integrity.js
@@ -1,24 +1,5 @@
 export default async function checkScriptIntegrity(file, integrity)
 {
-    const iframe = document.createElement('iframe');
-    iframe.src = 'javascript:void(0)';
-    iframe.style.display = 'none';
-    document.body.appendChild(iframe);
-    const doc = iframe.contentDocument;
-    const script = doc.createElement('script');
-    script.type = 'module';
-    script.integrity = integrity;
-    script.crossOrigin = 'anonymous';
-    script.src = file;
-    const promise = new Promise((resolve, reject) => {
-        script.onload = resolve;
-        script.onerror = reject;
-    });
-
-    doc.head.append(script);
-
-    return await promise.then(() => {
-        iframe.parentNode.removeChild(iframe);
-        return true
-    });
+    await fetch(file, {integrity: integrity});
+    return true
 }
