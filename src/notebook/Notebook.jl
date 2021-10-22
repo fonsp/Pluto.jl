b5af2df3ae326e5bad4bac5f4731851f037f4f3e
Fons van der Plas <fonsvdplas@gmail.com>
2021-10-22 18:07:47 +0200

    Update Notebook.jl

--- src/notebook/Notebook.jl
+++ src/notebook/Notebook.jl
@@ -9,7 +9,7 @@ mutable struct BondValue
     # This is only so the client can send this, the updater will always put this to `false`
     is_first_value::Bool
 end
-function Base.convert(::Type{BondValue}, dict::Dict)
+function Base.convert(::Type{BondValue}, dict::AbstractDict)
     BondValue(dict["value"], something(get(dict, "is_first_value", false), false))
 end
 
