24c7ffadd5d880f7df57a2984ed52d6bff473a0b
Michiel Dral <m.c.dral@gmail.com>
2021-10-27 20:36:12 +0200

    HAHAHAHA

--- src/webserver/Static.jl
+++ src/webserver/Static.jl
@@ -13,7 +13,6 @@ function mime_fromfilename(filename)
 end
 
 function asset_response(path)
-    @info "PATH"
     if !isfile(path) && !endswith(path, ".html")
         return asset_response(path * ".html")
     end
