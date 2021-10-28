0a8f57475e4da708e0f4319bfe154c4a16c17ad1
Fons van der Plas <fonsvdplas@gmail.com>
2021-10-28 16:35:17 +0200

    Separate tests in three runs

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,27 +1,41 @@
 include("./helpers.jl")
 
+
+const test_id = Ref(0)
+function forme()
+    num_test_runners = parse(Int, get(ENV, "PLUTO_TEST_NUM_RUNNERS", "1"))
+    my_id = parse(Int, get(ENV, "PLUTO_TEST_RUNNER_ID", "0"))
+    
+    
+    should_run = (my_id % num_test_runners) == (test_id[] % num_test_runners)
+    
+    test_id[] += 1
+    should_run
+end
+
+
 # tests that start new processes:
-include("./WorkspaceManager.jl")
-include("./packages/Basic.jl")
-VERSION > v"1.6.99" || include("./RichOutput.jl")
-include("./React.jl")
-include("./Dynamic.jl")
-include("./MacroAnalysis.jl")
+forme() && include("./WorkspaceManager.jl")
+forme() && include("./packages/Basic.jl")
+forme() && (VERSION < v"1.7.0-a" && include("./RichOutput.jl"))
+forme() && include("./React.jl")
+forme() && include("./Dynamic.jl")
+forme() && include("./MacroAnalysis.jl")
 
 # for SOME reason 😞 the Notebook.jl tests need to run AFTER all the tests above, or the Github Actions runner on Windows gets internal julia errors.
-include("./Notebook.jl")
+forme() && include("./Notebook.jl")
 
 # tests that don't start new processes:
-include("./ReloadFromFile.jl")
-include("./packages/PkgCompat.jl")
-include("./ExpressionExplorer.jl")
-include("./MethodSignatures.jl")
-VERSION > v"1.6.99" || include("./Configuration.jl")
-include("./Analysis.jl")
-include("./Firebasey.jl")
-include("./DependencyCache.jl")
-include("./Throttled.jl")
-include("./cell_disabling.jl")
+forme() && include("./ReloadFromFile.jl")
+forme() && include("./packages/PkgCompat.jl")
+forme() && include("./ExpressionExplorer.jl")
+forme() && include("./MethodSignatures.jl")
+forme() && (VERSION < v"1.7.0-a" && include("./Configuration.jl"))
+forme() && include("./Analysis.jl")
+forme() && include("./Firebasey.jl")
+forme() && include("./DependencyCache.jl")
+forme() && include("./Throttled.jl")
+forme() && include("./cell_disabling.jl")
 
 # TODO: test PlutoRunner functions like:
 # - from_this_notebook
