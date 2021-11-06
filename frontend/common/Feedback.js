30ab98110a07b6649f2ec913a97e7fda058354c3
Michiel Dral <m.c.dral@gmail.com>
2021-11-06 03:01:48 +0000

    Just madly in love with this idea of only loading firebase when completely necessary

--- frontend/common/Feedback.js
+++ frontend/common/Feedback.js
@@ -1,58 +1,85 @@
 import { timeout_promise } from "./PlutoConnection.js"
 
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js"
+// Sorry Fons, even this part of the code is now unnessarily overengineered.
+// But at least, I overengineered this on purpose. - DRAL
 
-// // Add Firebase products that you want to use
-// import { auth } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-auth.js"
-// import { firestore } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js"
+let async = async (async) => async()
 
-const feedbackdb = {
-    instance: null,
-}
+let firebase_load_promise = null
 const init_firebase = async () => {
-    // @ts-ignore
-    firebase.initializeApp({
-        apiKey: "AIzaSyC0DqEcaM8AZ6cvApXuNcNU2RgZZOj7F68",
-        authDomain: "localhost",
-        projectId: "pluto-feedback",
-    })
-    // @ts-ignore
-    feedbackdb.instance = firebase.firestore()
+    if (firebase_load_promise == null) {
+        firebase_load_promise = async(async () => {
+            let [{ initializeApp }, firestore_module] = await Promise.all([
+                // @ts-ignore
+                import("https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js"),
+                // @ts-ignore
+                import("https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js"),
+            ])
+            let { getFirestore, addDoc, doc, collection } = firestore_module
+
+            // @ts-ignore
+            let app = initializeApp({
+                apiKey: "AIzaSyC0DqEcaM8AZ6cvApXuNcNU2RgZZOj7F68",
+                authDomain: "localhost",
+                projectId: "pluto-feedback",
+            })
+
+            let db = getFirestore(app)
+            let feedback_db = collection(db, "feedback")
+
+            let add_feedback = async (feedback) => {
+                await addDoc(feedback_db, feedback)
+            }
+
+            console.log("🔥base loaded")
+
+            // @ts-ignore
+            return add_feedback
+        })
+    }
+    return await firebase_load_promise
 }
 
 export const init_feedback = async () => {
     try {
-        await init_firebase()
-
+        // Only load firebase when the feedback form is touched
         const feedbackform = document.querySelector("form#feedback")
         feedbackform.addEventListener("submit", (e) => {
             const email = prompt("Would you like us to contact you?\n\nEmail: (leave blank to stay anonymous 👀)")
 
-            timeout_promise(
-                feedbackdb.instance.collection("feedback").add({
-                    // @ts-ignore
-                    feedback: new FormData(e.target).get("opinion"),
-                    // @ts-ignore
-                    timestamp: firebase.firestore.Timestamp.now(),
-                    email: email ? email : "",
-                }),
-                5000
-            )
-                .then(() => {
+            e.preventDefault()
+
+            async(async () => {
+                try {
+                    let add_feedback = await init_firebase()
+                    await timeout_promise(
+                        add_feedback({
+                            // @ts-ignore
+                            feedback: new FormData(e.target).get("opinion"),
+                            // @ts-ignore
+                            timestamp: Date.now(),
+                            email: email ? email : "",
+                        }),
+                        5000
+                    )
                     let message = "Submitted. Thank you for your feedback! 💕"
                     console.log(message)
                     alert(message)
                     // @ts-ignore
                     feedbackform.querySelector("#opinion").value = ""
-                })
-                .catch((error) => {
+                } catch (error) {
                     let message =
                         "Whoops, failed to send feedback 😢\nWe would really like to hear from you! Please got to https://github.com/fonsp/Pluto.jl/issues to report this failure:\n\n"
                     console.error(message)
                     console.error(error)
                     alert(message + error)
-                })
-            e.preventDefault()
+                }
+            })
+        })
+
+        feedbackform.addEventListener("focusin", () => {
+            // Start loading firebase when someone interacts with the form
+            init_firebase()
         })
     } catch (error) {
         console.error("Something went wrong loading the feedback form:", error)
