50945588582b770b40b21c4d30795cfdadf88e73
Fons van der Plas <fonsvdplas@gmail.com>
2021-12-07 15:36:54 +0100

    Fix fake audio duration

--- frontend/components/AudioPlayer.js
+++ frontend/components/AudioPlayer.js
@@ -8,8 +8,13 @@ export const AudioPlayer = ({ onPlay, src, loaded_recording, audio_element_ref }
     useLayoutEffect(() => {
         run(async () => {
             if (src == null) {
-                console.log(Math.ceil(_.last((await loaded_recording).steps)[0] + 0.1))
-                let fake_source = createSilentAudio(Math.ceil(_.last((await loaded_recording).steps)[0] + 0.1))
+                // We create a silent audio track to play. The duration is the last timestamp of the loaded recording state.
+                let last_timestamp = (things) => _.last([[0, null], ...things])[0]
+                let fake_duration = Math.max(last_timestamp((await loaded_recording).scrolls), last_timestamp((await loaded_recording).steps))
+                fake_duration = Math.ceil(fake_duration + 0.1)
+                console.log({ fake_duration })
+
+                let fake_source = createSilentAudio(fake_duration)
                 audio_element_ref.current.src = fake_source
             } else {
                 audio_element_ref.current.src = src
