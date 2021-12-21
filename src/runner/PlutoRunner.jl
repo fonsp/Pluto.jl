d8f03781f39eaac0b9df19f25899b736eb5d738c
Fons van der Plas <fonsvdplas@gmail.com>
2021-12-21 21:21:34 +0100

    Render `embed_display` directly if it will show as HTML (#1776)

--- src/runner/PlutoRunner.jl
+++ src/runner/PlutoRunner.jl
@@ -1869,17 +1869,21 @@ end
 assertpackable(t::Tuple) = foreach(assertpackable, t)
 assertpackable(t::NamedTuple) = foreach(assertpackable, t)
 
+const _EmbeddableDisplay_enable_html_shortcut = Ref{Bool}(true)
+
 struct EmbeddableDisplay
     x
-    script_id
+    script_id::String
 end
 
 function Base.show(io::IO, m::MIME"text/html", e::EmbeddableDisplay)
     body, mime = format_output_default(e.x, io)
 	
-    write(io, """
-    <pluto-display></pluto-display>
-    <script id=$(e.script_id)>
+    to_write = if mime === m && _EmbeddableDisplay_enable_html_shortcut[]
+        # In this case, we can just embed the HTML content directly.
+        body
+    else
+        """<pluto-display></pluto-display><script id=$(e.script_id)>
 
         // see https://plutocon2021-demos.netlify.app/fonsp%20%E2%80%94%20javascript%20inside%20pluto to learn about the techniques used in this script
         
@@ -1900,8 +1904,9 @@ function Base.show(io::IO, m::MIME"text/html", e::EmbeddableDisplay)
         }
         return display
 
-    </script>
-	""")
+        </script>"""
+    end
+    write(io, to_write)
 end
 
 export embed_display
