f184edea4d9ff552ffc222b8f66b66d719f99913
Fons van der Plas <fonsvdplas@gmail.com>
2022-01-20 01:00:41 +0100

    macro color

--- frontend/components/CellInput.js
+++ frontend/components/CellInput.js
@@ -64,7 +64,7 @@ export const pluto_syntax_colors = HighlightStyle.define([
     { tag: tags.propertyName, color: "var(--cm-def-color)" },
     { tag: tags.unit, color: "var(--cm-tag-color)" },
     { tag: tags.literal, color: "var(--cm-builtin-color)", fontWeight: 700 },
-    { tag: tags.macroName, color: "var(--cm-var-color)", fontWeight: 700 },
+    { tag: tags.macroName, color: "var(--cm-macro-color)" },
     // `nothing` I guess... Any others?
     {
         tag: tags.standard(tags.variableName),
