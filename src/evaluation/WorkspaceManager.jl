e1550a619b103541a0c314a930189163a03b5678
Mike <michaelhatherly@gmail.com>
2022-01-25 18:27:53 +0000

    Fix #1852, relocatable changes for sysimage usage.
    Uses `RelocatableFolders` to store all files that may be accessed
    at runtime rather than baking in their file paths, which won't
    point to valid file paths once a system image is created containing
    `Pluto` and then moved to a different machine/system.

--- src/evaluation/WorkspaceManager.jl
+++ src/evaluation/WorkspaceManager.jl
@@ -22,9 +22,9 @@ Base.@kwdef mutable struct Workspace
 end
 
 "These expressions get evaluated whenever a new `Workspace` process is created."
-const process_preamble = quote
+process_preamble() = quote
     ccall(:jl_exit_on_sigint, Cvoid, (Cint,), 0)
-    include($(project_relative_path("src", "runner", "Loader.jl")))
+    include($(project_relative_path(joinpath("src", "runner"), "Loader.jl")))
     ENV["GKSwstype"] = "nul"
     ENV["JULIA_REVISE_WORKER_ONLY"] = "1"
 end
@@ -217,7 +217,7 @@ function create_workspaceprocess(;compiler_options=CompilerOptions())::Integer
         $(Distributed_expr).addprocs(1; exeflags=$(_convert_to_flags(compiler_options))) |> first
     end)
 
-    Distributed.remotecall_eval(Main, [pid], process_preamble)
+    Distributed.remotecall_eval(Main, [pid], process_preamble())
 
     # so that we NEVER break the workspace with an interrupt 🤕
     @async Distributed.remotecall_eval(Main, [pid],
