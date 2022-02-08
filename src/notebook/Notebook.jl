15ccc91441d4126aa6b35228a9ff8b22ba226d32
Benjamin Lungwitz <benj.lung@googlemail.com>
2022-02-08 19:27:14 +0000

    load disabled status of notebooks

--- src/notebook/Notebook.jl
+++ src/notebook/Notebook.jl
@@ -91,8 +91,8 @@ const _order_delimiter = "# ╠═"
 const _order_delimiter_folded = "# ╟─"
 const _cell_suffix = "\n\n"
 
-const _disabled_prefix =               "#=╠═╡\n"
-const _disabled_suffix =             "\n  ╠═╡ =#"
+const _disabled_prefix = "#=╠═╡\n"
+const _disabled_suffix = "\n  ╠═╡ =#"
 
 const _ptoml_cell_id = UUID(1)
 const _mtoml_cell_id = UUID(2)
@@ -247,7 +247,13 @@ function load_notebook_nobackup(io, path)::Notebook
             # remove the cell suffix
             code = code_normalised[1:prevind(code_normalised, end, length(_cell_suffix))]
 
-            read_cell = Cell(cell_id, code)
+            # parse metadata
+            metadata = TOML.parse(join(metadata_toml_lines, "\n"))
+            cell_disabling_status = pop!(metadata, "disabled", "") # the disabling metadata is not part of the "normal" metadata but stored in separate fields due to its importance in notebook evaluation
+            running_disabled = cell_disabling_status == "directly"
+            depends_on_disabled_cells = cell_disabling_status == "indirectly"
+
+            read_cell = Cell(; cell_id, code, running_disabled, depends_on_disabled_cells, metadata)
             collected_cells[cell_id] = read_cell
         end
     end
