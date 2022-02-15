cfb667fa4c7b9d421d764c7a3766854515bb7824
Fons van der Plas <fonsvdplas@gmail.com>
2022-02-15 01:30:35 +0100

    Update HijackExternalLinksToOpenInNewTab.js

--- frontend/components/HackySideStuff/HijackExternalLinksToOpenInNewTab.js
+++ frontend/components/HackySideStuff/HijackExternalLinksToOpenInNewTab.js
@@ -3,7 +3,10 @@ import { useEffect } from "../../imports/Preact.js"
 /**
  * Time flies when you're building Pluto...
  * At one moment you self-assignee to issue number #1, next moment we're approaching issue #2000...
- *
+ * 
+ * We can't just put `<base target="black">` in the `<head>`, because this also opens hash links
+ * like `#id` in a new tab...
+ * 
  * This components takes every click event on an <a> that points to another origin (i.e. not `#id`)
  * and sneakily puts in a `target="_blank"` attribute so it opens in a new tab.
  *
