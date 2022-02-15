0643cc7ae8746782a1f56a0ced87ff7689f11900
Fons van der Plas <fonsvdplas@gmail.com>
2022-02-15 15:35:05 +0100

    Update editor.js

--- frontend/editor.js
+++ frontend/editor.js
@@ -1,6 +1,6 @@
 import { html, render } from "./imports/Preact.js"
 import "./common/NodejsCompatibilityPolyfill.js"
-//
+
 import { Editor } from "./components/Editor.js"
 
 // it's like a Rube Goldberg machine
