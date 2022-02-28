c3397b60c3fa63a61e8a5378d9ae69739f5aa00d
rikhuijzer <rikhuijzer@pm.me>
2022-02-28 13:49:26 +0100

    Add benchmark for compile time

---
+++ perf/compiletimes.jl
@@ -0,0 +1,49 @@
+using Pkg
+const PKGDIR = dirname(@__DIR__)
+Pkg.activate(PKGDIR)
+
+import Base
+
+const MODE = get(ENV, "PLUTO_COMPILE_TIMES_MODE", "with_precompile_directives")
+
+if MODE == "without_precompile_directives"
+    # Disable precompilation by overriding the method.
+    function Base.precompile(@nospecialize(f), args::Tuple)
+        return nothing
+    end
+    @assert precompile(sum, ()) == nothing
+end
+
+println("\nPrecompile:")
+@time Pkg.precompile()
+
+println("\nUsing Pluto:")
+@time using Pluto
+
+function wait_for_ready(nb::Pluto.Notebook)
+    while nb.process_status != Pluto.ProcessStatus.ready
+        sleep(0.1)
+    end
+end
+
+println("\nPluto.ServerSession():")
+@time session = Pluto.ServerSession()
+
+println("\nSessionActions.open:")
+@time nb = let
+    path = joinpath(PKGDIR, "sample", "Basic.jl")
+    Pluto.SessionActions.open(session, path; run_async=true)
+end
+
+wait_for_ready(nb)
+
+println("\nSessionActions.shutdown:")
+@time Pluto.SessionActions.shutdown(session, nb; async=true)
+
+# Let the shutdown complete.
+sleep(10)
+
+println("\nSessionActions.new:")
+@time nb = Pluto.SessionActions.new(session; run_async=true)
+
+wait_for_ready(nb)
--- sample/Basic.jl
+++ sample/Basic.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.14.0
+# v0.18.1
 
 using Markdown
 using InteractiveUtils
@@ -30,8 +30,25 @@ seq = n .^ -2
 # ╔═╡ b2d792c2-7f73-11ea-0c65-a5042701e9f3
 sqrt(sum(seq) * 6.0)
 
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
 # ╔═╡ Cell order:
 # ╟─b2d786ec-7f73-11ea-1a0c-f38d7b6bbc1e
 # ╠═b2d792c2-7f73-11ea-0c65-a5042701e9f3
 # ╠═b2d79330-7f73-11ea-0d1c-a9aad1efaae1
 # ╠═b2d79376-7f73-11ea-2dce-cb9c449eece6
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
