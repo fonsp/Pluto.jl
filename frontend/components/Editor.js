23afa24c3fda3474104cc9526380edda0922f3b6
Fons van der Plas <fonsvdplas@gmail.com>
2022-03-11 15:17:46 +0100

    Move initial state download out of Editor.js

--- frontend/components/Editor.js
+++ frontend/components/Editor.js
@@ -3,6 +3,7 @@ import * as preact from "../imports/Preact.js"
 import immer, { applyPatches, produceWithPatches } from "../imports/immer.js"
 import _ from "../imports/lodash.js"
 
+import { empty_notebook_state, set_disable_ui_css } from "../editor.js"
 import { create_pluto_connection } from "../common/PlutoConnection.js"
 import { init_feedback } from "../common/Feedback.js"
 import { serialize_cells, deserialize_cells, detect_deserializer } from "../common/Serialization.js"
@@ -21,13 +22,9 @@ import { Popup } from "./Popup.js"
 
 import { slice_utf8, length_utf8 } from "../common/UnicodeTools.js"
 import { has_ctrl_or_cmd_pressed, ctrl_or_cmd_name, is_mac_keyboard, in_textarea_or_input } from "../common/KeyboardShortcuts.js"
-import { handle_log } from "../common/Logging.js"
 import { PlutoContext, PlutoBondsContext, PlutoJSInitializingContext, SetWithEmptyCallback } from "../common/PlutoContext.js"
-import { unpack } from "../common/MsgPack.js"
-import { PkgTerminalView } from "./PkgTerminalView.js"
 import { start_binder, BinderPhase, count_stat } from "../common/Binder.js"
 import { setup_mathjax } from "../common/SetupMathJax.js"
-import { read_Uint8Array_with_progress, FetchProgress } from "./FetchProgress.js"
 import { BinderButton } from "./BinderButton.js"
 import { slider_server_actions, nothing_actions } from "../common/SliderServerClient.js"
 import { ProgressBar } from "./ProgressBar.js"
@@ -39,7 +36,7 @@ import { HijackExternalLinksToOpenInNewTab } from "./HackySideStuff/HijackExtern
 // This is imported asynchronously - uncomment for development
 // import environment from "../common/Environment.js"
 
-const default_path = "..."
+export const default_path = "..."
 const DEBUG_DIFFING = false
 
 // from our friends at https://stackoverflow.com/a/2117523
@@ -166,6 +163,22 @@ const first_true_key = (obj) => {
  */
 
 /**
+ * @typedef LaunchParameters
+ * @type {{
+ *  notebook_id: string?,
+ *  statefile: string?,
+ *  notebookfile: string?,
+ *  disable_ui: boolean,
+ *  preamble_html: string?,
+ *  isolated_cell_ids: string[]?,
+ *  binder_url: string?,
+ *  slider_server_url: string?,
+ *  recording_url: string?,
+ *  recording_audio_url: string?,
+ * }}
+ */
+
+/**
  * @typedef NotebookData
  * @type {{
  *  notebook_id: string,
@@ -190,36 +203,19 @@ const url_logo_big = document.head.querySelector("link[rel='pluto-logo-big']").g
 const url_logo_small = document.head.querySelector("link[rel='pluto-logo-small']").getAttribute("href")
 
 /**
- *
- * @returns {NotebookData}
+ * @typedef EditorProps
+ * @type {{launch_params: LaunchParameters,initial_notebook_state: NotebookData,}}
+ * @augments Component<EditorProps,{}>
  */
-const initial_notebook = ({ notebook_id }) => ({
-    notebook_id: notebook_id,
-    path: default_path,
-    shortpath: "",
-    in_temp_dir: true,
-    process_status: "starting",
-    last_save_time: 0.0,
-    last_hot_reload_time: 0.0,
-    cell_inputs: {},
-    cell_results: {},
-    cell_dependencies: {},
-    cell_order: [],
-    cell_execution_order: [],
-    published_objects: {},
-    bonds: {},
-    nbpkg: null,
-})
-
 export class Editor extends Component {
-    constructor(props) {
+    constructor(/** @type {EditorProps} */ props) {
         super(props)
 
         console.log(this.props)
-        const { launch_params } = this.props
+        const { launch_params, initial_notebook_state } = this.props
 
         this.state = {
-            notebook: /** @type {NotebookData} */ initial_notebook(this.props.launch_params),
+            notebook: /** @type {NotebookData} */ initial_notebook_state,
             cell_inputs_local: /** @type {{ [id: string]: CellInputData }} */ ({}),
             desired_doc_query: null,
             recently_deleted: /** @type {Array<{ index: number, cell: CellInputData }>} */ (null),
@@ -227,7 +223,6 @@ export class Editor extends Component {
 
             disable_ui: launch_params.disable_ui,
             static_preview: launch_params.statefile != null,
-            statefile_download_progress: null,
             offer_binder: launch_params.notebookfile != null && launch_params.binder_url != null,
             binder_phase: null,
             binder_session_url: null,
@@ -647,7 +642,10 @@ patch: ${JSON.stringify(
                         let apply_promise = Promise.resolve()
                         if (message?.response?.from_reset) {
                             console.log("Trying to reset state after failure")
-                            apply_promise = apply_notebook_patches(message.patches, initial_notebook()).catch((e) => {
+                            apply_promise = apply_notebook_patches(
+                                message.patches,
+                                empty_notebook_state({ notebook_id: this.state.notebook.notebook_id })
+                            ).catch((e) => {
                                 alert("Oopsie!! please refresh your browser and everything will be alright!")
                                 throw e
                             })
@@ -748,7 +746,7 @@ patch: ${JSON.stringify(
                       actions: this.actions,
                       launch_params: launch_params,
                       apply_notebook_patches,
-                      get_original_state: () => this.original_state,
+                      get_original_state: () => this.props.initial_notebook_state,
                       get_current_state: () => this.state.notebook,
                   })
                 : nothing_actions({
@@ -756,30 +754,18 @@ patch: ${JSON.stringify(
                   })
 
         this.on_disable_ui = () => {
-            document.body.classList.toggle("disable_ui", this.state.disable_ui)
-            document.head.querySelector("link[data-pluto-file='hide-ui']").setAttribute("media", this.state.disable_ui ? "all" : "print")
+            set_disable_ui_css(this.state.disable_ui)
+
             //@ts-ignore
             this.actions = this.state.disable_ui || (launch_params.slider_server_url != null && !this.state.connected) ? this.fake_actions : this.real_actions //heyo
         }
         this.on_disable_ui()
 
-        this.original_state = null
         if (this.state.static_preview) {
-            ;(async () => {
-                const r = await fetch(launch_params.statefile)
-                const data = await read_Uint8Array_with_progress(r, (progress) => {
-                    this.setState({
-                        statefile_download_progress: progress,
-                    })
-                })
-                const state = unpack(data)
-                this.original_state = state
-                this.setState({
-                    notebook: state,
-                    initializing: false,
-                    binder_phase: this.state.offer_binder ? BinderPhase.wait_for_user : null,
-                })
-            })()
+            this.setState({
+                initializing: false,
+                binder_phase: this.state.offer_binder ? BinderPhase.wait_for_user : null,
+            })
             // view stats on https://stats.plutojl.org/
             count_stat(`article-view`)
         } else {
@@ -1317,16 +1303,16 @@ patch: ${JSON.stringify(
                         reset_notebook_state=${() =>
                             this.setStatePromise(
                                 immer((state) => {
-                                    state.notebook = this.original_state
+                                    state.notebook = this.props.initial_notebook_state
                                 })
                             )}
                     />
                     
-                    <${BinderButton} binder_phase=${this.state.binder_phase} start_binder=${() =>
-            start_binder({ setStatePromise: this.setStatePromise, connect: this.connect, launch_params: launch_params })} notebookfile=${
-            launch_params.notebookfile == null ? null : new URL(launch_params.notebookfile, window.location.href).href
-        } />
-                    <${FetchProgress} progress=${this.state.statefile_download_progress} />
+                    <${BinderButton} 
+                        binder_phase=${this.state.binder_phase} 
+                        start_binder=${() => start_binder({ setStatePromise: this.setStatePromise, connect: this.connect, launch_params: launch_params })} 
+                        notebookfile=${launch_params.notebookfile == null ? null : new URL(launch_params.notebookfile, window.location.href).href} />
+                    
                     ${launch_params.preamble_html ? html`<${RawHTMLContainer} body=${launch_params.preamble_html} className=${"preamble"} />` : null}
                     <${Main}>
                         <${Preamble}
--- frontend/editor.js
+++ frontend/editor.js
@@ -1,9 +1,18 @@
-import { html, render } from "./imports/Preact.js"
+import { html, render, useEffect, useRef, useState } from "./imports/Preact.js"
 import "./common/NodejsCompatibilityPolyfill.js"
 
-import { Editor } from "./components/Editor.js"
+import { Editor, default_path } from "./components/Editor.js"
+import { FetchProgress, read_Uint8Array_with_progress } from "./components/FetchProgress.js"
+import { BinderPhase } from "./common/Binder.js"
+import { unpack } from "./common/MsgPack.js"
+import { RawHTMLContainer } from "./components/CellOutput.js"
 
 const url_params = new URLSearchParams(window.location.search)
+
+/**
+ *
+ * @type {import("./components/Editor.js").LaunchParameters}
+ */
 const launch_params = {
     //@ts-ignore
     notebook_id: url_params.get("id") ?? window.pluto_notebook_id,
@@ -28,5 +37,81 @@ const launch_params = {
 }
 console.log("Launch parameters: ", launch_params)
 
+/**
+ *
+ * @returns {import("./components/Editor.js").NotebookData}
+ */
+export const empty_notebook_state = ({ notebook_id }) => ({
+    notebook_id: notebook_id,
+    path: default_path,
+    shortpath: "",
+    in_temp_dir: true,
+    process_status: "starting",
+    last_save_time: 0.0,
+    last_hot_reload_time: 0.0,
+    cell_inputs: {},
+    cell_results: {},
+    cell_dependencies: {},
+    cell_order: [],
+    cell_execution_order: [],
+    published_objects: {},
+    bonds: {},
+    nbpkg: null,
+})
+
+/**
+ *
+ * @param {{
+ *  launch_params: import("./components/Editor.js").LaunchParameters,
+ * }} props
+ */
+const EditorLoader = ({ launch_params }) => {
+    const static_preview = launch_params.statefile != null
+
+    const [statefile_download_progress, set_statefile_download_progress] = useState(null)
+
+    const initial_notebook_state_ref = useRef(empty_notebook_state(launch_params))
+    const [ready_for_editor, set_ready_for_editor] = useState(!static_preview)
+
+    useEffect(() => {
+        if (!ready_for_editor && static_preview) {
+            ;(async () => {
+                const r = await fetch(launch_params.statefile)
+                const data = await read_Uint8Array_with_progress(r, set_statefile_download_progress)
+                const state = unpack(data)
+                initial_notebook_state_ref.current = state
+                set_ready_for_editor(true)
+            })()
+        }
+    }, [ready_for_editor, static_preview, launch_params.statefile])
+
+    useEffect(() => {
+        set_disable_ui_css(launch_params.disable_ui)
+    }, [launch_params.disable_ui])
+
+    const preamble_element = launch_params.preamble_html ? html`<${RawHTMLContainer} body=${launch_params.preamble_html} className=${"preamble"} />` : null
+
+    return ready_for_editor
+        ? html`<${Editor} initial_notebook_state=${initial_notebook_state_ref.current} launch_params=${launch_params} preamble_element=${preamble_element} />`
+        : // todo: show preamble html
+          html`
+              ${preamble_element}
+              <${FetchProgress} progress=${statefile_download_progress} />
+          `
+}
+
 // it's like a Rube Goldberg machine
-render(html`<${Editor} launch_params=${launch_params} />`, document.body)
+render(html`<${EditorLoader} launch_params=${launch_params} />`, document.body)
+
+//////////////
+// utils:
+
+const set_attribute_if_needed = (element, attr, value) => {
+    if (element.getAttribute(attr) !== value) {
+        element.setAttribute(attr, value)
+    }
+}
+export const set_disable_ui_css = (val) => {
+    document.body.classList.toggle("disable_ui", val)
+    set_attribute_if_needed(document.head.querySelector("link[data-pluto-file='hide-ui']"), "media", val ? "all" : "print")
+}
