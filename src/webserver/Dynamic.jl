e693252ff0fdff8c373ab80f30425e20401cf576
Fons van der Plas <fonsvdplas@gmail.com>
2022-03-12 18:39:24 +0100

    Update Dynamic.jl

--- src/webserver/Dynamic.jl
+++ src/webserver/Dynamic.jl
@@ -87,6 +87,8 @@ end
 # All of the arrays in the notebook_to_js object are 'immutable' (we write code as if they are), so we can enable this optimization:
 Firebasey.use_triple_equals_for_arrays[] = true
 
+precompile(Firebasey.diff, (Dict{String,Any},Dict{String,Any}))
+
 # the only possible Arrays are:
 # - cell_order
 # - cell_execution_order
@@ -169,6 +171,7 @@ function notebook_to_js(notebook::Notebook)
         "cell_execution_order" => cell_id.(collect(topological_order(notebook))),
     )
 end
+precompile(notebook_to_js, (Notebook,))
 
 """
 For each connected client, we keep a copy of their current state. This way we know exactly which updates to send when the server-side state changes.
