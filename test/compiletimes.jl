2be1148b4a9800a64b01f2058424413cf0cb31e6
Rik Huijzer <rikhuijzer@pm.me>
2022-03-14 22:05:21 +0100

    Add more compilation tests

--- test/compiletimes.jl
+++ test/compiletimes.jl
@@ -1,5 +1,9 @@
 # Collect Time To Finish Task (TTFT)
 
+@timeit TOUT "Pluto.Cell" cell = Pluto.Cell("1 + 1")
+
+@timeit TOUT "Pluto.Notebook" nb = Pluto.Notebook([cell])
+
 function wait_for_ready(notebook::Pluto.Notebook)
     while notebook.process_status != Pluto.ProcessStatus.ready
         sleep(0.1)
@@ -17,3 +21,17 @@ path = joinpath(pkgdir(Pluto), "sample", "Basic.jl")
 wait_for_ready(nb)
 
 @timeit TOUT "SessionActions.shutdown" Pluto.SessionActions.shutdown(🍭, nb; async=true)
+
+@timeit TOUT "Pluto.run" server_task = let
+    port = 13435
+    options = Pluto.Configuration.from_flat_kwargs(; port, launch_browser=false, workspace_use_distributed=true, require_secret_for_access=false, require_secret_for_open_links=false)
+    🍭 = Pluto.ServerSession(; options)
+    server_task = @async Pluto.run(🍭)
+
+    retry(; delays=ExponentialBackOff(n=5, first_delay=0.5)) do
+        HTTP.get("http://localhost:$port/edit").status == 200
+    end
+    server_task
+end
+
+schedule(server_task, InterruptException(); error=true)
