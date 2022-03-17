54627434a9f7d09b8eff5654d6b435c49e02a15e
Rik Huijzer <rikhuijzer@pm.me>
2022-03-17 09:33:42 +0100

    Cleanup

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,6 @@ include("helpers.jl")
 
 @timeit_include("compiletimes.jl")
 verify_no_running_processes()
-show(TOUT; compact=true, sortby=:firstexec)
-exit(0)
 @timeit_include("Events.jl")
 verify_no_running_processes()
 @timeit_include("WorkspaceManager.jl")
