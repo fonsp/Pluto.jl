854e6eb9670df387bf116934f8a3ef99cfde1221
Fons van der Plas <fonsvdplas@gmail.com>
2022-04-21 18:10:31 +0200

    collections

--- frontend/components/welcome/Featured.js
+++ frontend/components/welcome/Featured.js
@@ -5,26 +5,37 @@ import { FeaturedCard } from "./FeaturedCard.js"
 const run = (f) => f()
 
 /**
- * @typedef SourceManifestEntry
+ * @typedef SourceManifestNotebookEntry
  * @type {{
- *  id: String,
- *  hash: String,
- *  html_path: String,
- *  statefile_path: String,
- *  notebookfile_path: String,
- *  frontmatter: Record<string,any>,
+ *   id: String,
+ *   hash: String,
+ *   html_path: String,
+ *   statefile_path: String,
+ *   notebookfile_path: String,
+ *   frontmatter: Record<string,any>,
+ * }}
+ */
+
+/**
+ * @typedef SourceManifestCollectionEntry
+ * @type {{
+ *   title: String?,
+ *   description: String?,
+ *   tags: Array<String>?,
  * }}
  */
 
 /**
  * @typedef SourceManifest
  * @type {{
- *   notebooks: Record<string,SourceManifestEntry>,
- *   collections: Array<String>,
+ *   notebooks: Record<string,SourceManifestNotebookEntry>,
+ *   collections: Array<SourceManifestCollectionEntry>?,
  *   pluto_version: String,
  *   julia_version: String,
  *   format_version: String,
  *   source_url: String,
+ *   title: String?,
+ *   description: String?,
  * }}
  */
 
@@ -61,23 +72,41 @@ export const Featured = () => {
         }
     }, [sources])
 
+    useEffect(() => {
+        if (source_data.length > 0) {
+            console.log("Sources:", source_data)
+        }
+    }, [source_data])
+
     return html`
-        <h2>Featured</h2>
-        <div class="featured-sources">
-            ${source_data != null
-                ? source_data.map(
-                      (data) => html`
-                          <div class="featured-source">
-                              <p>${data.description ?? "Blasdfjklhaksdjfh kajsdhf kajdsfh kjhasd fkjshdfkjshdf kwehfwuhefiwfk  m dfsa dsfbm s dbf"}</p>
-                              <div class="featured-source-notebooks">
-                                  ${Object.entries(data.notebooks).map(
-                                      ([id, entry]) => html` <${FeaturedCard} entry=${entry} source_url=${data.source_url} /> `
-                                  )}
-                              </div>
-                          </div>
-                      `
-                  )
-                : null}
-        </div>
+        ${source_data != null
+            ? source_data.map(
+                  (data) => html`
+                      <div class="featured-source">
+                          <h1>${data.title}</h1>
+                          <p>${data.description}</p>
+                          ${data.collections.map((coll) => {
+                              return html`
+                                  <div class="collection">
+                                      <h2>${coll.title}</h2>
+                                      <p>${coll.description}</p>
+                                      <div class="card-list">
+                                          ${collection(Object.values(data.notebooks), coll.tags).map(
+                                              (entry) => html`<${FeaturedCard} entry=${entry} source_url=${data.source_url} />`
+                                          )}
+                                      </div>
+                                  </div>
+                              `
+                          })}
+                      </div>
+                  `
+              )
+            : null}
     `
+}
+
+const collection = (/** @type {SourceManifestNotebookEntry[]} */ notebooks, /** @type {String[]} */ tags) => {
+    const nbs = notebooks.filter((notebook) => tags.some((t) => (notebook.frontmatter?.tags ?? []).includes(t)))
+
+    return /** @type {SourceManifestNotebookEntry[]} */ (_.sortBy(nbs, [(nb) => Number(nb?.frontmatter?.order), "id"]))
 }
