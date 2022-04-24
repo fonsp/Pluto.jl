c9f3b9ca6406737a6a7fb2d68ea221606d093d9f
Fons van der Plas <fonsvdplas@gmail.com>
2022-04-24 22:30:18 +0200

    (technically api breaking) Remove the `notebook` field of `NewNotebookEvent` (#2049)

--- src/webserver/SessionActions.jl
+++ src/webserver/SessionActions.jl
@@ -31,7 +31,7 @@ function open_url(session::ServerSession, url::AbstractString; kwargs...)
         ); suffix=".jl")
     
     path = download_cool(url, random_notebook.path)
-    result = try_event_call(session, NewNotebookEvent(random_notebook))
+    result = try_event_call(session, NewNotebookEvent())
     nb = if result isa UUID
         open(session, path; notebook_id=result, kwargs...)
     else
@@ -221,7 +221,7 @@ function new(session::ServerSession; run_async=true, notebook_id::UUID=uuid1())
         end
     end
     # Run NewNotebookEvent handler before assigning ID
-    isid = try_event_call(session, NewNotebookEvent(nb))
+    isid = try_event_call(session, NewNotebookEvent())
     nb.notebook_id = isnothing(isid) ? notebook_id : isid
 
     update_save_run!(session, nb, nb.cells; run_async=run_async, prerender_text=true)
