8a68342376dc0724481d3cdd9da7c850e28468e2
Rik Huijzer <rikhuijzer@pm.me>
2022-06-07 15:00:10 +0200

    Also trigger compilation for Unicode

--- frontend/components/Editor.js
+++ frontend/components/Editor.js
@@ -780,6 +780,7 @@ patch: ${JSON.stringify(
             // do one autocomplete to trigger its precompilation
             // TODO Do this from julia itself
             this.client.send("complete", { query: "sq" }, { notebook_id: this.state.notebook.notebook_id })
+            this.client.send("complete", { query: "\sq" }, { notebook_id: this.state.notebook.notebook_id })
 
             setTimeout(init_feedback, 2 * 1000) // 2 seconds - load feedback a little later for snappier UI
         }
