67777214c169fcfda189561885b1fcfbf1113ed3
Fons van der Plas <fonsvdplas@gmail.com>
2022-06-16 17:26:42 +0200

    update sources

--- frontend/featured_sources.js
+++ frontend/featured_sources.js
@@ -1,8 +1,8 @@
 export default {
     sources: [
         {
-            url: "https://cdn.jsdelivr.net/gh/JuliaPluto/featured@5a7c784/pluto_export.json",
-            integrity: "sha256-Tzznrgmk+NaIL2Spb784vKjDKPM0dOIUXbcvx/z6IBQ=",
+            url: "https://cdn.jsdelivr.net/gh/JuliaPluto/featured@a0158dceac9ec602aaf4d8e0d53ddd10b8b56854/pluto_export.json",
+            integrity: "sha256-6LMXdEg2AvJcgkO1ne6tf75UFC8Vob6YqRAx6ytrfk8=",
         },
     ],
 }
