4758c4e73e472808b1dad1b765fc12bfb3cd4071
Fons van der Plas <fonsvdplas@gmail.com>
2022-06-16 17:56:13 +0200

    fix bundle

--- frontend/featured_sources.js
+++ frontend/featured_sources.js
@@ -1,4 +1,5 @@
 export default {
+    // check out https://github.com/JuliaPluto/pluto-developer-instructions/blob/main/How%20to%20update%20the%20featured%20notebooks.md to learn more
     sources: [
         {
             url: "https://cdn.jsdelivr.net/gh/JuliaPluto/featured@a0158dceac9ec602aaf4d8e0d53ddd10b8b56854/pluto_export.json",
