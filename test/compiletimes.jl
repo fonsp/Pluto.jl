bb8875196e3185fc531260f46d95930842c10dc0
Paul <paul@plutojl.org>
2022-06-19 14:50:33 +0200

    Use Accept-Encoding: identity in test/compiletimes.jl

--- test/compiletimes.jl
+++ test/compiletimes.jl
@@ -40,8 +40,8 @@ wait_for_ready(nb)
 
 Pluto.SessionActions.shutdown(🍭, nb; async=false)
 
-# Compile HTTP get.
-HTTP.get("http://github.com")
+# Compile HTTP get. Use no encoding since there seem to be an issue with Accept-Encoding: gzip
+HTTP.get("http://github.com"; headers = Dict("Accept-Encoding" => "identity"))
 
 @timeit TOUT "Pluto.run" server_task = @eval let
     port = 13435
