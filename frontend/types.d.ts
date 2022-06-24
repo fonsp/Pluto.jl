929e6ad92e9a579adf27eb753bbd4e6a9ce8870b
illusion <dhruvtiwari110058@gmail.com>
2022-06-25 01:38:23 +0530

    fix: Add window.electron type definition

---
+++ frontend/types.d.ts
@@ -0,0 +1,33 @@
+declare global {
+    /**
+     * This type has to be in sync with the type "Channels"
+     * defined in PlutoDesktop/{branch}/src/main/proload.ts
+     */
+    type Channels = "ipc-example"
+    /**
+     * This type has to be in sync with the interface "Window"
+     * defined in PlutoDesktop/{branch}/src/renderer/preload.d.ts
+     */
+    interface Window {
+        electron?: {
+            ipcRenderer: {
+                sendMessage(channel: Channels, args: unknown[]): void
+                on(channel: string, func: (...args: unknown[]) => void): (() => void) | undefined
+                once(channel: string, func: (...args: unknown[]) => void): void
+            }
+            fileSystem: {
+                /**
+                 * @param path path to a notebook, if already selected
+                 * @param forceNew [default = false] If false and valid path is there,
+                 * opens that notebook. If false and no path is there, opens the file selector.
+                 * If true, opens a new blank notebook.
+                 */
+                openNotebook(path?: string, forceNew?: boolean): void
+                shutdownNotebook(id?: string): void
+                moveNotebook(id?: string): void
+            }
+        }
+    }
+}
+
+export {}
