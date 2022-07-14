748b4889efa03e9bd3c059aedbfc6be521ee2f53
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliacomputing.com>
2022-07-14 10:49:03 +0000

    Memoize component orelse it rerenders empty and looses state

--- frontend/components/Editor/LaunchBackendButton.js
+++ frontend/components/Editor/LaunchBackendButton.js
@@ -1,4 +1,4 @@
-import { html } from "../../imports/Preact.js"
+import { html, useMemo } from "../../imports/Preact.js"
 import * as preact from "../../imports/Preact.js"
 import { RunLocalButton, BinderButton } from "../EditOrRunButton.js"
 import { start_local } from "../../common/RunLocal.js"
@@ -6,11 +6,15 @@ import { BackendLaunchPhase, start_binder } from "../../common/Binder.js"
 import immer, { applyPatches, produceWithPatches } from "../../imports/immer.js"
 
 export const EditorLaunchBackendButton = ({ editor, launch_params, status }) => {
-    // @ts-ignore
+    const EnvRun = useMemo(
+        // @ts-ignore
+        () => window.pluto_injected_environment.environment({ client: editor.client, editor, imports: { immer, preact } }).custom_run_or_edit,
+        [editor.client, editor]
+    )
+
     if (window?.pluto_injected_environment?.provides_backend) {
         try {
             // @ts-ignore
-            const EnvRun = window.pluto_injected_environment.environment({ client: editor.client, editor, imports: { immer, preact } }).custom_run_or_edit
             return html`<${EnvRun} editor=${editor} backend_phases=${BackendLaunchPhase} launch_params=${launch_params} />`
             // Don't allow a misconfigured environment to stop offering other backends
         } catch (e) {}
