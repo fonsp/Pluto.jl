0f4a167ac050dc2f1674308ee470f10a86b9de16
illusion <dhruvtiwari110058@gmail.com>
2022-07-29 12:04:43 +0530

    fix: calm down typescript

--- frontend/index.js
+++ frontend/index.js
@@ -3,4 +3,5 @@ import "./common/NodejsCompatibilityPolyfill.js"
 
 import { Welcome } from "./components/welcome/Welcome.js"
 
+// @ts-ignore
 render(html`<${Welcome} />`, document.querySelector("#app"))
