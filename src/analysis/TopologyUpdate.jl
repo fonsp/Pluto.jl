37d63eae599a86721352c4400bb2960fb1ddf324
Fons van der Plas <fonsvdplas@gmail.com>
2022-09-07 19:04:03 +0200

    updated_topology: maintain identity when there are no changes

--- src/analysis/TopologyUpdate.jl
+++ src/analysis/TopologyUpdate.jl
@@ -6,7 +6,7 @@ function updated_topology(old_topology::NotebookTopology, notebook::Notebook, ce
 	
 	updated_codes = Dict{Cell,ExprAnalysisCache}()
 	updated_nodes = Dict{Cell,ReactiveNode}()
-	unresolved_cells = copy(old_topology.unresolved_cells.c)
+	
 	for cell in cells
 		old_code = old_topology.codes[cell]
 		if old_code.code !== cell.code
@@ -20,17 +20,9 @@ function updated_topology(old_topology::NotebookTopology, notebook::Notebook, ce
 			# reset computer code
 			updated_codes[cell] = ExprAnalysisCache(old_code; forced_expr_id=nothing, function_wrapped=false)
 		end
-
-		new_reactive_node = get(updated_nodes, cell, old_topology.nodes[cell])
-		if !isempty(new_reactive_node.macrocalls)
-			# The unresolved cells are the cells for wich we cannot create
-			# a ReactiveNode yet, because they contains macrocalls.
-			push!(unresolved_cells, cell) 
-		else
-			pop!(unresolved_cells, cell, nothing)
-		end
 	end
-
+	
+	
 	old_cells = all_cells(old_topology)
 	removed_cells = setdiff(old_cells, notebook.cells)
 	if isempty(removed_cells)
@@ -38,9 +30,28 @@ function updated_topology(old_topology::NotebookTopology, notebook::Notebook, ce
 		new_codes = merge(old_topology.codes, updated_codes)
 		new_nodes = merge(old_topology.nodes, updated_nodes)
 	else
-		setdiff!(unresolved_cells, removed_cells)
 		new_codes = merge(setdiffkeys(old_topology.codes, removed_cells), updated_codes)
 		new_nodes = merge(setdiffkeys(old_topology.nodes, removed_cells), updated_nodes)
+	end
+	
+	unresolved_cells = if isempty(updated_nodes) && isempty(removed_cells)
+		old_topology.unresolved_cells
+	else
+		# The new set of unresolved cells is...
+		new_unresolved_set = setdiff!(
+			union!(
+				Set{Cell}(),
+				# all cells that were unresolved before...
+				old_topology.unresolved_cells,
+				# ...plus all cells that changed...
+				keys(updated_nodes),
+			),
+			# ...minus cells that were removed...
+			removed_cells, 
+			# ...minus cells that changed, which do not use any macros.
+			(c for (c,n) in updated_nodes if isempty(n.macrocalls))
+		)
+		ImmutableSet{Cell}(new_unresolved_set; skip_copy=true)
 	end
 
 	cell_order = if old_cells == notebook.cells
@@ -48,11 +59,11 @@ function updated_topology(old_topology::NotebookTopology, notebook::Notebook, ce
 	else
 		ImmutableVector(notebook.cells)
 	end
-
+	
 	NotebookTopology(;
 		nodes=new_nodes,
 		codes=new_codes,
-		unresolved_cells=ImmutableSet(unresolved_cells; skip_copy=true), 
+		unresolved_cells, 
 		cell_order,
 	)
 end
