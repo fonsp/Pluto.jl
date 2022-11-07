0ce6e2f1d5fb2fc439bad7b20f7e8137005feffa
Michiel Dral <m.c.dral@gmail.com>
2022-11-07 16:56:13 +0000

    "This is exactly why you should open PRs first"

--- frontend/components/CellInput/mod_d_command.js
+++ frontend/components/CellInput/mod_d_command.js
@@ -1,5 +1,9 @@
 import { EditorView, EditorSelection, selectNextOccurrence, syntaxTree } from "../../imports/CodemirrorPlutoSetup.js"
 
+let array_at = (array, pos) => {
+    return array.slice(pos, pos + 1)[0]
+}
+
 export let mod_d_command = {
     key: "Mod-d",
     /** @param {EditorView} view */
@@ -48,7 +52,7 @@ export let mod_d_command = {
         let new_ranges = state.selection.ranges.filter((x) => x !== just_selected)
         let new_main_index = new_ranges.indexOf(state.selection.main)
 
-        let previous_selected = new_ranges.at(state.selection.mainIndex - 1)
+        let previous_selected = array_at(new_ranges, state.selection.mainIndex - 1)
 
         dispatch({
             selection: EditorSelection.create(new_ranges, new_main_index),
