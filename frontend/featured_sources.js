d18e339859443506ca7d85ae0992936821e44b17
Fons van der Plas <fonsvdplas@gmail.com>
2022-12-22 23:18:52 +0100

    Update featured_sources.js

--- frontend/featured_sources.js
+++ frontend/featured_sources.js
@@ -2,8 +2,8 @@ export default {
     // check out https://github.com/JuliaPluto/pluto-developer-instructions/blob/main/How%20to%20update%20the%20featured%20notebooks.md to learn more
     sources: [
         {
-            url: "https://cdn.jsdelivr.net/gh/JuliaPluto/featured@08d6f16/pluto_export.json",
-            integrity: "sha256-DyxjtJQ+7/LkrO2y+MldQng8fcwFWY7FwRHwbtIooWM=",
+            url: "https://cdn.jsdelivr.net/gh/JuliaPluto/featured@fa62114/pluto_export.json",
+            integrity: "sha256-mqaCAS0NOcrmgfYmN4284eP44913HWPQfM8OaXj01II=",
         },
     ],
 }
