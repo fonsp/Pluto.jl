78f7d9874347c939b781d14130d7bc2aad3f4a55
Fons van der Plas <fonsvdplas@gmail.com>
2023-03-14 14:51:47 +0100

    Instantiate after update (#2505)

--- src/packages/Packages.jl
+++ src/packages/Packages.jl
@@ -545,6 +545,14 @@ function update_nbpkg_core(
                 stoplistening(iolistener)
 
                 🐧 = !PkgCompat.is_original(notebook.nbpkg_ctx)
+                should_instantiate_again = !notebook.nbpkg_ctx_instantiated || 🐧
+                
+                if should_instantiate_again
+                    # Status.report_business!(pkg_status, :instantiate2) do
+                    instantiate(notebook, iolistener)
+                    # end
+                end
+
                 (
                     did_something=🐧,
                     restart_recommended=🐧,
