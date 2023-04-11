5f19e54898c136b6440c1d7c6e09cc70b5ac675b
fonsp <fonsvdplas@gmail.com>
2023-04-11 08:40:44 +0000

    whitespace

--- src/packages/precompile_isolated.jl
+++ src/packages/precompile_isolated.jl
@@ -1,6 +1,3 @@
-
-
-
 function precompile_isolated(
     environment::String; 
     compiler_options::Configuration.CompilerOptions=Configuration.CompilerOptions(),
@@ -9,17 +6,12 @@ function precompile_isolated(
     flags = Configuration._convert_to_flags(compiler_options)
     
     code = """
-    # Add some color
-    
-    out_stream = IOContext(stdout, :color => true)
-    
-    # redirect_stdout(IOContext(stdout, :color => true))
-    # redirect_stderr(IOContext(stderr, :color => true))
-    
     # import Pkg with safe load path
     pushfirst!(LOAD_PATH, "@stdlib")
     import Pkg
     popfirst!(LOAD_PATH)
+    
+    out_stream = IOContext(stdout, :color => true)
     
     Pkg.activate($(repr(environment)); io=out_stream)
     if VERSION >= v"1.8.0" # https://github.com/JuliaLang/Pkg.jl/pull/2816
@@ -32,12 +24,9 @@ function precompile_isolated(
     cmd = `$(Base.julia_cmd()[1]) $(flags) -e $(code)`
 
     Base.run(pipeline(
-        cmd; stdout=io, #dont campture stderr because we want it to show in the server terminal when something goes wrong
+        cmd; stdout=io, #dont capture stderr because we want it to show in the server terminal when something goes wrong
     ))
     
     # In the future we could allow interrupting the precompilation process (e.g. when the notebook is shut down)
     # by running this code using Malt.jl
 end
-
-
-
