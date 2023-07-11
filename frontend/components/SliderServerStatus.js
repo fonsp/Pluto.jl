475b8bd91b21492c52ea76e90899b3a8f633a8f1
Connor Burns <ctrekker4@gmail.com>
2023-07-11 12:56:59 -0600

    Remove sliders status indicator

--- frontend/components/SliderServerStatus.js
+++
@@ -1,6 +0,0 @@
-import { html, useEffect, useState, useContext, useRef, useMemo } from "../imports/Preact.js"
-
-export const SliderServerStatus = ({ interactive }) => {
-    const details_message = interactive ? "Connected to sliders!" : ""
-    return html`<div id="sliderstatus-container" title=${details_message}>${interactive && html`<span></span>`}</div>`
-}
