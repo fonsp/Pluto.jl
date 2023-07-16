ce919bee9e0d56adca3693acc53f0f56bf360ba9
Alberto Mengali <disberd@gmail.com>
2023-07-16 19:17:21 +0200

    add frontend test for published_to_js

---
+++ test/frontend/__tests__/published_to_js.js
@@ -0,0 +1,58 @@
+import puppeteer from "puppeteer"
+import { lastElement, saveScreenshot, getTestScreenshotPath, createPage } from "../helpers/common"
+import {
+    getCellIds,
+    importNotebook,
+    waitForCellOutput,
+    getPlutoUrl,
+    prewarmPluto,
+    writeSingleLineInPlutoInput,
+    waitForNoUpdateOngoing,
+    shutdownCurrentNotebook,
+    setupPlutoBrowser,
+} from "../helpers/pluto"
+
+describe("publish_to_js", () => {
+    /**
+     * Launch a shared browser instance for all tests.
+     * I don't use jest-puppeteer because it takes away a lot of control and works buggy for me,
+     * so I need to manually create the shared browser.
+     * @type {puppeteer.Browser}
+     */
+    let browser = null
+    /** @type {puppeteer.Page} */
+    let page = null
+    beforeAll(async () => {
+        browser = await setupPlutoBrowser()
+    })
+    beforeEach(async () => {
+        page = await createPage(browser)
+        await page.goto(getPlutoUrl(), { waitUntil: "networkidle0" })
+    })
+    afterEach(async () => {
+        await saveScreenshot(page)
+        await shutdownCurrentNotebook(page)
+        await page.close()
+        page = null
+    })
+    afterAll(async () => {
+        await browser.close()
+        browser = null
+    })
+
+    it("Should correctly show published_to_js in cell output, but not in logs", async () => {
+        await importNotebook(page, "published_to_js.jl")
+        await waitForNoUpdateOngoing(page, { polling: 100 })
+        let output_of_published = await page.evaluate(() => {
+            return document.querySelector("#to_cell_output")?.textContent
+        })
+        expect(output_of_published).toBe("[1,2,3] MAGIC!")
+
+        // The log content is not shown, so #to_cell_log does not exist
+        let log_of_published = await page.evaluate(() => {
+            return document.querySelector("#to_cell_log")?.textContent
+        })
+        // This test is currently broken, due to https://github.com/fonsp/Pluto.jl/issues/2092
+        // expect(log_of_published).toBe("[4,5,6] MAGIC!")
+    })
+})
