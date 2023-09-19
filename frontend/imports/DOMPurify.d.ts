091b48f337d654b21829c86a16392d036ee5f709
Fons van der Plas <fonsvdplas@gmail.com>
2023-09-19 09:44:42 +0200

    Update DOMPurify.d.ts

--- frontend/imports/DOMPurify.d.ts
+++ frontend/imports/DOMPurify.d.ts
@@ -34,7 +34,7 @@ interface createDOMPurifyI extends DOMPurify.DOMPurifyI {
 declare namespace DOMPurify {
     interface DOMPurifyI {
         sanitize(source: string | Node): string;
-        sanitize(source: string | Node, config: Config & { RETURN_TRUSTED_TYPE: true }): TrustedHTML;
+        // sanitize(source: string | Node, config: Config & { RETURN_TRUSTED_TYPE: true }): TrustedHTML;
         sanitize(
             source: string | Node,
             config: Config & { RETURN_DOM_FRAGMENT?: false | undefined; RETURN_DOM?: false | undefined },
