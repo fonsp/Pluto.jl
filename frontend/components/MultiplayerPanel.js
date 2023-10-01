c961c75f7f2f5ecb1888325677a9b6ab7d1cd7df
Paul <paul@plutojl.org>
2023-10-01 19:10:14 +0200

    style ✨✨

--- frontend/components/MultiplayerPanel.js
+++ frontend/components/MultiplayerPanel.js
@@ -1,13 +1,15 @@
 import { html } from "../imports/Preact.js"
 
 export const MultiplayerPanel = ({ users }) => {
-    if (!users) return
+    if (!users || Object.keys(users).length == 1) return
 
     return html`
-        <ul>
-            ${Object.entries(users).map(
-                ([clientID, { name, color, focused_cell }]) => html`<li key=${clientID} style=${`color: ${color};`}>${name} - ${focused_cell}</li>`
-            )}
-        </ul>
+        <div className="pluto-multiplayer">
+            <ul>
+                ${Object.entries(users).map(
+                    ([clientID, { name, color, focused_cell }]) => html`<li key=${clientID} style=${`color: ${color};`}>${name} - ${focused_cell}</li>`
+                )}
+            </ul>
+        </div>
     `
 }
