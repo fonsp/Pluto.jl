c6c9ff2e0a6a9dc07019746d73d7cc85375af65c
Fons van der Plas <fonsvdplas@gmail.com>
2023-10-18 12:45:10 +0200

    Add featured.plutojl.org as a featured source

--- frontend/featured_sources.js
+++ frontend/featured_sources.js
@@ -2,8 +2,14 @@ export default {
     // check out https://github.com/JuliaPluto/pluto-developer-instructions/blob/main/How%20to%20update%20the%20featured%20notebooks.md to learn more
     sources: [
         {
+            url: "https://featured.plutojl.org/pluto_export.json",
+            valid_until: "2024-10",
+            id: "featured pluto",
+        },
+        {
             url: "https://cdn.jsdelivr.net/gh/JuliaPluto/featured@v3/pluto_export.json",
             integrity: "sha256-y2E/niS8Em5a4wfSupsmDi0JaTrKSI0WF9DBkfEiQYQ=",
+            id: "featured pluto",
         },
     ],
 }
