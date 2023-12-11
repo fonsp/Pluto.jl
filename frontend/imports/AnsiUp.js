e3356a046b8ad302094a46379ec1bbfd45ffcf13
Fons van der Plas <fonsvdplas@gmail.com>
2023-12-11 15:55:24 +0100

    ugh

--- frontend/imports/AnsiUp.js
+++ frontend/imports/AnsiUp.js
@@ -1,5 +1,12 @@
 // @ts-ignore
-const AnsiUp = window?.AnsiUp ?? class {}
+const AnsiUp =
+    window?.AnsiUp ??
+    class {
+        ansi_to_html(value) {
+            throw new Error("Method not implemented.")
+            return ""
+        }
+    }
 
 // @ts-ignore
 export default AnsiUp
