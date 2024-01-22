4fdd0b76dde42f57b5f65a9fd0a1f84819f12999
Fons van der Plas <fonsvdplas@gmail.com>
2024-01-22 11:31:53 +0100

    Use animation-delay to hide the updates

--- frontend/components/PkgTerminalView.js
+++ frontend/components/PkgTerminalView.js
@@ -6,8 +6,11 @@ const make_spinner_spin = (original_html) => original_html.replaceAll("◐", `<s
 const TerminalViewAnsiUp = ({ value }) => {
     const node_ref = useRef(/** @type {HTMLElement?} */ (null))
 
+    const start_time = useRef(Date.now())
+
     useEffect(() => {
         if (!node_ref.current) return
+        node_ref.current.style.cssText = `--animation-delay: -${(Date.now() - start_time.current) % 1000}ms`
         node_ref.current.innerHTML = make_spinner_spin(new AnsiUp().ansi_to_html(value))
         const parent = node_ref.current.parentElement
         if (parent) parent.scrollTop = 1e5
