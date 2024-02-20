dec9a98b55be6a6a649610438d9f2e66568ccf90
Παναγιώτης Γεωργακόπουλος <pankgeorg@gmail.com>
2024-02-20 11:20:45 +0200

    fix(dialogs): backward compatible dialog usage (#2816)

--- frontend/common/useDialog.js
+++ frontend/common/useDialog.js
@@ -10,13 +10,17 @@ export const useDialog = () => {
     const dialog_ref = useRef(/** @type {HTMLDialogElement?} */ (null))
 
     useLayoutEffect(() => {
-        if (dialog_ref.current != null) dialogPolyfill.registerDialog(dialog_ref.current)
+        if (dialog_ref.current != null && typeof HTMLDialogElement !== "function") dialogPolyfill.registerDialog(dialog_ref.current)
     }, [dialog_ref.current])
 
     return useMemo(() => {
-        const open = () => dialog_ref.current?.showModal()
-        const close = () => dialog_ref.current?.close()
-        const toggle = () => (dialog_ref.current?.open === true ? dialog_ref.current?.close() : dialog_ref.current?.showModal())
+        const open = () => {
+            if (!dialog_ref.current?.open) dialog_ref.current?.showModal()
+        }
+        const close = () => {
+            if (dialog_ref.current?.open === true) dialog_ref.current?.close?.()
+        }
+        const toggle = () => (dialog_ref.current?.open === true ? dialog_ref.current?.close?.() : dialog_ref.current?.showModal?.())
 
         return [dialog_ref, open, close, toggle]
     }, [dialog_ref])
