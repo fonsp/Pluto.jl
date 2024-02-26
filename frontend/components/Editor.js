bb5dd11d344c0c6aceb8764e444a73539a1ec350
Connor Burns <ctrekker4@gmail.com>
2024-02-25 20:17:21 -0700

    Include secret when starting featured notebook

--- frontend/components/Editor.js
+++ frontend/components/Editor.js
@@ -904,7 +904,7 @@ patch: ${JSON.stringify(
 
         this.connect = (/** @type {string | undefined} */ ws_address = undefined) =>
             create_pluto_connection({
-                ws_address: ws_address,
+                ws_address: ws_address ?? ws_address_from_base(this.props.launch_params.pluto_server_url ?? window.location.href),
                 on_unrequested_update: on_update,
                 on_connection_status: on_connection_status,
                 on_reconnect: on_reconnect,
@@ -1382,7 +1382,7 @@ patch: ${JSON.stringify(
                       `article-view/${window?.version_info?.pluto ?? this.state.notebook.pluto_version ?? "unknown"}`
             )
         } else {
-            this.connect(lp.pluto_server_url ? ws_address_from_base(lp.pluto_server_url) : undefined)
+            this.connect()
         }
     }
 
