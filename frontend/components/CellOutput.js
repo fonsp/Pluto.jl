c3fcfe021f8874620a9c209a3b6a946691e18d1f
Fons van der Plas <fonsvdplas@gmail.com>
2024-02-26 16:39:31 +0100

    fix

--- frontend/components/CellOutput.js
+++ frontend/components/CellOutput.js
@@ -410,14 +410,14 @@ const execute_scripttags = async ({ root_node, script_nodes, previous_results_ma
                                 // @ts-ignore
                                 getPublishedObject: (id) => cell.getPublishedObject(id),
 
-                                _internal_getJSLinkResponse: (cell_id, link_id) => (input) => {
-                                    let [success, result] = pluto_actions.request_js_link_response(cell_id, link_id, input)
+                                _internal_getJSLinkResponse: (cell_id, link_id) => (input) =>
+                                    pluto_actions.request_js_link_response(cell_id, link_id, input).then(([success, result]) => {
+                                        console.debug({ success, result })
 
-                                    if (success) return result
-                                    console.error("JS link error received:", result)
-                                    throw result
-                                },
-
+                                        if (success) return result
+                                        console.error("JS link error received:", result)
+                                        throw result
+                                    }),
                                 getBoundElementValueLikePluto: get_input_value,
                                 setBoundElementValueLikePluto: set_input_value,
                                 getBoundElementEventNameLikePluto: eventof,
--- test/frontend/fixtures/with_js_link.jl
+++ test/frontend/fixtures/with_js_link.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.36
+# v0.19.40
 
 using Markdown
 using InteractiveUtils
