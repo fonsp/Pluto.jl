92cd7f7af1ed8b3b20c334849a44d2d383600ed8
Fons van der Plas <fonsvdplas@gmail.com>
2024-05-24 10:45:14 +0200

    Add featured notebooks from plutojl.org/en/docs (#2927)

--- frontend/featured_sources.js
+++ frontend/featured_sources.js
@@ -12,5 +12,11 @@ export default {
             url: "https://cdn.jsdelivr.net/gh/JuliaPluto/featured@v5/pluto_export.json",
             integrity: "sha256-+zI9b/gHEIJGV/DrckBY85hkxNWGIewgYffkAkEq4/w=",
         },
+        {
+            url: "https://plutojl.org/pluto_export.json",
+            // this is one month before the expiry date of our domain registration at njal.la
+            valid_until: "2025-10",
+            id: "pluto website",
+        },
     ],
 }
