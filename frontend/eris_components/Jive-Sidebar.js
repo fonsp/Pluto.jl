b3262e5d1bb08a622b34d9d095945bd783e24a2d
bmandracchia <biagio.mandracchia@gmail.com>
2024-06-20 21:27:53 +0200

    update - sidebar

--- frontend/eris_components/Jive-Sidebar.js
+++ frontend/eris_components/Jive-Sidebar.js
@@ -85,7 +85,7 @@ searchItem.addEventListener("search", function () {
 
 ///////////////// BUTTONS ///////////////////////
 
-// FILE
+////////// FILE /////////////
 const itemBarFile = document.createElement("div")
 
 const accButtonFile = document.createElement("button")
@@ -108,7 +108,7 @@ accItemFileLoadImage.className = "jv-bar-item jv-button"
 accItemFileLoadImage.innerHTML = " "
 accItemFileLoadImage.innerText += "Load Image"
 accItemFileLoadImage.onclick = function () {
-    createCellWithCode("str = JIVECore.Files.loadImageIntoDict() \n image_data[str]")
+    createCellWithCode("image_data[JIVECore.Files.loadImageIntoDict()]")
 }
 const accItemFileImportSequence = document.createElement("a")
 accItemFileImportSequence.href = "#"
@@ -116,7 +116,7 @@ accItemFileImportSequence.className = "jv-bar-item jv-button"
 accItemFileImportSequence.innerHTML = " "
 accItemFileImportSequence.innerText += "Import Sequence"
 accItemFileImportSequence.onclick = function () {
-    createCellWithCode("begin \n path2 = pick_file() \n img2 = load(path2)  \n Images.mosaicview(img2; fillvalue=0.5, npad=2, ncol=7, rowmajor=true) \n end")
+    createCellWithCode("Images.mosaicview(image_data[JIVECore.Files.loadImageIntoDict()]; fillvalue=0.5, npad=2, ncol=7, rowmajor=true)")
 }
 const accItemFileImportVideo = document.createElement("a")
 accItemFileImportVideo.href = "#"
@@ -124,7 +124,7 @@ accItemFileImportVideo.className = "jv-bar-item jv-button"
 accItemFileImportVideo.innerHTML = " "
 accItemFileImportVideo.innerText += "Import Video"
 accItemFileImportVideo.onclick = function () {
-    createCellWithCode("begin \n path3 = pick_file() \n img3 = load(path3)  \n imshow(img3) \n end;")
+    createCellWithCode("imshow(JIVECore.Files.loadImageIntoDict())")
 }
 
 accFile.appendChild(accItemFileLoadImage)
