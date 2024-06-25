3cc6ce96a8a8410d870dfc531fcef9a7192ce5b3
bmandracchia <biagio.mandracchia@gmail.com>
2024-06-25 17:52:42 +0200

    custom front page

--- frontend/components/welcome/Welcome.js
+++ frontend/components/welcome/Welcome.js
@@ -137,7 +137,10 @@ export const Welcome = ({ launch_params }) => {
 
     return html`
         <section id="title">
-            <h1>welcome to <img src=${url_logo_big} /></h1>
+            <h1><i>welcome to</i> <b>JIVE</b> <img src=${url_logo_big} /></h1>
+        </section>
+        <section id="subscript">
+            <p style="font-size:1em;"><i>The Julia notebooks are powered by</i> <b>Pluto.jl </b><img width='10' src=./img/favicon-16x16.png /></p>
         </section>
         <section id="mywork">
             <div>
