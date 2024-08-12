c633fc953d1c193953ed851317db87b737b5c0e8
Daniel Wennberg <daniel.wennberg@gmail.com>
2024-08-12 15:37:45 +0200

    Distinguish stdout and display contexts, see #2727

--- src/runner/PlutoRunner/src/PlutoRunner.jl
+++ src/runner/PlutoRunner/src/PlutoRunner.jl
@@ -1007,10 +1007,17 @@ const default_iocontext = IOContext(devnull,
     :pluto_with_js_link => (io, callback, on_cancellation) -> core_with_js_link(io, callback, on_cancellation),
 )
 
+# `stdout` mimics a TTY, the only relevant property is :color
 const default_stdout_iocontext = IOContext(devnull, 
-    :color => true, 
-    :limit => true, 
-    :displaysize => (18, 75), 
+    :color => true,
+    :is_pluto => false,
+)
+
+# `display` sees a richer context like in the REPL, see #2727
+const default_display_iocontext = IOContext(devnull,
+    :color => true,
+    :limit => true,
+    :displaysize => (18, 75),
     :is_pluto => false,
 )
 
@@ -2826,7 +2833,7 @@ function with_io_to_logs(f::Function; enabled::Bool=true, loglevel::Logging.LogL
     end
 
     # To make the `display` function work.
-    redirect_display = TextDisplay(pe_stdout)
+    redirect_display = TextDisplay(IOContext(pe_stdout, default_display_iocontext))
     pushdisplay(redirect_display)
 
     # Run the function `f`, capturing all output that it might have generated.
