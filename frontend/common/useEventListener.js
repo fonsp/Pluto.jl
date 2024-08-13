dfb37599f93fdc0bf8367daac8625b6a5ed55f08
Fons van der Plas <fonsvdplas@gmail.com>
2024-08-13 11:49:25 +0200

    Keyboard navigation for interactive array viewer

--- frontend/common/useEventListener.js
+++ frontend/common/useEventListener.js
@@ -1,15 +1,23 @@
 import { useCallback, useEffect } from "../imports/Preact.js"
 
+/**
+ * @typedef EventListenerAddable
+ * @type Document | HTMLElement | Window | EventSource | MediaQueryList | null
+ */
+
 export const useEventListener = (
-    /** @type {Document | HTMLElement | Window | EventSource | MediaQueryList | null} */ element,
+    /** @type {EventListenerAddable | import("../imports/Preact.js").Ref<EventListenerAddable>} */ element,
     /** @type {string} */ event_name,
     /** @type {EventListenerOrEventListenerObject} */ handler,
     /** @type {any[] | undefined} */ deps
 ) => {
     let handler_cached = useCallback(handler, deps)
     useEffect(() => {
-        if (element == null) return
-        element.addEventListener(event_name, handler_cached)
-        return () => element.removeEventListener(event_name, handler_cached)
+        /** @type {EventListenerAddable} */
+        const useme = element != null && "current" in element ? element.current : element
+
+        if (useme == null) return
+        useme.addEventListener(event_name, handler_cached)
+        return () => useme.removeEventListener(event_name, handler_cached)
     }, [element, event_name, handler_cached])
 }
