4be6c055dab29b3a3ade8e98fe478d0bee58c97d
Fons van der Plas <fonsvdplas@gmail.com>
2024-08-20 12:24:04 +0200

    moreee

--- src/runner/PlutoRunner/src/PlutoRunner.jl
+++ src/runner/PlutoRunner/src/PlutoRunner.jl
@@ -1,26 +1,3 @@
-# Will be evaluated _inside_ the workspace process.
-
-# Pluto does most things on the server, but it uses worker processes to evaluate notebook code in.
-# These processes don't import Pluto, they only import this module.
-# Functions from this module are called by WorkspaceManager.jl via Malt.
-
-# When reading this file, pretend that you are living in a worker process,
-# and you are communicating with Pluto's server, who lives in the main process.
-# The package environment that this file is loaded with is the NotebookProcessProject.toml file in this directory.
-
-# SOME EXTRA NOTES
-
-# 1. The entire PlutoRunner should be a single file.
-# 2. Restrict the communication between this PlutoRunner and the Pluto server to only use *Base Julia types*, like `String`, `Dict`, `NamedTuple`, etc. 
-
-# These restriction are there to allow flexibility in the way that this file is
-# loaded on a runner process, which is something that we might want to change
-# in the future.
-
-# DEVELOPMENT TIP
-# If you are editing this file, you cannot use Revise unfortunately.
-# However! You don't need to restart Pluto to test your changes! You just need to restart the notebook from the Pluto main menu, and the new PlutoRunner.jl will be loaded.
-
 module PlutoRunner
 
 # import these two so that they can be imported from Main on the worker process if it launches without the stdlibs in its LOAD_PATH
@@ -89,15 +66,16 @@ include("./display/tree viewer.jl")
 
 
 include("./integrations.jl")
-include("./completions.jl")
-include("./docs.jl")
+include("./ide features/completions.jl")
+include("./ide features/docs.jl")
 include("./bonds.jl")
 include("./js/published_to_js.jl")
 include("./display/embed_display.jl")
 include("./display/DivElement.jl")
 
 include("./js/jslink.jl")
-include("./logging.jl")
+include("./io/logging.jl")
+include("./io/stdout.jl")
 include("./precompile.jl")
 
 end
