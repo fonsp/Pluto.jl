ccb2c2a333a08b63e8e09917375a8051a3ef2193
Fons van der Plas <fonsvdplas@gmail.com>
2024-10-29 11:04:49 +0100

    Bundle Web Assets
    Built from hash 68ee31f5

---
+++ frontend-dist/index.1edba0a6.js
@@ -0,0 +1,123 @@
+var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},t={},o={},n=e.parcelRequire94c2;null==n&&((n=function(e){if(e in t)return t[e].exports;if(e in o){var n=o[e];delete o[e];var r={id:e,exports:{}};return t[e]=r,n.call(r.exports,r,r.exports),r.exports}var l=new Error("Cannot find module '"+e+"'");throw l.code="MODULE_NOT_FOUND",l}).register=function(e,t){o[e]=t},e.parcelRequire94c2=n);var r=n("1Tjky");n("7f8u5");var l,i,a,s,u={};l=u,i="Welcome",a=()=>G,s=e=>G=e,Object.defineProperty(l,i,{get:a,set:s,enumerable:!0,configurable:!0}),n("66N5W");r=n("1Tjky"),r=n("1Tjky");var c=n("14wLl");const d=async()=>{let e=await fetch("https://api.github.com/repos/fonsp/Pluto.jl/releases",{method:"GET",mode:"cors",cache:"no-cache",headers:{"Content-Type":"application/json"},redirect:"follow",referrerPolicy:"no-referrer"});return(await e.json()).reverse()};n("66N5W");r=n("1Tjky");var p=n("hKPYg"),h=n("5oZSF");r=n("1Tjky");const f=e=>{const t=`${e}\n`.replace("\r\n","\n"),o=t.indexOf("### A Pluto.jl notebook ###"),n=t.match(/# ... ........-....-....-....-............/g),r=(null==n?void 0:n.length)??0;let l=t.indexOf("# ╔═╡ Cell order:")+17+1;for(let e=1;e<=r;e++)l=t.indexOf("\n",l+1)+1;return t.slice(o,l)},m=({on_start_navigation:e})=>{const t=async t=>{var o;let n;if(console.log(t),(null===(o=((null==t?void 0:t.path)??(null==t?void 0:t.composedPath())).filter((e=>{var t;return null==e||null===(t=e.classList)||void 0===t?void 0:t.contains(".cm-editor")})))||void 0===o?void 0:o.length)>0)return;switch(t.type){case"paste":n=f(t.clipboardData.getData("text/plain"));break;case"dragstart":return void(t.dataTransfer.dropEffect="move");case"dragover":return void t.preventDefault();case"drop":t.preventDefault(),n=t.dataTransfer.types.includes("Files")?await(l=t.dataTransfer.files[0],new Promise(((e,t)=>{const{name:o,type:n}=l,r=new FileReader;r.onerror=()=>t("Failed to read file!"),r.onloadstart=()=>{},r.onprogress=({loaded:e,total:t})=>{},r.onload=()=>{},r.onloadend=()=>e({file:r.result,name:o,type:n}),r.readAsText(l)}))).then((({file:e})=>e)):f(await(r=t.dataTransfer.items[0],new Promise(((e,t)=>{try{r.getAsString((t=>{console.log(t),e(t)}))}catch(e){t(e)}}))))}var r,l;if(!n)return;e("notebook from clipboard",!1),document.body.classList.add("loading");const i=await fetch("./notebookupload",{method:"POST",body:n});if(i.ok)window.location.href=b(await i.text());else{let e=await i.blob();window.location.href=URL.createObjectURL(e)}};return h.useEventListener(document,"paste",t,[t]),h.useEventListener(document,"drop",t,[t]),h.useEventListener(document,"dragstart",t,[t]),h.useEventListener(document,"dragover",t,[t]),r.html`<span />`};var _=n("4ib27");const v=({client:e,connected:t,CustomPicker:o,show_samples:n,on_start_navigation:l})=>{const i=o??{text:"Open a notebook",placeholder:"Enter path or URL..."};return r.html`<${m} on_start_navigation=${l} />
+        <h2>${i.text}</h2>
+        <div id="new" class=${window.plutoDesktop?"desktop_opener":""}>
+            <${p.FilePicker}
+                key=${i.placeholder}
+                client=${e}
+                value=""
+                on_submit=${async e=>{const t=await _.guess_notebook_location(e);l(t.path_or_url),window.location.href=("path"===t.type?g:w)(t.path_or_url)}}
+                on_desktop_submit=${async e=>{var t;null===(t=window.plutoDesktop)||void 0===t||t.fileSystem.openNotebook("path")}}
+                clear_on_blur=${!1}
+                button_label=${window.plutoDesktop?"Open File":"Open"}
+                placeholder=${i.placeholder}
+            />
+            ${null!=window.plutoDesktop?r.html`<${p.FilePicker}
+                      key=${i.placeholder}
+                      client=${e}
+                      value=""
+                      on_desktop_submit=${async e=>{var t;null===(t=window.plutoDesktop)||void 0===t||t.fileSystem.openNotebook("url",e)}}
+                      button_label="Open from URL"
+                      placeholder=${i.placeholder}
+                  />`:null}
+        </div>`},g=(e,t=!1)=>"open?"+new URLSearchParams({path:e}).toString(),w=e=>"open?"+new URLSearchParams({url:e}).toString(),b=e=>"edit?id="+e;var k=n("66N5W"),y=(r=n("1Tjky"),r=n("1Tjky"),n("1XWhZ")),$=n("7Bvaz");const j=e=>({transitioning:!1,entry:void 0,path:e}),P=e=>({transitioning:!1,entry:e,path:e.path}),S=(e,t)=>e.split(/\/|\\/).slice(-t).join("/"),T=(e,t)=>{let o=1;for(const n of t)if(n!==e)for(;S(e,o)===S(n,o);)o++;return S(e,o)},E=({client:e,connected:t,remote_notebooks:o,CustomRecent:n,on_start_navigation:l})=>{const[i,a]=r.useState(null),s=r.useRef(i);s.current=i;const u=(e,t)=>{a((o=>(null==o?void 0:o.map((o=>o.path==e?{...o,...t}:o)))??null))};r.useEffect((()=>{null!=e&&t&&e.send("get_all_notebooks",{},{}).then((({message:e})=>{const t=e.notebooks.map((e=>P(e))),o=L(),n=[...k.default.sortBy(t,[e=>k.default.findIndex([...o,...t],(t=>t.path===e.path))]),...k.default.differenceBy(o,t,(e=>e.path))];a(n),document.body.classList.remove("loading")}))}),[null!=e&&t]),r.useEffect((()=>{const e=o;if(null!=s.current){const t=[],o=s.current.map((o=>{let n=null;if(n=null!=o.entry?e.find((e=>{var t;return e.notebook_id===(null===(t=o.entry)||void 0===t?void 0:t.notebook_id)})):e.find((e=>e.path===o.path)),null==n)return j(o.path);{const e=P(n);return t.push(n),e}})),n=e.filter((e=>!t.includes(e))).map(P);a([...n,...o])}}),[o]);r.useEffect((()=>{document.body.classList.toggle("nosessions",!(null==i||i.length>0))}),[i]);const c=null==i?void 0:i.map((e=>e.path));let d=null==i?r.html`<li class="not_yet_ready"><em>Loading...</em></li>`:i.map((t=>{var o,n;const i=null!=t.entry;return r.html`<li
+                      key=${t.path}
+                      class=${y.cl({running:i,recent:!i,transitioning:t.transitioning})}
+                  >
+                      <button
+                          onclick=${()=>(t=>{if(t.transitioning)return;if(null!=t.entry){var o;if(null==e)return;confirm((null===(o=t.entry)||void 0===o?void 0:o.process_status)===$.ProcessStatus.waiting_for_permission?"Close notebook session?":"Shut down notebook process?")&&(u(t.path,{running:!1,transitioning:!0}),e.send("shutdown_notebook",{keep_in_session:!1},{notebook_id:null===(o=t.entry)||void 0===o?void 0:o.notebook_id},!1))}else u(t.path,{transitioning:!0}),fetch(g(t.path)+"&execution_allowed=true",{method:"GET"}).then((e=>{if(!e.redirected)throw new Error("file not found maybe? try opening the notebook directly")})).catch((e=>{console.error("Failed to start notebook in background"),console.error(e),u(t.path,{transitioning:!1,notebook_id:null})}))})(t)}
+                          title=${i?(null===(o=t.entry)||void 0===o?void 0:o.process_status)===$.ProcessStatus.waiting_for_permission?"Stop session":"Shut down notebook":"Start notebook in background"}
+                      >
+                          <span class="ionicon"></span>
+                      </button>
+                      <a
+                          href=${i?b(null===(n=t.entry)||void 0===n?void 0:n.notebook_id):g(t.path)}
+                          title=${t.path}
+                          onClick=${e=>{i||(l(T(t.path,c)),u(t.path,{transitioning:!0}))}}
+                          >${T(t.path,c)}</a
+                      >
+                  </li>`}));return null==n?r.html`
+            <h2>My work</h2>
+            <ul id="recent" class="show_scrollbar">
+                <li class="new">
+                    <a
+                        href="new"
+                        onClick=${e=>{l("new notebook")}}
+                        ><button><span class="ionicon"></span></button>Create a <strong>new notebook</strong></a
+                    >
+                </li>
+                ${d}
+            </ul>
+        `:r.html`<${n} cl=${y.cl} combined=${i} client=${e} recents=${d} />`},L=()=>{const e=localStorage.getItem("recent notebooks"),t=null!=e?JSON.parse(e):[];return(t instanceof Array?t:[]).map(j)};k=n("66N5W"),r=n("1Tjky");var x=n("gHc5c"),C=n("1tz5b");const O=[{title:"Featured Notebooks",description:"These notebooks from the Julia community show off what you can do with Pluto. Give it a try, you might learn something new!",collections:[{title:"Loading...",tags:[]}],notebooks:{}}],R=r.html`
+    <div class="featured-source">
+        <h1>${O[0].title}</h1>
+        <p>Here are a couple of notebooks to get started with Pluto.jl:</p>
+        <ul>
+            <li>1. <a href="sample/Getting%20started.jl">Getting started</a></li>
+            <li>2. <a href="sample/Markdown.jl">Markdown</a></li>
+            <li>3. <a href="sample/Basic%20mathematics.jl">Basic mathematics</a></li>
+            <li>4. <a href="sample/Interactivity.jl">Interactivity</a></li>
+            <li>5. <a href="sample/PlutoUI.jl.jl">PlutoUI.jl</a></li>
+            <li>6. <a href="sample/Plots.jl.jl">Plots.jl</a></li>
+            <li>7. <a href="sample/Tower%20of%20Hanoi.jl">Tower of Hanoi</a></li>
+            <li>8. <a href="sample/JavaScript.jl">JavaScript</a></li>
+        </ul>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p>Tip: <em>Visit this page again when you are connected to the internet to read our online collection of featured notebooks.</em></p>
+    </div>
+`,N=[{title:"Notebooks",tags:"everything"}],D=e=>(null==e?void 0:e.id)??e.url,A=({sources:e,direct_html_links:t})=>{const[o,n]=r.useState({});r.useEffect((()=>{if(null!=e){i(!1),n({});const t=Array.from(new Set(e.map(D))).map((t=>{const o=e.filter((e=>D(e)===t));return I(o.map((async e=>{const{url:o,integrity:n,valid_until:r}=e;if(null!=r&&new Date(r)<new Date)throw new Error(`Source ${o} is expired with valid_until ${r}`);const l=await(await fetch(new Request(o,{integrity:n??void 0}))).json();if("1"!==l.format_version)throw new Error(`Invalid format version: ${l.format_version}`);return[l,t,o]}))).then((([e,t,o])=>{n((n=>({...n,[t]:{...e,source_url:o}})))}))}));Promise.any(t).catch((e=>{console.error("All featured sources failed to load: ",e),((null==e?void 0:e.errors)??[]).forEach((e=>console.error(e))),i(!0)}))}}),[e]),r.useEffect((()=>{Object.entries(o).length>0&&console.log("Sources:",o)}),[o]);const[l,i]=r.useState(!1);r.useEffect((()=>{setTimeout((()=>{i(!0)}),8e3)}),[]);const a=0===Object.entries(o).length,s=Array.from(new Set((null==e?void 0:e.map(D))??[])).map((e=>o[e])).filter((e=>null!=e));return a&&l?R:r.html`
+              ${(a?O:s).map((e=>{let o=(null==e?void 0:e.collections)??N;return r.html`
+                      <div class="featured-source">
+                          <h1>${e.title}</h1>
+                          <p>${e.description}</p>
+                          ${o.map((o=>r.html`
+                                  <div class="collection">
+                                      <h2>${o.title}</h2>
+                                      <p>${o.description}</p>
+                                      <div class="card-list">
+                                          ${F(Object.values(e.notebooks),o.tags??[]).map((o=>r.html`<${C.FeaturedCard} entry=${o} source_manifest=${e} direct_html_links=${t} />`))}
+                                      </div>
+                                  </div>
+                              `))}
+                      </div>
+                  `}))}
+          `};x.default(A,"pluto-featured",["sources","direct_html_links"]);const F=(e,t)=>{const o="everything"===t?e:e.filter((e=>t.some((t=>{var o;return((null===(o=e.frontmatter)||void 0===o?void 0:o.tags)??[]).includes(t)}))));return k.default.sortBy(o,[e=>{var t;return(e=>isNaN(e)?e:Number(e))(null==e||null===(t=e.frontmatter)||void 0===t?void 0:t.order)},"id"])},I=(e,t=[])=>e.length<=1?Promise.any([...e,...t]):e[0].catch((()=>I(e.slice(1),[...t,e[0]])));var U,q=n("hSemq"),W={sources:[{url:"https://featured.plutojl.org/pluto_export.json",valid_until:"2025-10",id:"featured pluto"},{id:"featured pluto",url:"https://cdn.jsdelivr.net/gh/JuliaPluto/featured@v5/pluto_export.json",integrity:"sha256-+zI9b/gHEIJGV/DrckBY85hkxNWGIewgYffkAkEq4/w="},{url:"https://plutojl.org/pluto_export.json",valid_until:"2025-10",id:"pluto website"}]};const B=(null===(U=document.head.querySelector("link[rel='pluto-logo-big']"))||void 0===U?void 0:U.getAttribute("href"))??"",G=({launch_params:e})=>{const[t,o]=r.useState([]),[n,l]=r.useState(!1),[i,a]=r.useState({show_samples:!0,CustomPicker:null,CustomRecent:null}),s=r.useRef({});r.useEffect((()=>{const t=l;c.create_pluto_connection({on_unrequested_update:({message:e,type:t})=>{"notebook_list"===t&&o(e.notebooks)},on_connection_status:t,on_reconnect:async()=>!0,ws_address:e.pluto_server_url?c.ws_address_from_base(e.pluto_server_url):void 0}).then((async e=>{Object.assign(s.current,e),l(!0);try{const t=await q.get_environment(e),{custom_recent:o,custom_filepicker:n,show_samples:l=!0}=t({client:e,editor:void 0,imports:{preact:r}});a((e=>({...e,CustomRecent:o,CustomPicker:n,show_samples:l})))}catch(e){}(e=>{d().then((t=>{const o=e.version_info.pluto,n=t[t.length-1].tag_name;console.log(`Pluto version ${o}`);const r=t.findIndex((e=>e.tag_name===o));-1!==r&&t.slice(r+1).filter((e=>e.body.toLowerCase().includes("recommended update"))).length>0&&(console.log(`Newer version ${n} is available`),e.version_info.dismiss_update_notification||alert("A new version of Pluto.jl is available! 🎉\n\n    You have "+o+", the latest is "+n+'.\n\nYou can update Pluto.jl using the julia package manager:\n    import Pkg; Pkg.update("Pluto")\nAfterwards, exit Pluto.jl and restart julia.'))})).catch((()=>{}))})(e),e.send("current_time"),e.send("completepath",{query:""},{})}))}),[]);const{show_samples:u,CustomRecent:p,CustomPicker:h}=i,[f,m]=r.useState(null),_=(e,t=!0)=>{if(t){const t=t=>{m(e)};window.addEventListener("beforeunload",t),setTimeout((()=>window.removeEventListener("beforeunload",t)),1e3)}else m(e)},g=r.useMemo((()=>e.featured_sources??(e.featured_source_url?[{url:e.featured_source_url,integrity:e.featured_source_integrity}]:W.sources)),[e]);return null!=f?r.html`
+            <div class="navigating-away-banner">
+                <h2>Loading ${f}...</h2>
+            </div>
+        `:r.html`
+        <section id="title">
+            <h1>welcome to <img src=${B} /></h1>
+        </section>
+        <section id="mywork">
+            <div>
+                <${E}
+                    client=${s.current}
+                    connected=${n}
+                    remote_notebooks=${t}
+                    CustomRecent=${p}
+                    on_start_navigation=${_}
+                />
+            </div>
+        </section>
+        <section id="open">
+            <div>
+                <${v}
+                    client=${s.current}
+                    connected=${n}
+                    CustomPicker=${h}
+                    show_samples=${u}
+                    on_start_navigation=${_}
+                />
+            </div>
+        </section>
+        <section id="featured">
+            <div>
+                <${A} sources=${g} direct_html_links=${e.featured_direct_html_links} />
+            </div>
+        </section>
+    `},J=new URLSearchParams(window.location.search),H={featured_direct_html_links:!!(J.get("featured_direct_html_links")??window.pluto_featured_direct_html_links),featured_sources:window.pluto_featured_sources,featured_source_url:J.get("featured_source_url")??window.pluto_featured_source_url,featured_source_integrity:J.get("featured_source_integrity")??window.pluto_featured_source_integrity,pluto_server_url:J.get("pluto_server_url")??window.pluto_server_url};console.log("Launch parameters: ",H),r.render(r.html`<${u.Welcome} launch_params=${H} />`,document.querySelector("#app"));
