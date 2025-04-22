5fe7e9195f1c225797e8510da681ebb118033b19
Ashlin Harris <90787010+AshlinHarris@users.noreply.github.com>
2025-04-22 09:01:35 -0400

    Zero out motivational_word_probability

--- frontend/components/ErrorMessage.js
+++ frontend/components/ErrorMessage.js
@@ -550,7 +550,7 @@ const get_first_package = (limited_stacktrace) => {
     }
 }
 
-const motivational_word_probability = 0.1
+const motivational_word_probability = 0.0
 const motivational_words = [
     //
     "Don't panic!",
