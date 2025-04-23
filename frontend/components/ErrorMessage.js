92023e792ee63321aa55eeafab4f5b78265427cc
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliahub.com>
2025-04-23 13:16:11 +0300

    chore: reset the motivational_word_probability to 0.1

--- frontend/components/ErrorMessage.js
+++ frontend/components/ErrorMessage.js
@@ -552,7 +552,7 @@ const get_first_package = (limited_stacktrace) => {
     }
 }
 
-const motivational_word_probability = 1
+const motivational_word_probability = 0.1
 const motivational_words = [
     //
     "Don't panic!",
