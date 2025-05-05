3de270ca6dcd35dcf28dd5c7a923be6b10e76d2a
Fons van der Plas <fonsvdplas@gmail.com>
2025-05-05 16:16:54 +0200

    recursive cell deps

--- frontend/components/AIContext.js
+++ frontend/components/AIContext.js
@@ -1,15 +1,10 @@
 import _ from "../imports/lodash.js"
-import { html, useState, useEffect, useMemo, useRef, useContext, useLayoutEffect, useErrorBoundary, useCallback } from "../imports/Preact.js"
+import { html, useState, useRef, useContext } from "../imports/Preact.js"
 
-import { CellOutput } from "./CellOutput.js"
-import { CellInput } from "./CellInput.js"
-import { Logs } from "./Logs.js"
-import { RunArea, useDebouncedTruth } from "./RunArea.js"
 import { cl } from "../common/ClassTable.js"
 import { PlutoActionsContext } from "../common/PlutoContext.js"
-import { SafePreviewOutput } from "./SafePreviewUI.js"
 import { useEventListener } from "../common/useEventListener.js"
-import { julia } from "../imports/CodemirrorPlutoSetup.js"
+import { upstream_recursive } from "../common/SliderServerClient.js"
 
 const format_code = (s) =>
     s == null
@@ -49,6 +44,8 @@ export const AIContext = ({ cell_id, current_code }) => {
     const default_question = notebook.cell_results[cell_id]?.errored === true ? "Why does this cell error?" : ""
     const [userQuestion, setUserQuestion] = useState(default_question)
 
+    const recursive = true
+
     const current_cell = `
 <pluto-ai-context-current-cell>
 The current cell has the following code:
@@ -59,16 +56,21 @@ ${format_cell_output(notebook.cell_results[cell_id])}
 </pluto-ai-context-current-cell>
 `
 
-    const upstream = notebook.cell_dependencies[cell_id]?.upstream_cells_map ?? {}
-    const upstream_from_nb = Object.fromEntries(Object.entries(upstream).filter(([variable, upstream_cell_ids]) => upstream_cell_ids.length > 0))
+    const graph = notebook.cell_dependencies
 
     // Get the list of upstream cells, in the order that they appear in the notebook.
-    const upstream_cells_raw = Object.values(upstream_from_nb).flatMap((x) => x)
-    const upstream_cells = notebook.cell_order.filter((cid) => upstream_cells_raw.includes(cid))
+    const upstream_cellids = upstream_recursive(graph, [cell_id], { recursive })
+    const upstream_cells = notebook.cell_order.filter((cid) => upstream_cellids.has(cid))
+
+    // Get the variables that are used in the current cell, which are defined in other cells.
+    const upstream_direct = notebook.cell_dependencies[cell_id]?.upstream_cells_map ?? {}
+    const variables_used_from_upstream = Object.entries(upstream_direct)
+        .filter(([_var, upstream_cell_ids]) => upstream_cell_ids.length > 0)
+        .map(([variable]) => variable)
 
     const variable_context = `
 <pluto-ai-context-variables>
-The current cell uses the following variables from other cells: ${Object.keys(upstream_from_nb).join(", ")}.
+The current cell uses the following variables from other cells: ${variables_used_from_upstream.join(", ")}.
 
 These variables are defined in the following cells:
 
@@ -115,6 +117,7 @@ ${packages_context(notebook)}
                 name="pluto-ai-context-question"
                 class="ai-context-question-input"
                 placeholder="Type your question here..."
+                autocomplete="off"
                 value=${userQuestion}
                 onInput=${(e) => setUserQuestion(e.target.value)}
             />
@@ -129,7 +132,12 @@ ${packages_context(notebook)}
                 >
                     ${copied ? "Copied!" : "Copy"}
                 </button>
-                <div class="ai-context-prompt">
+                <div
+                    class=${cl({
+                        "ai-context-prompt": true,
+                        "ai-context-prompt-with-question": userQuestion.length > 0,
+                    })}
+                >
                     <pre>${prompt.trim()}</pre>
                 </div>
             </div>
@@ -148,6 +156,19 @@ const cell_output_to_plaintext = (/** @type {import("./Editor.js").CellResultDat
         return cell_output.body.plain_error
     }
     if (cell_output.mime.includes("image")) return "<!-- Image -->"
+
+    if (cell_output.mime === "text/html") {
+        try {
+            return JSON.stringify(cell_result, (key, value) => {
+                if (typeof value === "string" && value.length > 800) {
+                    return value.substring(0, 700) + `... <!-- ${value.length - 700 - 20} CHARACTERS TRUNCATED --> ... ` + value.substring(value.length - 20)
+                }
+                return value
+            })
+        } catch (e) {
+            return "<!-- HTML content that couldn't be stringified -->"
+        }
+    }
 
     return JSON.stringify(cell_output)
 }
