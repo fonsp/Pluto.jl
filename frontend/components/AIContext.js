2f34a3283a8644e2edefeee42ddf89033c5168eb
Fons van der Plas <fonsvdplas@gmail.com>
2025-05-05 16:41:22 +0200

    refactor, stay below 3k token limit

--- frontend/components/AIContext.js
+++ frontend/components/AIContext.js
@@ -13,8 +13,8 @@ const format_code = (s) =>
 ${s}
 </julia-code-block>`
 
-const format_cell_output = (/** @type {import("./Editor.js").CellResultData?} */ cell_result) => {
-    const text = cell_output_to_plaintext(cell_result)
+const format_cell_output = (/** @type {import("./Editor.js").CellResultData?} */ cell_result, /** @type {number} */ truncate_limit) => {
+    const text = cell_output_to_plaintext(cell_result, truncate_limit)
 
     return text == null
         ? ""
@@ -46,50 +46,24 @@ export const AIContext = ({ cell_id, current_code }) => {
 
     const recursive = true
 
-    const current_cell = `
-<pluto-ai-context-current-cell>
-The current cell has the following code:
-
-${format_code(current_code)}
-
-${format_cell_output(notebook.cell_results[cell_id])}
-</pluto-ai-context-current-cell>
-`
-
-    const graph = notebook.cell_dependencies
-
-    // Get the list of upstream cells, in the order that they appear in the notebook.
-    const upstream_cellids = upstream_recursive(graph, [cell_id], { recursive })
-    const upstream_cells = notebook.cell_order.filter((cid) => upstream_cellids.has(cid))
-
-    // Get the variables that are used in the current cell, which are defined in other cells.
-    const upstream_direct = notebook.cell_dependencies[cell_id]?.upstream_cells_map ?? {}
-    const variables_used_from_upstream = Object.entries(upstream_direct)
-        .filter(([_var, upstream_cell_ids]) => upstream_cell_ids.length > 0)
-        .map(([variable]) => variable)
-
-    const variable_context = `
-<pluto-ai-context-variables>
-The current cell uses the following variables from other cells: ${variables_used_from_upstream.join(", ")}.
-
-These variables are defined in the following cells:
-
-${upstream_cells.map((cid) => format_code(notebook.cell_inputs[cid].code)).join("\n\n")}
-</pluto-ai-context-variables>
-`
-
-    const prompt = `${userQuestion}
-
-<pluto-ai-context>
-To help me answer my question, here is some auto-generated context. The code is from a Pluto Julia notebook. We are concerned with one specific cell in the notebook, called "the current cell". And you will get additional context.
-
-${current_cell}
-
-${upstream_cells.length > 0 ? variable_context : ""}
-
-${packages_context(notebook)}
-</pluto-ai-context>
-`
+    const prompt_args = {
+        userQuestion,
+        recursive,
+        notebook,
+        cell_id,
+        current_code,
+    }
+    let prompt = generate_prompt(prompt_args)
+    let prompt_tokens = count_openai_tokens(prompt)
+    console.log("Prompt is too long, truncating...", prompt, prompt_tokens)
+    if (prompt_tokens > 3000) {
+        prompt = generate_prompt({
+            ...prompt_args,
+            recursive: false,
+            truncate_limit_current_cell: 1000,
+        })
+        prompt_tokens = count_openai_tokens(prompt)
+    }
 
     const copyToClipboard = async () => {
         try {
@@ -145,7 +119,67 @@ ${packages_context(notebook)}
     `
 }
 
-const cell_output_to_plaintext = (/** @type {import("./Editor.js").CellResultData?} */ cell_result) => {
+/**
+ * @param {{
+ *     userQuestion: string,
+ *     recursive: boolean,
+ *     notebook: import("./Editor.js").NotebookData,
+ *     cell_id: string,
+ *     current_code: string,
+ *     truncate_limit_current_cell?: number,
+ * }} props
+ * @returns {string}
+ */
+const generate_prompt = ({ userQuestion, recursive, notebook, cell_id, current_code, truncate_limit_current_cell = 800 }) => {
+    const current_cell = `
+<pluto-ai-context-current-cell>
+The current cell has the following code:
+
+${format_code(current_code)}
+
+${format_cell_output(notebook.cell_results[cell_id], truncate_limit_current_cell)}
+</pluto-ai-context-current-cell>
+`
+
+    const graph = notebook.cell_dependencies
+
+    // Get the list of upstream cells, in the order that they appear in the notebook.
+    const upstream_cellids = upstream_recursive(graph, [cell_id], { recursive })
+    const upstream_cells = notebook.cell_order.filter((cid) => upstream_cellids.has(cid))
+
+    // Get the variables that are used in the current cell, which are defined in other cells.
+    const upstream_direct = notebook.cell_dependencies[cell_id]?.upstream_cells_map ?? {}
+    const variables_used_from_upstream = Object.entries(upstream_direct)
+        .filter(([_var, upstream_cell_ids]) => upstream_cell_ids.length > 0)
+        .map(([variable]) => variable)
+
+    const variable_context = `
+<pluto-ai-context-variables>
+The current cell uses the following variables from other cells: ${variables_used_from_upstream.join(", ")}.
+
+These variables are defined in the following cells:
+
+${upstream_cells.map((cid) => format_code(notebook.cell_inputs[cid].code)).join("\n\n")}
+</pluto-ai-context-variables>
+`
+
+    const prompt = `${userQuestion}
+
+<pluto-ai-context>
+To help me answer my question, here is some auto-generated context. The code is from a Pluto Julia notebook. We are concerned with one specific cell in the notebook, called "the current cell". And you will get additional context.
+
+${current_cell}
+
+${upstream_cells.length > 0 ? variable_context : ""}
+
+${packages_context(notebook)}
+</pluto-ai-context>
+`
+
+    return prompt
+}
+
+const cell_output_to_plaintext = (/** @type {import("./Editor.js").CellResultData?} */ cell_result, /** @type {number} */ truncate_limit) => {
     if (cell_result == null) return null
 
     const cell_output = cell_result.output
@@ -160,8 +194,12 @@ const cell_output_to_plaintext = (/** @type {import("./Editor.js").CellResultDat
     if (cell_output.mime === "text/html") {
         try {
             return JSON.stringify(cell_result, (key, value) => {
-                if (typeof value === "string" && value.length > 800) {
-                    return value.substring(0, 700) + `... <!-- ${value.length - 700 - 20} CHARACTERS TRUNCATED --> ... ` + value.substring(value.length - 20)
+                if (typeof value === "string" && value.length > truncate_limit) {
+                    return (
+                        value.substring(0, truncate_limit / 2) +
+                        `... <!-- ${value.length - truncate_limit / 2 - 20} CHARACTERS TRUNCATED --> ... ` +
+                        value.substring(value.length - 20)
+                    )
                 }
                 return value
             })
@@ -171,4 +209,15 @@ const cell_output_to_plaintext = (/** @type {import("./Editor.js").CellResultDat
     }
 
     return JSON.stringify(cell_output)
+}
+
+/** Rough heuristic for counting tokens in a string. */
+const count_openai_tokens = (text) => {
+    const num_seps = text.match(/[^\p{L}]+/gmu)?.length ?? 0
+    const val1 = num_seps * 2.3
+
+    const val2 = text.length * 0.29
+
+    // Average
+    return (val1 + val2) / 2
 }
