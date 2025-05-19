c44821dc60dee6b1dbf1ff0336c720824f0186a5
Fons van der Plas <fonsvdplas@gmail.com>
2025-05-19 15:10:28 +0200

    tweaks

--- frontend/common/SetupCellEnvironment.js
+++ frontend/common/SetupCellEnvironment.js
@@ -1,9 +1,9 @@
 // @ts-ignore
 import { Library } from "https://cdn.jsdelivr.net/npm/@observablehq/stdlib@3.3.1/+esm"
 
-export let make_library = () => {
+export const make_library = () => {
     // @ts-ignore
-    let library = new Library()
+    const library = new Library()
     return {
         DOM: library.DOM,
         Files: library.Files,
@@ -28,11 +28,11 @@ const observablehq_for_myself = make_library()
 export const observablehq_for_cells = make_library()
 export { observablehq_for_myself as default }
 
-export let DOM = observablehq_for_myself.DOM
-export let Files = observablehq_for_myself.Files
-export let Generators = observablehq_for_myself.Generators
-export let Promises = observablehq_for_myself.Promises
-export let now = observablehq_for_myself.now
-export let svg = observablehq_for_myself.svg
-export let html = observablehq_for_myself.html
-export let require = observablehq_for_myself.require
+export const DOM = observablehq_for_myself.DOM
+export const Files = observablehq_for_myself.Files
+export const Generators = observablehq_for_myself.Generators
+export const Promises = observablehq_for_myself.Promises
+export const now = observablehq_for_myself.now
+export const svg = observablehq_for_myself.svg
+export const html = observablehq_for_myself.html
+export const require = observablehq_for_myself.require
