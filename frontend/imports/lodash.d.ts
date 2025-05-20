f4e000d06a4f2f48c1a384993faa2c5f183b53a3
Fons van der Plas <fonsvdplas@gmail.com>
2025-05-20 12:59:03 +0200

    asfd

--- frontend/imports/lodash.d.ts
+++ frontend/imports/lodash.d.ts
@@ -1,2 +1,4 @@
 import * as _ from "lodash-es"
 export default _
+
+// Note: run `npm install` if you want to get types in your editor
--- frontend/imports/lodash.js
+++ frontend/imports/lodash.js
@@ -1,3 +1,5 @@
 // @ts-ignore
 import _ from "https://cdn.jsdelivr.net/npm/lodash-es@4.17.21/+esm"
 export default _
+
+// Note: run `npm install` if you want to get types in your editor
