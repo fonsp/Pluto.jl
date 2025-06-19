42aec2dbba5367038f14bdf3a1dc528c89f04c3c
Fons van der Plas <fonsvdplas@gmail.com>
2025-06-19 11:25:15 +0200

    hardcoded

--- frontend/components/CellInput/pluto_autocomplete.js
+++ frontend/components/CellInput/pluto_autocomplete.js
@@ -8,6 +8,7 @@ import { open_bottom_right_panel } from "../BottomRightPanel.js"
 import { ENABLE_CM_AUTOCOMPLETE_ON_TYPE } from "../CellInput.js"
 import { GlobalDefinitionsFacet } from "./go_to_definition_plugin.js"
 import { STRING_NODE_NAMES } from "./mixedParsers.js"
+import { sub_charmap, sup_charmap } from "../../common/UnicodeTools.js"
 
 let { autocompletion, completionKeymap, completionStatus, acceptCompletion, selectedCompletion } = autocomplete
 
@@ -561,78 +562,49 @@ const apply_completion = (view, completion, from, to) => {
 }
 
 const special_symbols_completion = (/** @type {() => Promise<SpecialSymbols?>} */ request_special_symbols) => {
-    let found = null
-    const get_special_symbols = async () => {
-        if (found == null) {
-            found = request_special_symbols().then((data) => {
-                if (data != null) {
-                    const { latex, emoji } = data
-                    return [emoji, latex].flatMap((map) =>
-                        Object.entries(map).map(([label, value]) => {
-                            return {
-                                label,
-                                apply: apply_completion,
-                                detail: value ?? undefined,
-                                type: "c_special_symbol",
-                                boost: label === "\\in" ? 3 : special_latex_examples.includes(label) ? 2 : special_emoji_examples.includes(value) ? 1 : 0,
-                            }
-                        })
-                    )
-                }
-            })
+    const list = request_special_symbols().then((data) => {
+        if (data != null) {
+            const { latex, emoji } = data
+            return [emoji, latex].flatMap((map) =>
+                Object.entries(map).map(([label, value]) => {
+                    return {
+                        label,
+                        apply: apply_completion,
+                        detail: value ?? undefined,
+                        type: "c_special_symbol",
+                        boost: label === "\\in" ? 3 : special_latex_examples.includes(label) ? 2 : special_emoji_examples.includes(value) ? 1 : 0,
+                    }
+                })
+            )
         }
-        return found
-    }
+    })
 
     return async (/** @type {autocomplete.CompletionContext} */ ctx) => {
         if (!match_latex_symbol_complete(ctx)) return null
         if (!ctx.explicit && writing_variable_name_or_keyword(ctx)) return null
         if (not_explicit_and_too_boring(ctx, true)) return null
-        const result = await get_special_symbols()
-        return await autocomplete.completeFromList(result ?? [])(ctx)
+        return await autocomplete.completeFromList((await list) ?? [])(ctx)
     }
 }
 
-const superscript_subscript_completion = (/** @type {() => Promise<SpecialSymbols?>} */ request_special_symbols) => {
-    let found = null
-    const get_special_symbols = () => {
-        if (found == null) {
-            found = request_special_symbols().then((data) => {
-                if (data != null) {
-                    const { latex } = data
-                    const sups = Object.entries(latex).filter(([label]) => label.match(/^\\\^.$/))
-                    const subs = Object.entries(latex).filter(([label]) => label.match(/^\\\_.$/))
+const superscript_subscript_completion = () => {
+    const match_sup = new RegExp(
+        `\\\\\\\^([${Object.keys(sup_charmap)
+            .map((x) => (x.match(/[\w\d]/) ? x : `\\${x}`))
+            .join("")}]{2,})$`
+    )
+    const match_sub = new RegExp(
+        `\\\\\\\_([${Object.keys(sub_charmap)
+            .map((x) => (x.match(/[\w\d]/) ? x : `\\${x}`))
+            .join("")}]{2,})$`
+    )
 
-                    const sup_chars = sups.map(([label]) => label.slice(-1))
-                    const sub_chars = subs.map(([label]) => label.slice(-1))
-
-                    const match_sup = new RegExp(`\\\\\\\^([${sup_chars.map((x) => (x.match(/[\w\d]/) ? x : `\\${x}`)).join("")}]{2,})$`)
-                    const match_sub = new RegExp(`\\\\\\\_([${sub_chars.map((x) => (x.match(/[\w\d]/) ? x : `\\${x}`)).join("")}]{2,})$`)
-
-                    const to_charmap = (x) => Object.fromEntries(x.map(([label, value]) => [label.slice(-1), value]))
-                    return {
-                        sups: to_charmap(sups),
-                        subs: to_charmap(subs),
-                        match_sup,
-                        match_sub,
-                    }
-                }
-            })
-        }
-        return found
-    }
-
-    return async (/** @type {autocomplete.CompletionContext} */ ctx) => {
-        const result = await get_special_symbols()
-        if (!result) return null
-
-        const { sups, subs, match_sub, match_sup } = result
-        console.log("HERE IS THE DICT THAT YOU WANT ", result)
+    return (/** @type {autocomplete.CompletionContext} */ ctx) => {
         const sup_match_result = ctx.matchBefore(match_sup)
         const sub_match_result = ctx.matchBefore(match_sub)
 
         let match_result = sup_match_result ?? sub_match_result
-        let dict = sup_match_result != null ? sups : subs
+        let dict = sup_match_result != null ? sup_charmap : sub_charmap
 
         if (match_result) {
             const { text, from, to } = match_result
@@ -734,7 +706,7 @@ export let pluto_autocomplete = ({
             override: [
                 global_variables_completion(request_unsubmitted_global_definitions, cell_id),
                 special_symbols_completion(get_special_symbols_debounced),
-                superscript_subscript_completion(get_special_symbols_debounced),
+                superscript_subscript_completion(),
                 julia_code_completions_to_cm(memoize_last_request_autocomplete),
                 complete_keyword,
                 complete_package_name(request_packages),
