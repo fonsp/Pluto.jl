70ff4faa081d95adfb40d0e38f1c91a8741f84cf
Fons van der Plas <fonsvdplas@gmail.com>
2025-06-24 17:02:23 +0200

    🧾 Options to store notebook in visual order or executable order

--- src/notebook/Notebook.jl
+++ src/notebook/Notebook.jl
@@ -69,6 +69,7 @@ Base.@kwdef mutable struct Notebook
     bonds::Dict{Symbol,BondValue}=Dict{Symbol,BondValue}()
 
     metadata::Dict{String, Any}=copy(DEFAULT_NOTEBOOK_METADATA)
+    store_in_executable_order::Bool=true
 end
 
 function _initial_nb_status()
