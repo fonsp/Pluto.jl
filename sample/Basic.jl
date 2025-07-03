172ed692f599bd2cf71e2a87c21886b3f55dc680
Fons van der Plas <fonsvdplas@gmail.com>
2025-07-03 15:40:56 +0200

    Discard changes to sample/Basic.jl

--- sample/Basic.jl
+++ sample/Basic.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.20.14
+# v0.14.0
 
 using Markdown
 using InteractiveUtils
@@ -21,14 +21,14 @@ converges to
 ```
 """
 
-# ╔═╡ b2d792c2-7f73-11ea-0c65-a5042701e9f3
-sqrt(sum(seq) * 6.0)
-
 # ╔═╡ b2d79330-7f73-11ea-0d1c-a9aad1efaae1
 n = 1:100000
 
 # ╔═╡ b2d79376-7f73-11ea-2dce-cb9c449eece6
 seq = n .^ -2
+
+# ╔═╡ b2d792c2-7f73-11ea-0c65-a5042701e9f3
+sqrt(sum(seq) * 6.0)
 
 # ╔═╡ Cell order:
 # ╟─b2d786ec-7f73-11ea-1a0c-f38d7b6bbc1e
