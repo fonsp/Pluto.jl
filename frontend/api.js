2e8749504a5d19096dd7c098ded39f3f9d1a7837
Fons van der Plas <fonsvdplas@gmail.com>
2025-07-31 16:04:13 +0200

    Create api.js

---
+++ frontend/api.js
@@ -0,0 +1,3 @@
+export { create_pluto_connection, ws_address_from_base } from "./common/PlutoConnection.js"
+export { empty_notebook_state } from "./editor.js"
+export { applyPatches, produceWithPatches } from "./imports/immer.js"
