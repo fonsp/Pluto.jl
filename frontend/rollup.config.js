eb2fd2296a35e81bb0f528d1a27df43e6d840b59
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliahub.com>
2025-08-06 22:17:54 +0300

    fix: more stuff

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -2,6 +2,42 @@ import typescript from "@rollup/plugin-typescript"
 import resolve from "@rollup/plugin-node-resolve"
 import alias from "@rollup/plugin-alias"
 
+const external = []
+const aliasEntries = [
+    { find: "https://cdn.jsdelivr.net/npm/@observablehq/stdlib@3.3.1/+esm", replacement: "@observablehq/stdlib" },
+    {
+        find: "https://cdn.jsdelivr.net/gh/fonsp/msgpack-lite@0.1.27-es.1/dist/msgpack-es.min.mjs",
+        replacement: "msgpack-lite/dist/msgpack-es.min.mjs",
+    },
+    {
+        find: "https://esm.sh/seamless-scroll-polyfill@2.1.8/lib/polyfill.js?pin=v113&target=es2020",
+        replacement: "seamless-scroll-polyfill/lib/polyfill.js",
+    },
+    { find: "https://esm.sh/preact@10.13.2?pin=v113&target=es2020", replacement: "preact" },
+    { find: "https://esm.sh/preact@10.13.2/hooks?pin=v113&target=es2020", replacement: "preact/hooks" },
+    { find: "https://esm.sh/htm@3.1.1?pin=v113&target=es2020", replacement: "htm" },
+    { find: "https://cdn.jsdelivr.net/npm/immer@8.0.0/dist/immer.esm.js", replacement: "immer" },
+    { find: "https://cdn.jsdelivr.net/npm/lodash-es@4.17.21/+esm", replacement: "lodash-es" },
+    {
+        find: "https://cdn.jsdelivr.net/gh/JuliaPluto/codemirror-pluto-setup@2001.0.0/dist/index.es.min.js",
+        replacement: "@plutojl/codemirror-pluto-setup",
+    },
+    { find: "https://esm.sh/dompurify@3.2.3?pin=v135", replacement: "dompurify" },
+    { find: "https://cdn.jsdelivr.net/npm/ansi_up@5.1.0/+esm", replacement: "ansi_up" },
+    { find: "https://cdn.jsdelivr.net/npm/dialog-polyfill@0.5.6/dist/dialog-polyfill.esm.min.js", replacement: "dialog-polyfill" },
+    { find: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/highlight.min.js", replacement: "highlight.js" },
+    {
+        find: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/languages/julia.min.js",
+        replacement: "highlight.js/lib/languages/julia",
+    },
+    {
+        find: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/languages/julia-repl.min.js",
+        replacement: "highlight.js/lib/languages/julia-repl",
+    },
+    { find: "https://cdn.jsdelivr.net/npm/requestidlecallback-polyfill@1.0.2/index.js", replacement: "requestidlecallback-polyfill" },
+    { find: "https://cdn.jsdelivr.net/npm/vmsg@0.4.0/vmsg.js", replacement: "vmsg" },
+]
+
 export default [
     // ES Module build
     {
@@ -11,8 +47,11 @@ export default [
             format: "es",
             sourcemap: true,
         },
-        external: ["immer"],
+        external,
         plugins: [
+            alias({
+                entries: aliasEntries,
+            }),
             typescript({
                 tsconfig: "./tsconfig.json",
                 declaration: true,
@@ -23,42 +62,6 @@ export default [
                     checkJs: false,
                     noEmit: false,
                 },
-            }),
-            alias({
-                entries: [
-                    { find: "https://cdn.jsdelivr.net/npm/@observablehq/stdlib@3.3.1/+esm", replacement: "@observablehq/stdlib" },
-                    {
-                        find: "https://cdn.jsdelivr.net/gh/fonsp/msgpack-lite@0.1.27-es.1/dist/msgpack-es.min.mjs",
-                        replacement: "msgpack-lite",
-                    },
-                    {
-                        find: "https://esm.sh/seamless-scroll-polyfill@2.1.8/lib/polyfill.js?pin=v113&target=es2020",
-                        replacement: "seamless-scroll-polyfill/lib/polyfill.js",
-                    },
-                    { find: "https://esm.sh/preact@10.13.2?pin=v113&target=es2020", replacement: "preact" },
-                    { find: "https://esm.sh/preact@10.13.2/hooks?pin=v113&target=es2020", replacement: "preact/hooks" },
-                    { find: "https://esm.sh/htm@3.1.1?pin=v113&target=es2020", replacement: "htm" },
-                    { find: "https://cdn.jsdelivr.net/npm/immer@8.0.0/dist/immer.esm.js", replacement: "immer" },
-                    { find: "https://cdn.jsdelivr.net/npm/lodash-es@4.17.21/+esm", replacement: "lodash-es" },
-                    {
-                        find: "https://cdn.jsdelivr.net/gh/JuliaPluto/codemirror-pluto-setup@2001.0.0/dist/index.es.min.js",
-                        replacement: "@plutojl/codemirror-pluto-setup",
-                    },
-                    { find: "https://esm.sh/dompurify@3.2.3?pin=v135", replacement: "dompurify" },
-                    { find: "https://cdn.jsdelivr.net/npm/ansi_up@5.1.0/+esm", replacement: "ansi_up" },
-                    { find: "https://cdn.jsdelivr.net/npm/dialog-polyfill@0.5.6/dist/dialog-polyfill.esm.min.js", replacement: "dialog-polyfill" },
-                    { find: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/highlight.min.js", replacement: "highlight.js" },
-                    {
-                        find: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/languages/julia.min.js",
-                        replacement: "highlight.js/lib/languages/julia",
-                    },
-                    {
-                        find: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/languages/julia-repl.min.js",
-                        replacement: "highlight.js/lib/languages/julia-repl",
-                    },
-                    { find: "https://cdn.jsdelivr.net/npm/requestidlecallback-polyfill@1.0.2/index.js", replacement: "requestidlecallback-polyfill" },
-                    { find: "https://cdn.jsdelivr.net/npm/vmsg@0.4.0/vmsg.js", replacement: "vmsg" },
-                ],
             }),
             resolve({
                 browser: true,
@@ -76,8 +79,11 @@ export default [
             sourcemap: true,
             exports: "named",
         },
-        external: ["immer"],
+        external,
         plugins: [
+            alias({
+                entries: aliasEntries,
+            }),
             typescript({
                 tsconfig: "./tsconfig.json",
                 declaration: true,
@@ -88,42 +94,6 @@ export default [
                     checkJs: false,
                     noEmit: false,
                 },
-            }),
-            alias({
-                entries: [
-                    { find: "https://cdn.jsdelivr.net/npm/@observablehq/stdlib@3.3.1/+esm", replacement: "@observablehq/stdlib" },
-                    {
-                        find: "https://cdn.jsdelivr.net/gh/fonsp/msgpack-lite@0.1.27-es.1/dist/msgpack-es.min.mjs",
-                        replacement: "msgpack-lite",
-                    },
-                    {
-                        find: "https://esm.sh/seamless-scroll-polyfill@2.1.8/lib/polyfill.js?pin=v113&target=es2020",
-                        replacement: "seamless-scroll-polyfill/lib/polyfill.js",
-                    },
-                    { find: "https://esm.sh/preact@10.13.2?pin=v113&target=es2020", replacement: "preact" },
-                    { find: "https://esm.sh/preact@10.13.2/hooks?pin=v113&target=es2020", replacement: "preact/hooks" },
-                    { find: "https://esm.sh/htm@3.1.1?pin=v113&target=es2020", replacement: "htm" },
-                    { find: "https://cdn.jsdelivr.net/npm/immer@8.0.0/dist/immer.esm.js", replacement: "immer" },
-                    { find: "https://cdn.jsdelivr.net/npm/lodash-es@4.17.21/+esm", replacement: "lodash-es" },
-                    {
-                        find: "https://cdn.jsdelivr.net/gh/JuliaPluto/codemirror-pluto-setup@2001.0.0/dist/index.es.min.js",
-                        replacement: "@plutojl/codemirror-pluto-setup",
-                    },
-                    { find: "https://esm.sh/dompurify@3.2.3?pin=v135", replacement: "dompurify" },
-                    { find: "https://cdn.jsdelivr.net/npm/ansi_up@5.1.0/+esm", replacement: "ansi_up" },
-                    { find: "https://cdn.jsdelivr.net/npm/dialog-polyfill@0.5.6/dist/dialog-polyfill.esm.min.js", replacement: "dialog-polyfill" },
-                    { find: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/highlight.min.js", replacement: "highlight.js" },
-                    {
-                        find: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/languages/julia.min.js",
-                        replacement: "highlight.js/lib/languages/julia",
-                    },
-                    {
-                        find: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/languages/julia-repl.min.js",
-                        replacement: "highlight.js/lib/languages/julia-repl",
-                    },
-                    { find: "https://cdn.jsdelivr.net/npm/requestidlecallback-polyfill@1.0.2/index.js", replacement: "requestidlecallback-polyfill" },
-                    { find: "https://cdn.jsdelivr.net/npm/vmsg@0.4.0/vmsg.js", replacement: "vmsg" },
-                ],
             }),
             resolve({
                 browser: true,
