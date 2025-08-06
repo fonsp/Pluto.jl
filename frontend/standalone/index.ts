43e3f1ac5810ebc0fc3294941d180e9beeb971b2
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliahub.com>
2025-08-06 12:37:36 +0300

    properly export types

--- frontend/standalone/index.ts
+++ frontend/standalone/index.ts
@@ -1,15 +1,18 @@
 /**
  * @fileoverview Main entry point for the Pluto Notebook API package
- * 
+ *
  * This package provides a TypeScript/JavaScript API for programmatically
  * interacting with Pluto notebooks without requiring the full Editor UI.
  */
 
 // Export main API classes from PlutoNotebookAPI.js
-export { Pluto, PlutoNotebook } from './PlutoNotebookAPI.js';
+export { Pluto, PlutoNotebook, type CellData } from "./PlutoNotebookAPI.js"
 
 // Export notebook parser functions
-export { default as parseNotebook, serializeNotebook } from './NotebookParser.js';
+export { default as parseNotebook, serializeNotebook } from "./NotebookParser.js"
 
 // Export utility constants
-export { DEFAULT_CELL_METADATA, PTOML_CELL_ID, MTOML_CELL_ID } from './NotebookParser.js';
+export { DEFAULT_CELL_METADATA, PTOML_CELL_ID, MTOML_CELL_ID } from "./NotebookParser.js"
+
+export type { NotebookData, CellDependencyData, CellResultData, CellDependencyGraph, CellInputData, CellMetaData } from "../components/Editor.js"
+export type { PlutoConnection, WebsocketConnection } from "../common/PlutoConnection.js"
