07b0ddc0fe92c360502ec9a735b0323616304bdd
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliahub.com>
2025-08-18 15:04:04 +0300

    feat: split out terminal and progress logs

--- frontend/standalone/getters.ts
+++ frontend/standalone/getters.ts
@@ -1,6 +1,9 @@
 /** Utilities to work with NotebookData */
 import { Worker } from "./client.js"
 
+export const PROGRESS_LOG_LEVEL = "LogLevel(-1)"
+export const STDOUT_LOG_LEVEL = "LogLevel(-555)"
+
 export type CellStatus = "queued" | "running" | "errored" | "done" | "pending"
 
 export function getResult(worker: Worker, cell_id: string) {
@@ -14,7 +17,15 @@ export function getSnippetLogs(worker: Worker, cell_id: string) {
 export function getProgressLogs(worker: Worker, cell_id: string) {
     return (
         getSnippetLogs(worker, cell_id).filter((log) => {
-            return "group" in log && log.group === "ProgressLogging"
+            return log.level === PROGRESS_LOG_LEVEL
+        }) ?? []
+    )
+}
+
+export function getTerminalLogs(worker: Worker, cell_id: string) {
+    return (
+        getSnippetLogs(worker, cell_id).filter((log) => {
+            return log.level === STDOUT_LOG_LEVEL
         }) ?? []
     )
 }
