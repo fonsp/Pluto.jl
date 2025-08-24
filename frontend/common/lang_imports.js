96a932a8f91a3bc2f475bac36c8711413425e71b
Fons van der Plas <fonsvdplas@gmail.com>
2025-08-24 11:23:52 +0200

    fixes

--- frontend/common/lang_imports.js
+++ frontend/common/lang_imports.js
@@ -12,6 +12,4 @@ export { default as french } from "../lang/french.json" with { type: "json" }
 // @ts-ignore
 export { default as nederlands } from "../lang/nederlands.json" with { type: "json" }
 // @ts-ignore
-export { default as nederlands } from "../lang/nederlands.json" with { type: "json" }
-// @ts-ignore
 export { default as norsk_bokmål } from "../lang/norsk_bokmål.json" with { type: "json" }
