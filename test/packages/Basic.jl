952b333524a657c1532a0bab3f722a401f054c04
Fons van der Plas <fonsvdplas@gmail.com>
2025-09-18 17:23:29 +0200

    reenable broken test

--- test/packages/Basic.jl
+++ test/packages/Basic.jl
@@ -709,10 +709,10 @@ import Malt
 
             full_logs = join([log["msg"][1] for log in notebook.cells[1].logs], "\n")
             
-            is_broken_idk_why = Sys.iswindows() && v"1.11.0-aaa" <= VERSION < v"1.12.0-aaa" && !match
-            is_broken_idk_why |= VERSION >= v"1.12.0-aaa" && !match
+            # is_broken_idk_why = Sys.iswindows() && v"1.11.0-aaa" <= VERSION < v"1.12.0-aaa" && !match
+            # is_broken_idk_why |= VERSION >= v"1.12.0-aaa" && !match
 
-            if !is_broken_idk_why
+            if true#!is_broken_idk_why
                 # There should be a log message about loading the cache.
                 @test occursin(r"Loading.*cache"i, full_logs)
                 # There should NOT be a log message about rejecting the cache.
