60dc5d10df00cc86e0e0ea499721c634d27d750b
bvdmitri <bvdmitri@gmail.com>
2025-09-23 20:51:57 +0200

    fix the code

--- frontend/imports/immer.js
+++ frontend/imports/immer.js
@@ -1,5 +1,10 @@
 // @ts-nocheck
-import immer, { produceWithPatches, applyPatches, enablePatches, setAutoFreeze } from "https://cdn.jsdelivr.net/npm/immer@10.1.3/+esm"
+import { produce as immer, produceWithPatches, applyPatches, enablePatches, setAutoFreeze } from "https://cdn.jsdelivr.net/npm/immer@10.1.3/dist/immer.mjs"
+
+// note that on the previous version, we used `import immer, { produceWithPatches, ... } from "https://cdn.jsdelivr.net/npm/immer@8.0.0/dist/immer.esm.js"`
+// the `immer` variable was actually shadowing the `produce` function on the previously used version
+// for backward compatibility with the rest of the code, we still export the `immer` variable which is a renamed version of the `produce` function
+// see https://github.com/fonsp/Pluto.jl/pull/3372
 
 export { applyPatches, produceWithPatches }
 export default immer
