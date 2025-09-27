418fcd10e79b3ccc6f06960e99f0ff709a181e75
Fons van der Plas <fonsvdplas@gmail.com>
2025-09-27 13:48:08 +0200

    remove debug logs

--- src/webserver/timer callback.jl
+++ src/webserver/timer callback.jl
@@ -1,25 +1,18 @@
 import .TempDirInScratch
 
 function timer_callback(session::ServerSession)
-    
-    # Update TempDirInScratch
-    @info "# Update TempDirInScratch"
     try
+        # Update TempDirInScratch
         for notebook in values(session.notebooks)
             ctx = notebook.nbpkg_ctx
             if ctx !== nothing
                 d = PkgCompat.env_dir(ctx)
-                @info "# Marking temp dir $d as current"
                 TempDirInScratch.mark_as_current(d)
             end
         end
-        
-        @info "# Cleaning up temp dirs"
         TempDirInScratch.cleanup()
     catch e
         @warn "Error in async update loop" exception=(e, catch_backtrace())
-    finally
-        @info "# Done updating TempDirInScratch"
     end    
 end
 
