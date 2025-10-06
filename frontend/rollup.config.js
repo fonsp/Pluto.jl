6368dd8bae9a7006e3feb5e98f8c189589f693f7
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliahub.com>
2025-10-07 01:56:20 +0300

    feat: bundle ui in rainbow

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -6,7 +6,22 @@ import json from "@rollup/plugin-json"
 import nodePolyfills from "rollup-plugin-node-polyfills"
 import { copy } from "@web/rollup-plugin-copy"
 
-const external = ["react", "react-dom"]
+const external = [
+    "react",
+    "react-dom",
+    "https://esm.sh/preact@10.13.2?pin=v113&target=es2020",
+    "https://esm.sh/preact@10.13.2/hooks?pin=v113&target=es2020",
+    "https://esm.sh/htm@3.1.1?pin=v113&target=es2020",
+    "https://cdn.jsdelivr.net/npm/lodash-es@4.17.21/+esm",
+    "https://cdn.jsdelivr.net/gh/JuliaPluto/codemirror-pluto-setup@2001.0.0/dist/index.es.min.js",
+    "https://esm.sh/dompurify@3.2.3?pin=v135",
+    "https://cdn.jsdelivr.net/npm/ansi_up@5.1.0/+esm",
+    "https://cdn.jsdelivr.net/npm/dialog-polyfill@0.5.6/dist/dialog-polyfill.esm.min.js",
+    "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/languages/julia-repl.min.js",
+    "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/languages/julia.min.js",
+    "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/highlight.min.js",
+]
+
 const aliasEntries = [
     { find: "https://cdn.jsdelivr.net/npm/@observablehq/stdlib@3.3.1/+esm", replacement: "@observablehq/stdlib" },
     {
@@ -17,27 +32,8 @@ const aliasEntries = [
         find: "https://esm.sh/seamless-scroll-polyfill@2.1.8/lib/polyfill.js?pin=v113&target=es2020",
         replacement: "seamless-scroll-polyfill/lib/polyfill.js",
     },
-    { find: "https://esm.sh/preact@10.13.2?pin=v113&target=es2020", replacement: "preact" },
-    { find: "https://esm.sh/preact@10.13.2/hooks?pin=v113&target=es2020", replacement: "preact/hooks" },
-    { find: "https://esm.sh/htm@3.1.1?pin=v113&target=es2020", replacement: "htm" },
     { find: "https://cdn.jsdelivr.net/npm/immer@10.1.1/dist/immer.production.mjs", replacement: "immer" },
     { find: "https://cdn.jsdelivr.net/npm/lodash-es@4.17.21/+esm", replacement: "lodash-es" },
-    {
-        find: "https://cdn.jsdelivr.net/gh/JuliaPluto/codemirror-pluto-setup@2001.0.0/dist/index.es.min.js",
-        replacement: "@plutojl/codemirror-pluto-setup",
-    },
-    { find: "https://esm.sh/dompurify@3.2.3?pin=v135", replacement: "dompurify" },
-    { find: "https://cdn.jsdelivr.net/npm/ansi_up@5.1.0/+esm", replacement: "ansi_up" },
-    { find: "https://cdn.jsdelivr.net/npm/dialog-polyfill@0.5.6/dist/dialog-polyfill.esm.min.js", replacement: "dialog-polyfill" },
-    { find: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/highlight.min.js", replacement: "highlight.js" },
-    {
-        find: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/languages/julia.min.js",
-        replacement: "highlight.js/lib/languages/julia",
-    },
-    {
-        find: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/languages/julia-repl.min.js",
-        replacement: "highlight.js/lib/languages/julia-repl",
-    },
     { find: "https://cdn.jsdelivr.net/npm/requestidlecallback-polyfill@1.0.2/index.js", replacement: "requestidlecallback-polyfill" },
     { find: "https://cdn.jsdelivr.net/npm/vmsg@0.4.0/vmsg.js", replacement: "vmsg" },
     { find: "fs", replacement: "memfs" },
@@ -60,9 +56,33 @@ const plugins = [
     copy({ patterns: "standalone/integrations/node-polyfill.js" }),
     copy({ patterns: "standalone/index.d.ts" }),
     copy({ patterns: "standalone/integrations/react.d.ts" }),
+    copy({ patterns: "imports/*.d.ts", rootDir: "." }),
 ]
 
 export default [
+    // ES Module build
+    {
+        input: "ui.ts",
+        output: {
+            file: "dist/ui.esm.js",
+            format: "es",
+            sourcemap: true,
+        },
+        external,
+        plugins: [plugins.slice(0)],
+    },
+    // CommonJS build: parcel will do this
+    {
+        input: "ui.ts",
+        output: {
+            file: "dist/ui.js",
+            format: "cjs",
+            sourcemap: true,
+            exports: "named",
+        },
+        external,
+        plugins: [...plugins.slice(1)],
+    },
     // ES Module build
     {
         input: "standalone/index.js",
