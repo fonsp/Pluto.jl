4adc29c62325afa85208265a954edb4214784c1f
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliahub.com>
2025-10-07 02:39:56 +0300

    feat: export bond helpers

--- frontend/ui.ts
+++ frontend/ui.ts
@@ -2,4 +2,13 @@ export { StatusTab } from "./components/StatusTab.js"
 export { CellOutput, OutputBody } from "./components/CellOutput.js"
 export { CellInput } from "./components/CellInput.js"
 
+export {
+    set_bound_elements_to_their_value,
+    add_bonds_disabled_message_handler,
+    get_input_value,
+    set_input_value,
+    add_bonds_listener,
+    eventof,
+} from "./common/Bond.js"
+
 export * from "./imports/Preact.js"
