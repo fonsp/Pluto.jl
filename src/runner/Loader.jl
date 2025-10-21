2f94d53c500b8b02fe7d00b22e83c14685c8ee50
Fons van der Plas <fonsvdplas@gmail.com>
2025-10-22 00:02:00 +0200

    Faster process init: PlutoRunner Loader optimization

--- src/runner/Loader.jl
+++ src/runner/Loader.jl
@@ -5,10 +5,23 @@
 # Our solution is to create a temporary environment just for loading PlutoRunner. This environment is stored in a scratchspace parameterized by the Pluto version and julia version,
 # and used by all notebook launches. Reusing the environment means extra speed.
 
+macro time_it(ex)
+	s = "$ex"
+    quote
+		start = time()
+		try
+			$(esc(ex))
+		finally
+			elapsed = time() - start
+        	@info "Time $(round(elapsed; digits=2))s – $($(s))"
+		end
+    end
+end
+
 
 begin
     pushfirst!(LOAD_PATH, "@stdlib")
-    import Pkg
+    @time_it import Pkg
     popfirst!(LOAD_PATH)
 
     local original_LP = copy(LOAD_PATH)
@@ -25,40 +38,59 @@ begin
         # Activate the environment
         copy!(LOAD_PATH, new_LP)
         Base.ACTIVE_PROJECT[] = new_AP
-
-        # Set up our notebook boot package environment by adding a single package:
-        path = joinpath(@__DIR__, "PlutoRunner")
-        try
-            Pkg.develop([Pkg.PackageSpec(; path)]; io=devnull)
-        catch
-            @warn "Something went wrong while initializing the notebook boot environment... Trying again and showing you the output."
-            Pkg.develop([Pkg.PackageSpec(; path)])
-        end
-
-        # Resolve
-        try
-            Pkg.resolve(; io=devnull) # supress IO
-        catch
-            @warn "Something went wrong while initializing the notebook boot environment... Trying again and showing you the output."
+        
+        function setup()
+            @info "Running PlutoRunner setup..."
+            # Set up our notebook boot package environment by adding a single package:
+            path = joinpath(@__DIR__, "PlutoRunner")
             try
-                Pkg.resolve()
+                @time_it Pkg.develop([Pkg.PackageSpec(; path)])
+                # @time_it Pkg.develop([Pkg.PackageSpec(; path)]; io=devnull)
+            catch
+                @warn "Something went wrong while initializing the notebook boot environment... Trying again and showing you the output."
+                @time_it Pkg.develop([Pkg.PackageSpec(; path)])
+            end
+    
+            # Resolve
+            try
+                @time_it Pkg.resolve(; io=devnull) # supress IO
+            catch
+                @warn "Something went wrong while initializing the notebook boot environment... Trying again and showing you the output."
+                try
+                    @time_it Pkg.resolve()
+                catch e
+                    @error "Failed to resolve notebook boot environment" exception = (e, catch_backtrace())
+                end
+            end
+    
+            # Instantiate
+            try
+                # we don't suppress IO for this one because it can take very long, and that would be a frustrating experience without IO
+                # precompilation switched off because of https://github.com/fonsp/Pluto.jl/issues/875
+                @time_it Pkg.instantiate(; update_registry=false, allow_autoprecomp=false)
             catch e
-                @error "Failed to resolve notebook boot environment" exception = (e, catch_backtrace())
+                @error "Failed to instantiate notebook boot environment" exception = (e, catch_backtrace())
             end
         end
-
-        # Instantiate
-        try
-            # we don't suppress IO for this one because it can take very long, and that would be a frustrating experience without IO
-            # precompilation switched off because of https://github.com/fonsp/Pluto.jl/issues/875
-            Pkg.instantiate(; update_registry=false, allow_autoprecomp=false)
-        catch e
-            @error "Failed to instantiate notebook boot environment" exception = (e, catch_backtrace())
+        
+        @info "zzzzz.."
+        if !isfile(joinpath(new_AP, "Project.toml")) || !isfile(joinpath(new_AP, "Manifest.toml"))
+        @info "kljsdflksdfjlfds.."
+            setup()
+        else
+            @info "PlutoRunner setup already run. Skipping..."
         end
 
-        # Import PlutoRunner into Main
-        import PlutoRunner
-
+        try
+            # Import PlutoRunner into Main
+            # try the first time without setup
+            # This might trigger precompilation, which is actually what we want.
+            @time_it import PlutoRunner
+        catch
+            # this means that setup is needed.
+            setup()
+            @time_it import PlutoRunner
+        end
     finally
         # Reset the pkg environment
         copy!(LOAD_PATH, original_LP)
