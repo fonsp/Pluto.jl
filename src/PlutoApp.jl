68bce96392612ceb901a4477902ec2742ea509ac
Romeo Valentin <romeov@stanford.edu>
2025-11-25 17:08:32 -0800

    Formatting nits

--- src/PlutoApp.jl
+++ src/PlutoApp.jl
@@ -1,17 +1,17 @@
 module PlutoApp
+
 using Pluto
 import Comonicon
 
+# Note we need the `# Intro` header for Comonicon to pick up this docstring.
 """
 # Intro
 The `pluto` executable exposes a simple CLI interface for the `Pluto.run(; ...)` function.
-It is supposed to be used via Julia ["apps"](https://pkgdocs.julialang.org/v1/apps/) from Julia 1.11+
-and typically lives in your `\$PATH`.
-It can be installed via `julia -e "import Pkg; Pkg.Apps.add("Pluto")"`.
+It is supposed to be used via Julia ["apps"](https://pkgdocs.julialang.org/v1/apps/) from Julia 1.11+ and typically lives in your `\$PATH`.
+It can be installed via `julia -e "import Pkg; Pkg.Apps.add(\"Pluto\")"`.
 """
 Comonicon.@main function pluto(notebooks::String...;
-    # Comonicon flags must default to false
-    dont_launch_browser::Bool=false,
+    dont_launch_browser::Bool=false, # Comonicon flags must default to false
     port::Int=-1,  # Union{Nothing, Int} not supported by Comonicon
     host::String="127.0.0.1",
     auto_reload_from_file::Bool=false
@@ -24,8 +24,7 @@ Comonicon.@main function pluto(notebooks::String...;
 end
 function (Base.@main)(ARGS::Vector{String})
     # this function is generated into the current module by Comonicon
-    # and will ultimately call the function above annotated by
-    # `Comonicon.@main`
+    # and will ultimately call the function above annotated by `Comonicon.@main`
     return command_main(ARGS)
 end
 
