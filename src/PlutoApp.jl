469f8436a50a5d56508aa29e361a35c3aab10685
Romeo Valentin <romeov@stanford.edu>
2025-11-25 16:05:29 -0800

    Correctly add Comonicon for CLI arg parsing

--- src/PlutoApp.jl
+++ src/PlutoApp.jl
@@ -1,10 +1,25 @@
 module PlutoApp
 using Pluto
+import Comonicon
 
-function (@main)(ARGS)
-    Pluto.run()
+Comonicon.@main function pluto(notebooks::String...;
+    # Comonicon flags must default to false
+    dont_launch_browser::Bool=false,
+    port::Int=-1,  # Union{Nothing, Int} not supported by Comonicon
+    host::String="127.0.0.1",
+    auto_reload_from_file::Bool=false
+)
+    notebook = (isempty(notebooks) ? nothing : collect(notebooks))
+    port = (port == -1 ? nothing : port)
+    launch_browser = !dont_launch_browser
+    Pluto.run(; notebook, launch_browser, port, host, auto_reload_from_file)
     return
 end
-
+function (Base.@main)(ARGS::Vector{String})
+    # this function is generated into the current module by Comonicon
+    # and will ultimately call the function above annotated by
+    # `Comonicon.@main`
+    return command_main(ARGS)
+end
 
 end
